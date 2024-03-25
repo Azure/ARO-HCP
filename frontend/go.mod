332211445e7f615f478ea8c9ae1f2eeb0ce0a446
Matthew Barnes <mbarnes@fedorapeople.org>
2024-03-25 14:55:28 -0400

    frontend: Add middleware unit tests

--- frontend/go.mod
+++ frontend/go.mod
@@ -1,3 +1,5 @@
 module github.com/Azure/ARO-HCP/frontend
 
 go 1.22
+
+require github.com/google/go-cmp v0.6.0 // indirect
---
+++ frontend/middleware_lowercase_test.go
@@ -0,0 +1,34 @@
+package main
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestMiddlewareLowercase(t *testing.T) {
+	writer := httptest.NewRecorder()
+
+	request, err := http.NewRequest(http.MethodGet, "/TEST", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	next := func(w http.ResponseWriter, r *http.Request) {
+		request = r // capture modified request
+	}
+
+	MiddlewareLowercase(writer, request, next)
+
+	if request.URL.Path != "/test" {
+		t.Error(request.URL.Path)
+	}
+
+	originalPath := request.Context().Value(ContextKeyOriginalPath).(string)
+	if originalPath != "/TEST" {
+		t.Error(originalPath)
+	}
+}
