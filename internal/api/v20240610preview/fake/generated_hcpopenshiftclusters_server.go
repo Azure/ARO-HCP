e1b62d7a1d4a9b15e6a01aee355212e945e27b6f
Matthew Barnes <mbarnes@fedorapeople.org>
2024-03-28 12:10:59 -0400

    autorest-config.yaml: Fix containing-module path

--- internal/api/v20240610preview/fake/generated_hcpopenshiftclusters_server.go
+++ internal/api/v20240610preview/fake/generated_hcpopenshiftclusters_server.go
@@ -11,7 +11,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/Azure/ARO-HCP/internal/v20240610preview"
+	"github.com/Azure/ARO-HCP/internal/api/v20240610preview"
 	azfake "github.com/Azure/azure-sdk-for-go/sdk/azcore/fake"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/fake/server"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
