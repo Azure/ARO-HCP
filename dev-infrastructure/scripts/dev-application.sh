c9cefbe16a518adb41f37a0eca5890c0738f1238
Spencer Amann <samann@redhat.com>
2024-04-23 16:13:31 -0400

    updates script to enhance mock app creation/deltion

--- dev-infrastructure/scripts/dev-application.sh
+++ dev-infrastructure/scripts/dev-application.sh
@@ -5,11 +5,16 @@
 
 LOCATION="eastus"
 UNIQUE_PREFIX="HCP-$USER-$LOCATION"
+if [ ${#UNIQUE_PREFIX} -gt 20 ]; then
+    echo "UNIQUE_PREFIX=$UNIQUE_PREFIX is too long"
+    UNIQUE_PREFIX=${UNIQUE_PREFIX:0:20}
+    echo "trimmed UNIQUE_PREFIX=$UNIQUE_PREFIX to 20 characters"
+fi
 APP_KEY_VAULT_NAME="$UNIQUE_PREFIX-vlt"
 APP_CERT_NAME="$UNIQUE_PREFIX-svc"
 APP_REGISTRATION_NAME="$UNIQUE_PREFIX-app"
 RESOURCE_GROUP="$UNIQUE_PREFIX-RG"
-ROLE_DEFINITION_NAME="$UNIQUE_PREFIX Role"
+ROLE_DEFINITION_NAME="$UNIQUE_PREFIX-role"
 SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 
 printEnv() {
@@ -29,26 +34,39 @@ createMockFirstPartyApp() {
         exit 1
     fi
     
-    # Create a resource group to hold the key vault
+    echo "Creating resource group: $RESOURCE_GROUP"
     az group create \
     --name "$RESOURCE_GROUP" \
     --location "$LOCATION"
     
-    # Create a keyvault to hold the certificate
+    echo "Creating keyvault: $APP_KEY_VAULT_NAME"
     az keyvault create \
     --location "$LOCATION" \
     --name "$APP_KEY_VAULT_NAME" \
     --resource-group "$RESOURCE_GROUP" \
     --enable-rbac-authorization false
+
+    echo "checking if certificate: $APP_CERT_NAME exists"
+    certExists=$(az keyvault certificate list --vault-name $APP_KEY_VAULT_NAME --query "[?name=='$APP_CERT_NAME'].name" -o tsv)
+    if [ -n "$certExists" ]; then
+        echo "Certificate already exists"
+        exit 1
+    fi
     
-    # Create a certificate in the keyvault
+    echo "Creating certificate: $APP_CERT_NAME"
     az keyvault certificate create \
     --vault-name "$APP_KEY_VAULT_NAME" \
     --name "$APP_CERT_NAME" \
     --policy "$(az keyvault certificate get-default-policy)"
     
     # Create a custom role defintion if it doesn't exist already
-    # TODO: check if it exists
+    echo "checking if role definition: $ROLE_DEFINITION_NAME exists"
+    roleExists=$(az role definition list --name "$ROLE_DEFINITION_NAME" --query "[0].name" -o tsv)
+
+    if [ -n "$roleExists" ]; then
+        echo "Role definition already exists"
+        exit 1
+    fi
     
     # add assignable scope to the custom role with the current subscription
     roleDef=$(jq ".AssignableScopes = [\"/subscriptions/$SUBSCRIPTION_ID\"]" dev-role-definition.json)
@@ -56,25 +74,39 @@ createMockFirstPartyApp() {
     roleDef=$(jq ".Name = \"$ROLE_DEFINITION_NAME\"" temp.json)
     rm temp.json
     
+    echo "Creating role definition: $ROLE_DEFINITION_NAME \n $roleDef\n"
     az role definition create --role-definition "$roleDef"
     
-    # Create an app registration and adds a Service Pricipal using the certificate
+    echo "Creating app registration: $APP_REGISTRATION_NAME"
     az ad sp create-for-rbac \
     --display-name "$APP_REGISTRATION_NAME" \
     --keyvault "$APP_KEY_VAULT_NAME" \
     --cert "$APP_CERT_NAME" \
-    --role "ARO HCP Dev Role" \
+    --role "$ROLE_DEFINITION_NAME" \
     --scopes "/subscriptions/$SUBSCRIPTION_ID"
 }
 
 deleteMockFirstPartyApp() {
     echo "Deleting the standalone dev application with the following ENV:"
     printEnv
-    az ad sp delete --id "$(az ad sp list --display-name "$APP_REGISTRATION_NAME" --query "[0].id" -o tsv)"
-    az ad app delete --id "$(az ad app list --display-name "$APP_REGISTRATION_NAME" --query "[0].id" -o tsv)"
+    spId="$(az ad sp list --display-name "$APP_REGISTRATION_NAME" --query "[0].id" -o tsv)"
+    echo "deleting sp with id: $spId"
+    az ad sp delete --id "$spId"
+
+    appId="$(az ad app list --display-name "$APP_REGISTRATION_NAME" --query "[0].appId" -o tsv)"
+    echo "deleting app with id: $appId"
+    az ad app delete --id "$appId"
+
+    echo "deleting role definition: $ROLE_DEFINITION_NAME"
     az role definition delete --name "$ROLE_DEFINITION_NAME"
+
+    echo "deleting keyvault: $APP_KEY_VAULT_NAME in resource group: $RESOURCE_GROUP"
     az keyvault delete --name "$APP_KEY_VAULT_NAME" --resource-group "$RESOURCE_GROUP"
+
+    echo "purging keyvault: $APP_KEY_VAULT_NAME"
     az keyvault purge --name "$APP_KEY_VAULT_NAME"
+
+    echo "delete resource group: $RESOURCE_GROUP"
     az group delete --name "$RESOURCE_GROUP" --yes
 }
 
