8edef31828070fdbaf8e71889771092eac9bcee6
Spencer Amann <samann@redhat.com>
2024-04-24 09:02:39 -0400

    handles edge cases with role definition

--- dev-infrastructure/scripts/dev-application.sh
+++ dev-infrastructure/scripts/dev-application.sh
@@ -45,39 +45,46 @@ createMockFirstPartyApp() {
     --name "$APP_KEY_VAULT_NAME" \
     --resource-group "$RESOURCE_GROUP" \
     --enable-rbac-authorization false
-
+    
     echo "checking if certificate: $APP_CERT_NAME exists"
     certExists=$(az keyvault certificate list --vault-name $APP_KEY_VAULT_NAME --query "[?name=='$APP_CERT_NAME'].name" -o tsv)
     if [ -n "$certExists" ]; then
         echo "Certificate already exists"
         exit 1
+    else
+        echo "Certificate does not exist"
+        echo "Creating certificate: $APP_CERT_NAME"
+        az keyvault certificate create \
+        --vault-name "$APP_KEY_VAULT_NAME" \
+        --name "$APP_CERT_NAME" \
+        --policy "$(az keyvault certificate get-default-policy)"
     fi
-    
-    echo "Creating certificate: $APP_CERT_NAME"
-    az keyvault certificate create \
-    --vault-name "$APP_KEY_VAULT_NAME" \
-    --name "$APP_CERT_NAME" \
-    --policy "$(az keyvault certificate get-default-policy)"
     
     # Create a custom role defintion if it doesn't exist already
     echo "checking if role definition: $ROLE_DEFINITION_NAME exists"
     roleExists=$(az role definition list --name "$ROLE_DEFINITION_NAME" --query "[0].name" -o tsv)
-
+    
     if [ -n "$roleExists" ]; then
         echo "Role definition already exists"
-        exit 1
+    else
+        echo "Role definition does not exist"
+        # add assignable scope to the custom role with the current subscription
+        roleDef=$(jq ".AssignableScopes = [\"/subscriptions/$SUBSCRIPTION_ID\"]" mock-dev-role-definition.json)
+        echo $roleDef >> temp.json
+        roleDef=$(jq ".Name = \"$ROLE_DEFINITION_NAME\"" temp.json)
+        rm temp.json
+        
+        echo "creating role definition: $ROLE_DEFINITION_NAME \n $roleDef\n"
+        az role definition create --role-definition "$roleDef"
+        while [ -n "$roleExists" ]; do
+            roleExists=$(az role definition list --name "$ROLE_DEFINITION_NAME" --query "[0].name" -o tsv)
+            echo "waiting for role definition to be created"
+            sleep 5
+        done
     fi
     
-    # add assignable scope to the custom role with the current subscription
-    roleDef=$(jq ".AssignableScopes = [\"/subscriptions/$SUBSCRIPTION_ID\"]" dev-role-definition.json)
-    echo $roleDef >> temp.json
-    roleDef=$(jq ".Name = \"$ROLE_DEFINITION_NAME\"" temp.json)
-    rm temp.json
     
-    echo "Creating role definition: $ROLE_DEFINITION_NAME \n $roleDef\n"
-    az role definition create --role-definition "$roleDef"
-    
-    echo "Creating app registration: $APP_REGISTRATION_NAME"
+    echo "creating app registration: $APP_REGISTRATION_NAME"
     az ad sp create-for-rbac \
     --display-name "$APP_REGISTRATION_NAME" \
     --keyvault "$APP_KEY_VAULT_NAME" \
@@ -89,23 +96,27 @@ createMockFirstPartyApp() {
 deleteMockFirstPartyApp() {
     echo "Deleting the standalone dev application with the following ENV:"
     printEnv
+    
+    echo "deleting all role assignments with role: $ROLE_DEFINITION_NAME"
+    az role assignment list --role "$ROLE_DEFINITION_NAME" --query "[].id" -o tsv | xargs -I {} az role assignment delete --ids {}
+    
     spId="$(az ad sp list --display-name "$APP_REGISTRATION_NAME" --query "[0].id" -o tsv)"
     echo "deleting sp with id: $spId"
     az ad sp delete --id "$spId"
-
+    
     appId="$(az ad app list --display-name "$APP_REGISTRATION_NAME" --query "[0].appId" -o tsv)"
     echo "deleting app with id: $appId"
     az ad app delete --id "$appId"
-
+    
     echo "deleting role definition: $ROLE_DEFINITION_NAME"
     az role definition delete --name "$ROLE_DEFINITION_NAME"
-
+    
     echo "deleting keyvault: $APP_KEY_VAULT_NAME in resource group: $RESOURCE_GROUP"
     az keyvault delete --name "$APP_KEY_VAULT_NAME" --resource-group "$RESOURCE_GROUP"
-
+    
     echo "purging keyvault: $APP_KEY_VAULT_NAME"
     az keyvault purge --name "$APP_KEY_VAULT_NAME"
-
+    
     echo "delete resource group: $RESOURCE_GROUP"
     az group delete --name "$RESOURCE_GROUP" --yes
 }
@@ -113,12 +124,12 @@ deleteMockFirstPartyApp() {
 case "$1" in
     "create")
         createMockFirstPartyApp
-        ;;
+    ;;
     "delete")
         deleteMockFirstPartyApp
-        ;;
+    ;;
     *)
         echo "Usage: $0 {create|delete}"
         exit 1
-        ;;
+    ;;
 esac
