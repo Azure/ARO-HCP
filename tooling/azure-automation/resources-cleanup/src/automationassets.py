39ae48fc7f3e793320c7a43cd70f1570c5bfe10e
bennerv <10840174+bennerv@users.noreply.github.com>
2024-05-06 21:52:43 -0500

    Remove unused variable in automationassets.py

--- tooling/azure-automation/resources-cleanup/src/automationassets.py
+++ tooling/azure-automation/resources-cleanup/src/automationassets.py
@@ -1,32 +1,9 @@
 """ Azure Automation assets module to be used with Azure Automation during offline development """
 #!/usr/bin/env python3
-# ----------------------------------------------------------------------------------
-#
-# MIT License
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-# ----------------------------------------------------------------------------------
-
-# Constant keys for extracing items from automation assets.
+# Constant keys for extracting items from automation assets.
 _KEY_NAME = "Name"
 _KEY_VALUE = "Value"
-_KEY_USERNAME = "Username"
 _KEY_PASSWORD = "Password"
 _KEY_CERTPATH = "CertPath"
 _KEY_CONNECTION_FIELDS = "ValueFields"
@@ -122,7 +99,7 @@ def set_automation_variable(name, value):
 
 
 def get_automation_credential(name):
-    """ Returns an automation credential as a dictionay with username and password as keys """
+    """ Returns an automation credential as a dictionary with username and password as keys """
     credential = _get_asset(_KEY_CREDENTIAL, name)
 
     # Return a dictionary of the credential asset
