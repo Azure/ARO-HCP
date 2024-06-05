994fbb766150a2c59951755bb8e56a6333f65480
Michael Shen <mshen@redhat.com>
2024-06-04 23:13:35 -0400

    improve documentation of the DBClient
    * Add docstrings for GetClusterDoc, DeleteClusterDoc, and
      GetSubscriptionDoc
    * Rename partitionKey parameter to subscriptionID for clarity
    
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- frontend/pkg/database/cache.go
+++ frontend/pkg/database/cache.go
@@ -4,11 +4,15 @@ import "context"
 
 var _ DBClient = &Cache{}
 
+// Cache is a simple DBClient that allows us to perform simple tests without needing a real CosmosDB. For production,
+// use CosmosDBClient instead. Call NewCache() to initialize a Cache correctly.
 type Cache struct {
 	cluster      map[string]*HCPOpenShiftClusterDocument
 	subscription map[string]*SubscriptionDocument
 }
 
+// NewCache initializes a new Cache to allow for simple tests without needing a real CosmosDB. For production, use
+// NewCosmosDBConfig instead.
 func NewCache() DBClient {
 	return &Cache{
 		cluster:      make(map[string]*HCPOpenShiftClusterDocument),
@@ -20,7 +24,7 @@ func (c *Cache) DBConnectionTest(ctx context.Context) error {
 	return nil
 }
 
-func (c *Cache) GetClusterDoc(ctx context.Context, resourceID string, partitionKey string) (*HCPOpenShiftClusterDocument, error) {
+func (c *Cache) GetClusterDoc(ctx context.Context, resourceID string, subscriptionID string) (*HCPOpenShiftClusterDocument, error) {
 	if _, ok := c.cluster[resourceID]; ok {
 		return c.cluster[resourceID], nil
 	}
@@ -33,14 +37,14 @@ func (c *Cache) SetClusterDoc(ctx context.Context, doc *HCPOpenShiftClusterDocum
 	return nil
 }
 
-func (c *Cache) DeleteClusterDoc(ctx context.Context, resourceID string, partitionKey string) error {
+func (c *Cache) DeleteClusterDoc(ctx context.Context, resourceID string, subscriptionID string) error {
 	delete(c.cluster, resourceID)
 	return nil
 }
 
-func (c *Cache) GetSubscriptionDoc(ctx context.Context, partitionKey string) (*SubscriptionDocument, error) {
-	if _, ok := c.subscription[partitionKey]; ok {
-		return c.subscription[partitionKey], nil
+func (c *Cache) GetSubscriptionDoc(ctx context.Context, subscriptionID string) (*SubscriptionDocument, error) {
+	if _, ok := c.subscription[subscriptionID]; ok {
+		return c.subscription[subscriptionID], nil
 	}
 
 	return nil, ErrNotFound
