6611c140c02a8b88ebcef63ae87770d61902916c
Antony Natale <anatale@redhat.com>
2024-06-10 16:02:35 -0400

    minor bug fix and readme update

--- dev-infrastructure/scripts/aks-admin-access.sh
+++ dev-infrastructure/scripts/aks-admin-access.sh
@@ -2,7 +2,7 @@
 
 RESOURCEGROUP=$1
 CURRENTUSER_CLIENT_ID=$(az ad signed-in-user show | jq -r '.id')
-CLUSTER_ID=$(az aks list -g aro-hcp-svc-cluster-goberlec | jq -r .[0].id)
+CLUSTER_ID=$(az aks list -g $RESOURCEGROUP | jq -r .[0].id)
 
 az role assignment create --assignee $CURRENTUSER_CLIENT_ID --role "Azure Kubernetes Service Cluster Admin Role" --scope $CLUSTER_ID
 echo "It might take a couple of minutes for the permissions to take effect"
