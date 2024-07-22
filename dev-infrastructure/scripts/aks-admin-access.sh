38b459d9e88898d79780e6aa0eacb841828aab07
Gerd Oberlechner <goberlec@redhat.com>
2024-07-22 12:40:57 +0200

    make aks.kubeconfig without local accounts
    follows up on https://github.com/Azure/ARO-HCP/pull/371 to allow `make aks.kubeconfig` even with local accounts disabled
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- dev-infrastructure/scripts/aks-admin-access.sh
+++ dev-infrastructure/scripts/aks-admin-access.sh
@@ -4,5 +4,6 @@ RESOURCEGROUP=$1
 CURRENTUSER_CLIENT_ID=$(az ad signed-in-user show | jq -r '.id')
 CLUSTER_ID=$(az aks list -g $RESOURCEGROUP | jq -r .[0].id)
 
+az role assignment create --assignee $CURRENTUSER_CLIENT_ID --role "Azure Kubernetes Service RBAC Cluster Admin" --scope $CLUSTER_ID
 az role assignment create --assignee $CURRENTUSER_CLIENT_ID --role "Azure Kubernetes Service Cluster Admin Role" --scope $CLUSTER_ID
 echo "It might take a couple of minutes for the permissions to take effect"
