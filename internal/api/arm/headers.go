fdf6120f06b558c6da7b9d46e6f60d3a901be1e4
Matthew Barnes <mbarnes@fedorapeople.org>
2024-07-29 12:57:39 -0400

    api: Define Azure headers for asynchronous operations

--- internal/api/arm/headers.go
+++ internal/api/arm/headers.go
@@ -4,8 +4,15 @@ package arm
 // Licensed under the Apache License 2.0.
 
 const (
+	// Azure-specific HTTP header names
+	HeaderNameAsyncOperation       = "Azure-AsyncOperation"
+	HeaderNameAsyncNotification    = "Azure-AsyncNotification"
+	HeaderNameAsyncNotificationURI = "Azure-AsyncNotificationUri"
+
 	// Microsoft-specific HTTP header names
 	HeaderNameErrorCode             = "X-Ms-Error-Code"
+	HeaderNameHomeTenantID          = "X-Ms-Home-Tenant-Id"
+	HeaderNameClientObjectID        = "X-Ms-Client-Object-Id"
 	HeaderNameRequestID             = "X-Ms-Request-Id"
 	HeaderNameClientRequestID       = "X-Ms-Client-Request-Id"
 	HeaderNameCorrelationRequestID  = "X-Ms-Correlation-Request-Id"
