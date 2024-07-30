00d9677076e8f35ea5aefec8d9800128136dd2cc
Michael Shen <mshen@redhat.com>
2024-07-30 15:15:45 -0400

    Specify BASE_DOMAIN to fix the dev CS deployment
    The existing logic to list public zones in the provided resource group
    no longer works because both the long-lived dev environment and
    ephemeral environments create these zones in the same regional resource
    group. This commit does not fix the problem for ephemeral environments
    at this time, which still exists.
    
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- dev-infrastructure/configurations/dev.mk
+++ dev-infrastructure/configurations/dev.mk
@@ -4,3 +4,4 @@ REGIONAL_RESOURCEGROUP ?= aro-hcp-$(USER)-$(REGION)
 GLOBAL_RESOURCEGROUP ?= global
 ARO_HCP_IMAGE_ACR ?= arohcpdev
 REGIONAL_ACR_NAME ?= arohcpdev$(shell echo $(CURRENTUSER) | sha256sum  | head -c 24)
+BASE_DOMAIN ?= hcp.osadev.cloud
