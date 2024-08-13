165cf48f14bbefa9f65e3ebd5c5c5c274a19829a
Michael Shen <mshen@redhat.com>
2024-08-13 12:29:18 -0400

    Sync clusters-service last
    We're currently missing a pull secret and this will allow us to sync
    quay.io/pstefans/controlplaneoperator in the meantime
    
    Signed-off-by: Michael Shen <mshen@redhat.com>

--- dev-infrastructure/configurations/dev.mk
+++ dev-infrastructure/configurations/dev.mk
@@ -4,4 +4,4 @@ REGIONAL_RESOURCEGROUP ?= aro-hcp-$(USER)-$(REGION)
 GLOBAL_RESOURCEGROUP ?= global
 ARO_HCP_IMAGE_ACR ?= arohcpdev
 REGIONAL_ACR_NAME ?= arohcpdev$(shell echo $(CURRENTUSER) | sha256sum  | head -c 24)
-REPOSITORIES_TO_SYNC ?= '{registry.k8s.io/external-dns/external-dns,quay.io/acm-d/rhtap-hypershift-operator,quay.io/app-sre/uhc-clusters-service,quay.io/pstefans/controlplaneoperator}'
+REPOSITORIES_TO_SYNC ?= '{registry.k8s.io/external-dns/external-dns,quay.io/acm-d/rhtap-hypershift-operator,quay.io/pstefans/controlplaneoperator,quay.io/app-sre/uhc-clusters-service}'
