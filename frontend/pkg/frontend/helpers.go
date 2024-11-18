5fc2f15b0ce78c27ac2c73cbb43f0d9dbea34a6f
Matthew Barnes <mbarnes@fedorapeople.org>
2024-11-18 08:06:58 -0500

    frontend: Add more logic to DeleteResource
    Will be reusing DeleteResource for subscription deletion.
    
    Add database bookkeeping for the resource and any child resources.
    This includes creating implicit operations for each resource being
    deleted. The caller may then expose the returned operation ID.

--- frontend/pkg/frontend/helpers.go
+++ frontend/pkg/frontend/helpers.go
@@ -5,6 +5,7 @@ package frontend
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -69,39 +70,120 @@ func (f *Frontend) CheckForProvisioningStateConflict(ctx context.Context, operat
 	return nil
 }
 
-func (f *Frontend) DeleteResource(ctx context.Context, resourceID *arm.ResourceID) (*database.ResourceDocument, *arm.CloudError) {
-	doc, err := f.dbClient.GetResourceDoc(ctx, resourceID)
-	if err != nil {
-		if errors.Is(err, database.ErrNotFound) {
-			return nil, arm.NewResourceNotFoundError(resourceID)
-		} else {
-			f.logger.Error(err.Error())
-			return nil, arm.NewInternalServerError()
-		}
-	}
+func (f *Frontend) DeleteResource(ctx context.Context, resourceDoc *database.ResourceDocument) (string, *arm.CloudError) {
+	const operationRequest = database.OperationRequestDelete
+	var err error
 
-	switch doc.InternalID.Kind() {
+	switch resourceDoc.InternalID.Kind() {
 	case cmv1.ClusterKind:
-		err = f.clusterServiceClient.DeleteCSCluster(ctx, doc.InternalID)
+		err = f.clusterServiceClient.DeleteCSCluster(ctx, resourceDoc.InternalID)
 
 	case cmv1.NodePoolKind:
-		err = f.clusterServiceClient.DeleteCSNodePool(ctx, doc.InternalID)
+		err = f.clusterServiceClient.DeleteCSNodePool(ctx, resourceDoc.InternalID)
 
 	default:
-		f.logger.Error(fmt.Sprintf("unsupported Cluster Service path: %s", doc.InternalID))
-		return nil, arm.NewInternalServerError()
+		f.logger.Error(fmt.Sprintf("unsupported Cluster Service path: %s", resourceDoc.InternalID))
+		return "", arm.NewInternalServerError()
 	}
 
 	if err != nil {
 		var ocmError *ocmerrors.Error
 		if errors.As(err, &ocmError) && ocmError.Status() == http.StatusNotFound {
-			return nil, arm.NewResourceNotFoundError(resourceID)
+			return "", arm.NewResourceNotFoundError(resourceDoc.Key)
 		}
 		f.logger.Error(err.Error())
-		return nil, arm.NewInternalServerError()
+		return "", arm.NewInternalServerError()
 	}
 
-	return doc, nil
+	// Cluster Service will take care of canceling any ongoing operations
+	// on the resource or child resources, but we need to do some database
+	// bookkeeping to reflect that.
+
+	// FIXME This would be a good place to use Cosmos DB's transactional batch
+	//       operations to ensure all these write operations succeed together
+	//       or roll back. We would need two parallel transactions: one for
+	//       the Operations container and another for the Resources container.
+	//       But we're stymied currently by the DBClient interface, and I have
+	//       no desire to implement this in the in-memory cache. DBClient has
+	//       served us well up to this point, but I think it's time to bid it
+	//       farewell and switch to gomock in unit tests.
+
+	err = f.CancelActiveOperation(ctx, resourceDoc)
+	if err != nil {
+		f.logger.Error(err.Error())
+		return "", arm.NewInternalServerError()
+	}
+
+	operationDoc := database.NewOperationDocument(operationRequest, resourceDoc.Key, resourceDoc.InternalID)
+
+	err = f.dbClient.CreateOperationDoc(ctx, operationDoc)
+	if err != nil {
+		f.logger.Error(err.Error())
+		return "", arm.NewInternalServerError()
+	}
+
+	_, err = f.dbClient.UpdateResourceDoc(ctx, resourceDoc.Key, func(updateDoc *database.ResourceDocument) bool {
+		updateDoc.ActiveOperationID = operationDoc.ID
+		updateDoc.ProvisioningState = operationDoc.Status
+		return true
+	})
+	if err != nil {
+		f.logger.Error(err.Error())
+		return "", arm.NewInternalServerError()
+	}
+
+	iterator := f.dbClient.ListResourceDocs(ctx, resourceDoc.Key, -1, nil)
+
+	for item := range iterator.Items(ctx) {
+		// Anonymous function avoids repetitive error handling.
+		err = func() error {
+			var child database.ResourceDocument
+
+			err = json.Unmarshal(item, &child)
+			if err != nil {
+				return err
+			}
+
+			err = f.CancelActiveOperation(ctx, &child)
+			if err != nil {
+				return err
+			}
+
+			// This operation is not accessible through any REST endpoint.
+			// Its purpose is to cause the backend to delete the resource
+			// document once resource deletion completes.
+
+			childOperationDoc := database.NewOperationDocument(operationRequest, child.Key, child.InternalID)
+
+			err = f.dbClient.CreateOperationDoc(ctx, childOperationDoc)
+			if err != nil {
+				return err
+			}
+
+			_, err = f.dbClient.UpdateResourceDoc(ctx, child.Key, func(updateDoc *database.ResourceDocument) bool {
+				updateDoc.ActiveOperationID = childOperationDoc.ID
+				updateDoc.ProvisioningState = childOperationDoc.Status
+				return true
+			})
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}()
+		if err != nil {
+			f.logger.Error(err.Error())
+			return "", arm.NewInternalServerError()
+		}
+	}
+
+	err = iterator.GetError()
+	if err != nil {
+		f.logger.Error(err.Error())
+		return "", arm.NewInternalServerError()
+	}
+
+	return operationDoc.ID, nil
 }
 
 func (f *Frontend) MarshalResource(ctx context.Context, resourceID *arm.ResourceID, versionedInterface api.Version) ([]byte, *arm.CloudError) {
