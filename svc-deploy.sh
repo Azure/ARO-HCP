7b124b66fde5dfbd92c7de385d41c5b1703c8e30
Martin Bukatovič <mbukatov@redhat.com>
2024-12-03 00:46:47 +0100

    service deployment makefile cleanup
    User visible changes in Makefile targets deploying service components:
    
    - cs.deploy becomes cluster-service.deploy
    - rp.frontend.deploy becomes frontend.deploy
    - rp.backend.deploy becomes backend.deploy
    - hypershift.deploy becomes hypershiftoperator.deploy
    - maestro target no longer exists
    - deploy.svc.all becomes svc.deployall (the same goes with mgmt target)
    - deploy.all becomes just deployall
    
    So that:
    
    - all targets deploying single service ends with .deploy and use the
      service name matching the component directory name
    - all targets deploying all services ends with deployall

--- svc-deploy.sh
+++ svc-deploy.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
+# bash will report what is happening and stop in case of non zero return code
+set -xe
+
 # deploy a service to a cluster
 # ./svc-deploy <deploy-env> <dir> <cluster> [target]
 # this script expects the <dir> to contain a Makefile that takes care
 # of processing any config.mk template on its own
 
-cd $(dirname "$(realpath "${BASH_SOURCE[0]}")") || exit
+cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
 export DEPLOY_ENV=$1
 export DIR=$2
 export CLUSTER=$3
 export TARGET=${4:-deploy}
 
-if [[ "$CLUSTER" != "svc" && "$CLUSTER" != "mgmt" ]]; then
-    echo "Error: CLUSTER must be either 'svc' or 'mgmt'."
+if [[ "${CLUSTER}" != "svc" && "${CLUSTER}" != "mgmt" ]]; then
+    echo "Error: CLUSTER must be either 'svc' or 'mgmt'." >&2
     exit 1
 fi
 
-export KUBECONFIG=$(cd dev-infrastructure || exit ; make --no-print-directory $CLUSTER.aks.kubeconfigfile)
+cd dev-infrastructure
+KUBECONFIG=$(make --no-print-directory "${CLUSTER}.aks.kubeconfigfile")
+export KUBECONFIG
+cd -
 
-cd ${DIR} || exit
-make $TARGET
+cd "${DIR}"
+make "${TARGET}"
