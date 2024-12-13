e18b738ad6a6d4f60d880694c7e8baf2d7d21271
Jonathan Chang <jonachang@microsoft.com>
2024-12-13 09:14:38 -0800

    use verified code

--- dev-infrastructure/scripts/istio.sh
+++ dev-infrastructure/scripts/istio.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 echo "********** ISTIO Upgrade Started **************"
 # Get the current istio and check if it match target version
-export CURRENTVERSION=$(kubectl get pods -n aks-istio-system -o custom-columns="NAME:.metadata.name" | grep istiod | awk -F'-' '{print $2"-"$3"-"$4}' | uniq)
+export CURRENTVERSION=$(kubectl get pods -n aks-istio-system | tail -n +2 | tail -n 1 | cut -d '-' -f 2,3,4)
 if [ "$CURRENTVERSION" == "$TARGET_VERSION" ]; then
     echo "Istio is using Target Version. Exiting script."
     exit 0
