c8e3587d6ce8657720d33c2fc58e6412a22aa166
Gerd Oberlechner <goberlec@redhat.com>
2024-12-18 13:17:44 +0100

    * OS and arch aware istioctl download * ensure tag exists - if not set it to the current version
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- dev-infrastructure/scripts/istio.sh
+++ dev-infrastructure/scripts/istio.sh
@@ -1,8 +1,74 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
-echo "********** ISTIO Upgrade Started **************"
+echo "********** Download istioctl **************"
+# Determines the operating system.
+OS="${TARGET_OS:-$(uname)}"
+if [ "${OS}" = "Darwin" ] ; then
+  OSEXT="osx"
+else
+  OSEXT="linux"
+fi
+# Determine arch
+LOCAL_ARCH=$(uname -m)
+case "${LOCAL_ARCH}" in
+  x86_64|amd64)
+    ISTIO_ARCH=amd64
+    ;;
+  armv8*|aarch64*|arm64)
+    ISTIO_ARCH=arm64
+    ;;
+  armv*)
+    ISTIO_ARCH=armv7
+    ;;
+  *)
+    echo "This system's architecture, ${LOCAL_ARCH}, isn't supported"
+    exit 1
+    ;;
+esac
+
+
+ISTIO_URL="${ISTIOCTL_URL}/${ISTIOCTL_VERSION}/istio-${ISTIOCTL_VERSION}-${OSEXT}-${ISTIO_ARCH}.tar.gz"
+SHA256_URL="${ISTIOCTL_URL}/${ISTIOCTL_VERSION}/istio-${ISTIOCTL_VERSION}-${OSEXT}-${ISTIO_ARCH}.tar.gz.sha256"
+# Download the Istioctl binary
+wget -q "$ISTIO_URL" -O istio-"${ISTIOCTL_VERSION}"-${OSEXT}-${ISTIO_ARCH}.tar.gz
+
+# Download the SHA-256 checksum file
+wget -q "$SHA256_URL" -O istio-"${ISTIOCTL_VERSION}"-${OSEXT}-${ISTIO_ARCH}.tar.gz.sha256
+
+# Verify the downloaded file
+sha256sum -c istio-"${ISTIOCTL_VERSION}"-${OSEXT}-${ISTIO_ARCH}.tar.gz.sha256
+
+# Check the result of the verification
+if sha256sum -c istio-"${ISTIOCTL_VERSION}"-${OSEXT}-${ISTIO_ARCH}.tar.gz.sha256; then
+    echo "Verification successful: The file is intact."
+else
+    echo "Verification failed: The file is corrupted."
+    exit 1
+fi
+
+tar -xzf istio-"${ISTIOCTL_VERSION}"-${OSEXT}-${ISTIO_ARCH}.tar.gz
+cd istio-"${ISTIOCTL_VERSION}"
+export PATH=$PWD/bin:$PATH
+echo "=========================================================================="
+
+#
+# Create the tag if it does not exist yet
+#
+
+ISTIO_NAMESPACE="aks-istio-system"
+CURRENT_TAG_REVISION=$(istioctl tag list --istioNamespace ${ISTIO_NAMESPACE} -o json | jq --arg tag ${TAG} '.[] | select(.tag == $tag).revision' -r)
+
+echo "********** Ensure tag ${TAG} exists **************"
+if [ -z "$CURRENT_TAG_REVISION" ]; then
+    echo "Tag ${TAG} does not exist yet. Creating it with version ${CURRENT_VERSION}"
+    istioctl tag set ${TAG} --revision ${CURRENT_VERSION} --istioNamespace ${ISTIO_NAMESPACE}
+else
+    echo "Tag ${TAG} already exists and refers to version ${CURRENT_TAG_REVISION}"
+fi
+
+echo "********** ISTIO Upgrade **************"
 # Followed this guide for istio upgrade https://learn.microsoft.com/en-us/azure/aks/istio-upgrade
 # To upgrade or rollback, change the targetVersion to the desire version, and version to the current version.
 if [[ -z "$TARGET_VERSION" ]]; then
@@ -10,35 +76,10 @@ if [[ -z "$TARGET_VERSION" ]]; then
     exit 0
 fi
 
-echo "********** Download istioctl**************"
-ISTIO_URL="${ISTIOCTL_URL}/${ISTIOCTL_VERSION}/istio-${ISTIOCTL_VERSION}-linux-amd64.tar.gz"
-SHA256_URL="${ISTIOCTL_URL}/${ISTIOCTL_VERSION}/istio-${ISTIOCTL_VERSION}-linux-amd64.tar.gz.sha256"
-# Download the Istioctl binary
-wget "$ISTIO_URL" -O istio-"${ISTIOCTL_VERSION}"-linux-amd64.tar.gz
-
-# Download the SHA-256 checksum file
-wget "$SHA256_URL" -O istio-"${ISTIOCTL_VERSION}"-linux-amd64.tar.gz.sha256
-
-# Verify the downloaded file
-sha256sum -c istio-"${ISTIOCTL_VERSION}"-linux-amd64.tar.gz.sha256
-
-# Check the result of the verification
-if sha256sum -c istio-"${ISTIOCTL_VERSION}"-linux-amd64.tar.gz.sha256; then
-    echo "Verification successful: The file is intact."
-else
-    echo "Verification failed: The file is corrupted."
-    exit 1
-fi
-
-tar -xzf istio-"${ISTIOCTL_VERSION}"-linux-amd64.tar.gz
-cd istio-"${ISTIOCTL_VERSION}"
-export PATH=$PWD/bin:$PATH
-echo "=========================================================================="
-
 NEWVERSION="$TARGET_VERSION"
-echo "********** Istio UpGrade Started with version ${NEWVERSION} **************"
+echo "********** Istio Upgrade Started with version ${NEWVERSION} **************"
 
-istioctl tag set "$TAG" --revision "${NEWVERSION}" --istioNamespace aks-istio-system --overwrite
+istioctl tag set "$TAG" --revision "${NEWVERSION}" --istioNamespace ${ISTIO_NAMESPACE} --overwrite
 # Get the namespaces with the label istio.io/rev=$TAG
 namespaces=$(kubectl get namespaces --selector=istio.io/rev="$TAG" -o jsonpath='{.items[*].metadata.name}' | xargs -n1 echo)
 
