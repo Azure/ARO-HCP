6ee74d55d4c5ca8a25e37114e4a57c9d02b1d1c0
Gerd Oberlechner <goberlec@redhat.com>
2025-01-15 09:29:57 +0100

    fix MI deletion on cluster deletion
    the while read logic skipped the last element in the MI list so it was not deleted.
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- demo/05-delete-cluster.sh
+++ demo/05-delete-cluster.sh
@@ -13,22 +13,22 @@ delete_managed_identities_from_cluster() {
   UAMIS_JSON_MAP=$(echo ${EXISTING_CLUSTER_PAYLOAD} | jq '.properties.spec.platform.operatorsAuthentication.userAssignedIdentities')
 
   CP_UAMIS_ENTRIES=$(echo ${UAMIS_JSON_MAP}| jq -c '.controlPlaneOperators | to_entries | .[]')
-  echo -n "${CP_UAMIS_ENTRIES}" | while read cp_uami_entry; do
+  while read cp_uami_entry; do
     cp_operator_name=$(echo -n "${cp_uami_entry}" | jq .key)
     cp_operator_mi=$(echo -n "${cp_uami_entry}" | jq .value -r)
     echo "deleting $cp_operator_name operator's managed identity $cp_operator_mi"
     az identity delete --ids $cp_operator_mi
     echo "deleted managed identity $cp_operator_mi"
-  done
+  done <<< "${CP_UAMIS_ENTRIES}"
 
   DP_UAMIS_ENTRIES=$(echo ${UAMIS_JSON_MAP} | jq -c '.dataPlaneOperators | to_entries | .[]')
-  echo -n "${DP_UAMIS_ENTRIES}" | while read dp_uami_entry; do
+  while read dp_uami_entry; do
     dp_operator_name=$(echo -n "${dp_uami_entry}" | jq .key)
     dp_operator_mi=$(echo -n "${dp_uami_entry}" | jq .value -r)
     echo "deleting $dp_operator_name operator's managed identity $dp_operator_mi"
     az identity delete --ids $dp_operator_mi
     echo "deleted managed identity $dp_operator_mi"
-  done
+  done <<< "${DP_UAMIS_ENTRIES}"
 
   SMI_UAMI_ENTRY=$(echo ${UAMIS_JSON_MAP} | jq .serviceManagedIdentity -r)
   echo "deleting service managed identity ${SMI_UAMI_ENTRY}"
