fd33fa6506282b5d33d59e450e52e331babad42d
Matthew Barnes <mbarnes@fedorapeople.org>
2025-01-20 11:12:12 -0500

    api: Add validation struct tags to ManagedServiceIdentity

--- internal/api/arm/identity.go
+++ internal/api/arm/identity.go
@@ -5,16 +5,16 @@ package arm
 
 // Represents to support the ManagedServiceIdentity ARM resource.
 type ManagedServiceIdentity struct {
-	PrincipalID            string                           `json:"principalId,omitempty"`
-	TenantID               string                           `json:"tenantId,omitempty"`
-	Type                   ManagedServiceIdentityType       `json:"type"`
-	UserAssignedIdentities map[string]*UserAssignedIdentity `json:"userAssignedIdentities,omitempty"`
+	PrincipalID            string                           `json:"principalId,omitempty"            visibility:"read"`
+	TenantID               string                           `json:"tenantId,omitempty"               visibility:"read"`
+	Type                   ManagedServiceIdentityType       `json:"type"                                               validate:"omitempty,enum_managedserviceidentitytype"`
+	UserAssignedIdentities map[string]*UserAssignedIdentity `json:"userAssignedIdentities,omitempty"                   validate:"dive,keys,resource_id=Microsoft.ManagedIdentity/userAssignedIdentities,endkeys"`
 }
 
 // UserAssignedIdentity - User assigned identity properties https://azure.github.io/typespec-azure/docs/libraries/azure-resource-manager/reference/data-types/#Azure.ResourceManager.CommonTypes.UserAssignedIdentity
 type UserAssignedIdentity struct {
-	ClientID    *string
-	PrincipalID *string
+	ClientID    *string `json:"clientId,omitempty"    visibility:"read"`
+	PrincipalID *string `json:"principalId,omitempty" visibility:"read"`
 }
 
 type ManagedServiceIdentityType string
