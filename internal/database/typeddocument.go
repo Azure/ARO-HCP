51901862c7da602f57052d96feb3ba06bc59cad1
Matthew Barnes <mbarnes@fedorapeople.org>
2025-01-22 11:47:13 -0500

    database: Add typedDocument extension to baseDocument
    This is a major breaking change to our Cosmos DB documents.
    
    typedDocument standardizes the structure of documents as follows:
    
      {
        "id":           <item-id>
        "partitionKey": <partition-key>
        "resourceType": <azure-resource-type>
        "properties": {
          ...
        }
        "_rid":  <system-generated>
        "_self": <system-generated>
        "_etag": <system-generated>
        "_ts":   <system-generated>
      }
    
    A key insight is none of the system-generated fields are used outside
    of the database package. Therefore the data that the frontend/backend
    components care about can be encapsulated in a "properties" value,
    and the other document types no longer need to embed baseDocument.
    
    DocumentProperties is an interface for types that can be plugged
    into the "properties" field, like ResourceDocument. It requires a
    GetValidTypes() method that returns a set of valid values for the
    "resourceType" field, used for validation when [un]marshalling.
    
    One case worth noting is SubscriptionDocument was mostly already
    following this format, with its "subscription" field defined as a
    arm.Subscription struct. Instead, arm.Subscription now implements
    the DocumentProperties interface and SubscriptionDocument is gone.
    
    Also worth mentioning is DBClientIterator has been redefined as a
    iter.Seq2 instead of a iter.Seq. Effectively this means the Items
    method now returns the Cosmos DB item ID separately from the item
    (document) itself.
    
    To illustrate:
    
        old style:  for doc := range iterator.Items()
        new style:  for id, doc := range iterator.Items()
    
    Apologies for the size of this commit, but the changes here are
    all intertwined.

---
+++ internal/database/typeddocument.go
@@ -0,0 +1,120 @@
+package database
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the Apache License 2.0.
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+
+	azcorearm "github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos"
+)
+
+// typedDocumentError signifies a mismatched Type field and Properties type
+// when attempting to unmarshal JSON-encoded data.
+type typedDocumentError struct {
+	invalidType    string
+	propertiesType string
+}
+
+func (e typedDocumentError) Error() string {
+	if e.invalidType == "" {
+		return "missing type"
+	}
+
+	return fmt.Sprintf("invalid type '%s' for %s", e.invalidType, e.propertiesType)
+}
+
+// typedDocument is a baseDocument with a ResourceType field to
+// help distinguish heterogeneous items in a Cosmos DB container.
+// The Properties field can be unmarshalled to any type that
+// implements the DocumentProperties interface.
+type typedDocument struct {
+	baseDocument
+	PartitionKey string          `json:"partitionKey"`
+	ResourceType string          `json:"resourceType"`
+	Properties   json.RawMessage `json:"properties"`
+}
+
+// newTypedDocument returns a TypedDocument from a ResourceType.
+func newTypedDocument(partitionKey string, resourceType azcorearm.ResourceType) *typedDocument {
+	return &typedDocument{
+		baseDocument: newBaseDocument(),
+		PartitionKey: strings.ToLower(partitionKey),
+		ResourceType: strings.ToLower(resourceType.String()),
+	}
+}
+
+// getPartitionKey returns an azcosmos.PartitionKey.
+func (td *typedDocument) getPartitionKey() azcosmos.PartitionKey {
+	return azcosmos.NewPartitionKeyString(td.PartitionKey)
+}
+
+// validateType validates the type field against the given properties type.
+// If type validation fails, validateType returns a typedDocumentError.
+func (td *typedDocument) validateType(properties DocumentProperties) error {
+	for _, t := range properties.GetValidTypes() {
+		if strings.EqualFold(td.ResourceType, t) {
+			return nil
+		}
+	}
+
+	propertiesType := reflect.TypeOf(properties)
+	if propertiesType.Kind() == reflect.Pointer {
+		propertiesType = propertiesType.Elem()
+	}
+
+	return &typedDocumentError{
+		invalidType:    td.ResourceType,
+		propertiesType: propertiesType.Name(),
+	}
+}
+
+// typedDocumentMarshal returns the JSON encoding of typedDoc with innerDoc
+// as the properties value. First, however, typedDocumentMarshal validates
+// the type field in typeDoc against innerDoc to ensure compatibility. If
+// validation fails, typedDocumentMarshal returns a typedDocumentError.
+func typedDocumentMarshal[T DocumentProperties](typedDoc *typedDocument, innerDoc *T) ([]byte, error) {
+	err := typedDoc.validateType(*innerDoc)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := json.Marshal(innerDoc)
+	if err != nil {
+		return nil, err
+	}
+
+	typedDoc.Properties = data
+
+	return json.Marshal(typedDoc)
+}
+
+// typedDocumentUnmarshal parses JSON-encoded data into a typedDocument,
+// validates the type field against the type parameter T, and then parses
+// the JSON-encoded properties data into an instance of type parameter T.
+// If validation fails, typedDocumentUnmarshal returns a typedDocumentError.
+func typedDocumentUnmarshal[T DocumentProperties](data []byte) (*typedDocument, *T, error) {
+	var typedDoc typedDocument
+	var innerDoc T
+
+	err := json.Unmarshal(data, &typedDoc)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = typedDoc.validateType(innerDoc)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = json.Unmarshal(typedDoc.Properties, &innerDoc)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &typedDoc, &innerDoc, nil
+}
