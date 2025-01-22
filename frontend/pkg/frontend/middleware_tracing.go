a4445fcd5424c00375e995690660b7e23dd8df72
Benedikt Bongartz <bongartz@klimlive.de>
2025-01-22 14:23:32 +0100

    frontend: set correlationID on root span
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

--- frontend/pkg/frontend/middleware_tracing.go
+++ frontend/pkg/frontend/middleware_tracing.go
@@ -7,8 +7,13 @@ import (
 	"net/http"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func MiddlewareTracing(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	otelhttp.NewHandler(http.Handler(next), r.URL.Path).ServeHTTP(w, r)
+	trace.SpanFromContext(r.Context()).SetAttributes(
+		attribute.String("correlationID", r.Header.Get("x-ms-correlation-request-id")),
+	)
 }
