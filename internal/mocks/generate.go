67839017891c8f333d8bc5f9707b236984ab0c5b
Matthew Barnes <mbarnes@fedorapeople.org>
2025-02-04 11:35:32 -0500

    database: Add DBTransaction and DBTransactionResult interfaces
    Wraps transactional batch operations for Cosmos DB.

--- internal/mocks/generate.go
+++ internal/mocks/generate.go
@@ -15,4 +15,5 @@
 package mocks
 
 //go:generate $MOCKGEN -typed -source=../database/database.go -destination=dbclient.go -package mocks github.com/Azure/ARO-HCP/internal/database DBClient
+//go:generate $MOCKGEN -typed -source=../database/transaction.go -destination=dbtransaction.go -package mocks github.com/Azure/ARO-HCP/internal/database DBTransaction DBTransactionResult
 //go:generate $MOCKGEN -typed -source=../ocm/ocm.go -destination=ocm.go -package mocks github.com/Azure/ARO-HCP/internal/ocm ClusterServiceClientSpec
