6a97d796a0b2183c3b43443b70e5e3b30851ce03
Jan-Hendrik Boll <jboll@redhat.com>
2025-02-05 07:55:18 +0100

    Fix missing image bug. (#1236)
    Add script to resolve the latest available tag

---
+++ get-tag.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -x 
+if [ "$#" -ne 2 ]
+then
+    echo "Need ARO_HCP_IMAGE_ACR and REPOSITORY parameters"
+    exit 1
+fi
+
+aro_hcp_image_acr=${1}
+repository=${2}
+
+if [ -n "${COMMIT_OVERRIDE}" ];
+then
+    echo ${COMMIT_OVERRIDE}
+    exit 0
+fi
+
+if [ -n "${COMMIT}" ];
+then
+    echo ${COMMIT}
+    exit 0
+fi
+
+
+tags=$(mktemp)
+trap "rm ${tags}" EXIT
+
+az acr repository show-tags --orderby time_desc --n ${aro_hcp_image_acr} --repository ${repository} > $tags
+
+suggested_tag=$(grep $(git rev-parse --short=7 HEAD) $tags |cut -d '"' -f2)
+if [ -n "${suggested_tag}" ];
+then
+    echo ${suggested_tag}
+    exit 0
+fi
+
+grep '"' $tags | head -1 | cut -d '"' -f2
