106e2f5a3d90740d0dcf30e8d9ca126ad2c3e794
Jean-Francois Chevrette <jfchevrette@gmail.com>
2025-02-07 09:51:21 -0500

    refactor AZ script and helper func for proper AZ selection
    ... to account for regions with varied number of AZs and the eventuality
    that AZ names could be something other than 1,2,3

--- dev-infrastructure/scripts/list-az-regions.sh
+++ dev-infrastructure/scripts/list-az-regions.sh
@@ -7,12 +7,12 @@ az rest \
   --method get \
   --uri "/subscriptions/$(az account show --query id --output tsv)/locations?api-version=2024-11-01" \
   | jq -r '
-    # First, print items with availabilityZoneMappings set
-    "Regions with Availability Zones:",
-    (.value[] | select(.availabilityZoneMappings) | .name),
-    "",
-    # Then, print items without availabilityZoneMappings
-    "Regions without Availability Zones:",
-    (.value[] | select(.availabilityZoneMappings | not) | .name)
-  '
-
+    reduce .value[] as $item ({}; .[$item.name] = {
+      availabilityZones: (
+        if $item.availabilityZoneMappings then
+          $item.availabilityZoneMappings | map(.logicalZone)
+        else
+          []
+        end
+      )
+    }) | to_entries | sort_by(.key) | from_entries'
