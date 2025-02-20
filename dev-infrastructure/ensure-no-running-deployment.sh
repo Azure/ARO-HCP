0cbcfae1e455dd0716f22582c8c04f48c4027844
Jan-Hendrik Boll <jboll@redhat.com>
2025-02-20 14:54:26 +0100

    Remove ensure-no-running-deployment.sh
    This has been moved into templatize https://github.com/Azure/ARO-HCP/pull/1146

--- dev-infrastructure/ensure-no-running-deployment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-RESOURCEGROUP_NAME=$1
-DEPLOYMENT_NAME=$2
-
-az deployment group wait --name "${DEPLOYMENT_NAME}" --resource-group "${RESOURCEGROUP_NAME}" --created --updated --deleted --interval 10 || exit 0
