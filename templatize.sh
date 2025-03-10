b66f5bfe4011d786a4eb5ea30f62bb8cec8f60a6
Gerd Oberlechner <goberlec@redhat.com>
2025-03-10 12:49:31 +0100

    MSFT INT config (#1479)
    * make sure int resources have an `int` suffix part
    * fix templatlize.sh script to show region in materialized configuration
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- templatize.sh
+++ templatize.sh
@@ -121,9 +121,11 @@ if [ "$DEPLOY_ENV" == "personal-dev" ]; then
     REGION_STAMP="${REGION_SHORT}${USER:0:4}"
 elif [ "$DEPLOY_ENV" == "personal-perfscale" ]; then
     REGION_STAMP="${REGION_SHORT}p${USER:0:4}"
-else
+elif [ "$DEPLOY_ENV" == "dev" ] || [ "$DEPLOY_ENV" == "cs-pr" ]; then
     CLEAN_DEPLOY_ENV=$(echo "${DEPLOY_ENV}" | tr -cd '[:alnum:]')
     REGION_STAMP="${CLEAN_DEPLOY_ENV}"
+else
+    REGION_STAMP=${REGION_SHORT}
 fi
 
 make -s -C ${PROJECT_ROOT_DIR}/tooling/templatize templatize
