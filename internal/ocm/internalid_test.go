e845a00ebf4ee7b5250e9585247ef67b0c305b21
Matthew Barnes <mbarnes@fedorapeople.org>
2025-03-12 10:13:59 -0400

    ocm: Refactor unit tests
    Use github.com/stretchr/testify/assert (and /require).

--- internal/ocm/internalid_test.go
+++ internal/ocm/internalid_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 
 	cmv1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
+	"github.com/stretchr/testify/assert"
 )
 
 type FakeTransport struct{}
@@ -58,17 +59,17 @@ func TestInternalID(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			internalID, err := NewInternalID(tt.path)
-			if err != nil && !tt.expectErr || err == nil && tt.expectErr {
-				t.Error(err)
+			if tt.expectErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
 			}
 
 			transport := &FakeTransport{}
-			if _, ok := internalID.GetClusterClient(transport); ok == tt.expectErr {
-				t.Errorf("failed to get cluster client")
-			}
-			if _, ok := internalID.GetAroHCPClusterClient(transport); ok == tt.expectErr {
-				t.Errorf("failed to get cluster client")
-			}
+			_, ok := internalID.GetClusterClient(transport)
+			assert.NotEqual(t, tt.expectErr, ok)
+			_, ok = internalID.GetAroHCPClusterClient(transport)
+			assert.NotEqual(t, tt.expectErr, ok)
 
 			if tt.expectErr {
 				// test ends here if error is expected
@@ -76,34 +77,24 @@ func TestInternalID(t *testing.T) {
 			}
 
 			id := internalID.ID()
-			if id != tt.id {
-				t.Errorf("expected id '%s', got '%s'", tt.id, id)
-			}
+			assert.Equal(t, tt.id, id)
 
 			kind := internalID.Kind()
-			if kind != tt.kind {
-				t.Errorf("expected kind '%s', got '%s'", tt.kind, kind)
-			}
+			assert.Equal(t, tt.kind, kind)
 
 			str := internalID.String()
-			if str != tt.path {
-				t.Errorf("expected string '%s', got '%s'", tt.path, str)
-			}
+			assert.Equal(t, tt.path, str)
 
 			if kind == cmv1.NodePoolKind {
-				if _, ok := internalID.GetNodePoolClient(transport); !ok {
-					t.Errorf("failed to get node pool client")
-				}
+				_, ok := internalID.GetNodePoolClient(transport)
+				assert.True(t, ok, "failed to get node pool client")
 			}
 
 			bytes, err := json.Marshal(internalID)
-			if err != nil {
-				t.Error(err)
-			}
-			fmt.Printf("Bytes: %s\n", bytes)
-			err = json.Unmarshal(bytes, &internalID)
-			if err != nil {
-				t.Error(err)
+			if assert.NoError(t, err) {
+				fmt.Printf("Bytes: %s\n", bytes)
+				err = json.Unmarshal(bytes, &internalID)
+				assert.NoError(t, err)
 			}
 		})
 	}
