013673b9d4d29960bce3979ff20cc11604dd9f13
Matthew Barnes <mbarnes@fedorapeople.org>
2025-03-19 07:43:52 -0400

    api: Struct tag alignment

--- internal/api/hcpopenshiftclusternodepool.go
+++ internal/api/hcpopenshiftclusternodepool.go
@@ -18,20 +18,20 @@ type HCPOpenShiftClusterNodePool struct {
 // HCPOpenShiftClusterNodePool resource.
 type HCPOpenShiftClusterNodePoolProperties struct {
 	ProvisioningState arm.ProvisioningState   `json:"provisioningState,omitempty" visibility:"read"`
-	Version           VersionProfile          `json:"version,omitempty" visibility:"read create"`
-	Platform          NodePoolPlatformProfile `json:"platform,omitempty" visibility:"read create"`
-	Replicas          int32                   `json:"replicas,omitempty" visibility:"read create update" validate:"min=0,excluded_with=AutoScaling"`
-	AutoRepair        bool                    `json:"autoRepair,omitempty" visibility:"read create"`
-	AutoScaling       *NodePoolAutoScaling    `json:"autoScaling,omitempty" visibility:"read create update"`
-	Labels            map[string]string       `json:"labels,omitempty" visibility:"read create update"`
-	Taints            []*Taint                `json:"taints,omitempty" visibility:"read create update"   validate:"dive"`
+	Version           VersionProfile          `json:"version,omitempty"           visibility:"read create"`
+	Platform          NodePoolPlatformProfile `json:"platform,omitempty"          visibility:"read create"`
+	Replicas          int32                   `json:"replicas,omitempty"          visibility:"read create update" validate:"min=0,excluded_with=AutoScaling"`
+	AutoRepair        bool                    `json:"autoRepair,omitempty"        visibility:"read create"`
+	AutoScaling       *NodePoolAutoScaling    `json:"autoScaling,omitempty"       visibility:"read create update"`
+	Labels            map[string]string       `json:"labels,omitempty"            visibility:"read create update"`
+	Taints            []*Taint                `json:"taints,omitempty"            visibility:"read create update" validate:"dive"`
 }
 
 // NodePoolPlatformProfile represents a worker node pool configuration.
 // Visibility for the entire struct is "read create".
 type NodePoolPlatformProfile struct {
 	SubnetID               string `json:"subnetId,omitempty"`
-	VMSize                 string `json:"vmSize,omitempty" validate:"required_for_put"`
+	VMSize                 string `json:"vmSize,omitempty"                 validate:"required_for_put"`
 	DiskSizeGiB            int32  `json:"diskSizeGiB,omitempty"`
 	DiskStorageAccountType string `json:"diskStorageAccountType,omitempty"`
 	AvailabilityZone       string `json:"availabilityZone,omitempty"`
@@ -49,7 +49,7 @@ type NodePoolAutoScaling struct {
 
 type Taint struct {
 	Effect Effect `json:"effect,omitempty" validate:"required_for_put,enum_effect"`
-	Key    string `json:"key,omitempty" validate:"required_for_put"`
+	Key    string `json:"key,omitempty"    validate:"required_for_put"`
 	Value  string `json:"value,omitempty"`
 }
 
