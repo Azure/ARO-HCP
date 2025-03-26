d9c14d6ca0d80c4f67026170348e9bf02e296ca9
Gerd Oberlechner <goberlec@redhat.com>
2025-03-26 01:03:08 +0100

    build and push imagesync in pipeline
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- tooling/image-sync/config.tmpl.mk
+++
@@ -1,3 +0,0 @@
-ARO_HCP_IMAGE_ACR ?= {{ .acr.svc.name }}
-ARO_HCP_BASE_IMAGE ?= ${ARO_HCP_IMAGE_ACR}.azurecr.io
-ARO_HCP_IMAGE_SYNC_IMAGE ?= $(ARO_HCP_BASE_IMAGE)/{{ .imageSync.componentSync.image.repository }}
