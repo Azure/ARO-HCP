e0c4e6029684d7a4c1c84448270207f5bb8fa2de
Gerd Oberlechner <goberlec@redhat.com>
2025-03-31 11:12:50 +0200

    remove check for magic dashboard folderUid string
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- grafana/deploy.sh
+++ grafana/deploy.sh
@@ -4,7 +4,7 @@
 # TODO:
 #  - check if dashboard really needs update (will cause many versions in grafana db)
 #  - check if stale folder needs to be deleted
-#  - check if stale dashboard needs to be deleted 
+#  - check if stale dashboard needs to be deleted
 
 set -euo pipefail
 
@@ -19,36 +19,31 @@ fi
 
 cd dashboards
 
-existing_folders=$(az grafana folder list -g ${RESOURCEGROUP}  -n ${GRAFANA_NAME})
+existing_folders=$(az grafana folder list -g "${RESOURCEGROUP}"  -n "${GRAFANA_NAME}")
 
 for d in $(find . -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
 do
-    if [[ $(echo $existing_folders | jq --arg TITLE "$d"  -c '.[] | select( .title == $TITLE )' |wc -l )  -gt 0 ]];
+    if [[ $(echo "${existing_folders}" | jq --arg TITLE "$d"  -c '.[] | select( .title == $TITLE )' |wc -l )  -gt 0 ]];
     then
-        folderUid=$(echo $existing_folders | jq --arg TITLE "$d"  -r '.[] | select( .title == $TITLE ) | .uid' )
+        folderUid=$(echo "${existing_folders}" | jq --arg TITLE "$d"  -r '.[] | select( .title == $TITLE ) | .uid' )
     else
         if [[ ${DRY_RUN} == "true" ]];
         then
             echo "would create folder '${d}' on managed grafana ${GRAFANA_NAME} in rg ${RESOURCEGROUP}"
         else
-            folderUid=$(az grafana folder create --only-show-errors  -g ${RESOURCEGROUP}  -n ${GRAFANA_NAME} --title ${d} |jq -r '.uid')
+            folderUid=$(az grafana folder create --only-show-errors  -g "${RESOURCEGROUP}"  -n "${GRAFANA_NAME}" --title "${d}" |jq -r '.uid')
         fi
     fi
-    pushd ${d}
+    pushd "${d}"
     IFS=$'\n'; for dashboard in $(ls -1)
     do
-        dashboard_name=$(cat ${dashboard} | jq '.dashboard.title' )
+        dashboard_name=$(cat "${dashboard}" | jq '.dashboard.title' )
         if [[ ${DRY_RUN} == "true" ]];
         then
             echo "would create/update dashboard '${dashboard_name}' on managed grafana ${GRAFANA_NAME} in rg ${RESOURCEGROUP}"
         else
-            if [[ $(grep -c XYZTOBESETBYPIPELINEZYX ${dashboard}) -ne 1 ]];
-            then
-                echo "Magic string XYZTOBESETBYPIPELINEZYX not found in dashboard file ${dashboard}" >&2
-            else
-                yq -i ".folderUid = \"${folderUid}\"" ${dashboard}
-                az grafana dashboard update --overwrite true -g ${RESOURCEGROUP}  -n ${GRAFANA_NAME} --definition ${dashboard}
-            fi
+            yq -i ".folderUid = \"${folderUid}\"" "${dashboard}"
+            az grafana dashboard update --overwrite true -g "${RESOURCEGROUP}"  -n "${GRAFANA_NAME}" --definition "${dashboard}"
         fi
     done
     popd
