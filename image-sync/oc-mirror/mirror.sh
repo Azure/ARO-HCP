839b71349db835bfbcba39a5b57dcb33f5f00422
Gerd Oberlechner <goberlec@redhat.com>
2025-04-01 09:32:23 +0200

    oc-mirror image lifecycle and enhancements (#1576)
    * use newer azure-cli image from MCR
    * ... and pin it by digest
    * shell script hygiene
    * update `yq`
    * add DNS diagnostics for target ACR (validate privatelink setup)
    * use `--client-id` instead of `-u` for `az login` to mitigate deprecation warning
    * fix a naming bug in the mirror jobs (remove `-tmp` suffix)
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- image-sync/oc-mirror/mirror.sh
+++ image-sync/oc-mirror/mirror.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
-az login --identity -u ${AZURE_CLIENT_ID}
-DOCKER_COMMAND=/usr/local/bin/docker-login.sh az acr login -n ${REGISTRY}
+echo "Azure login"
+az login --identity --client-id "${AZURE_CLIENT_ID}"
 
+echo "ACR login"
+DOCKER_COMMAND=/usr/local/bin/docker-login.sh az acr login -n "${REGISTRY}"
+
+# Prepare configuration
 IMAGE_SET_CONFIG_FILE="/config/imageset-config.yaml"
-echo ${IMAGE_SET_CONFIG} | base64 -d | yq eval -P > ${IMAGE_SET_CONFIG_FILE}
+echo "${IMAGE_SET_CONFIG}" | base64 -d | yq eval -P > ${IMAGE_SET_CONFIG_FILE}
 API_VERSION=$(yq eval '.apiVersion' ${IMAGE_SET_CONFIG_FILE})
-
 if echo "$API_VERSION" | grep -q "^mirror.openshift.io/v2"; then
     ADDITIONAL_FLAGS="--workspace file:///oc-mirror-workspace --v2"
 else
     ADDITIONAL_FLAGS="--continue-on-error"
 fi
 
+# switching between versions of oc-mirror is a temporary fix until
+# all oc-mirror related problems have been resolved
+# * https://issues.redhat.com/browse/OCPBUGS-54340 - storage issue
+# * https://issues.redhat.com/browse/CLID-325 - CPU bug
+# * https://issues.redhat.com/browse/OCPBUGS-52471 - memory bug
 if [ "$OC_MIRROR_COMPATIBILITY" = "NOCATALOG" ]; then
     export OC_MIRROR_VERSION="4.16"
 else
@@ -19,4 +27,8 @@ else
 fi
 echo "Using oc-mirror version: ${OC_MIRROR_VERSION}"
 
+echo "Inspecting DNS for target registry"
+dig "${REGISTRY_URL}"
+
+echo "Start mirroring"
 /usr/local/bin/oc-mirror-${OC_MIRROR_VERSION} --config ${IMAGE_SET_CONFIG_FILE} ${ADDITIONAL_FLAGS} docker://${REGISTRY_URL} @$
