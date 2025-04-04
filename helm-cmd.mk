8c8b15e50813d7cf3469f5a27c7070fa72c8face
Gerd Oberlechner <goberlec@redhat.com>
2025-04-04 11:52:31 +0200

    fix helm rollout (#1593)
    keep the default helm rollout behaviour or services that have not yet adapted for the diagnostics script
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- helm-cmd.mk
+++ helm-cmd.mk
@@ -4,5 +4,5 @@ PROJECT_ROOT_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 ifdef DRY_RUN
 HELM_CMD ?= helm diff upgrade --install --dry-run=server --suppress-secrets --three-way-merge
 else
-HELM_CMD ?= ${PROJECT_ROOT_DIR}/hack/helm.sh
+HELM_CMD ?= helm upgrade --install --wait --wait-for-jobs
 endif
