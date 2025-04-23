c685006887c1bc12016ed35641102c33a6c6128f
Simon Pasquier <spasquie@redhat.com>
2025-04-23 17:46:47 +0200

    Fix commit SHA version in backend/frontend images
    This change ensures that the version reported by the backend and the
    frontend is effectively the commit SHA.
    
    The tooling also adds a `-dirty` suffix if the Git repository has
    modified/untracked files.
    
    Signed-off-by: Simon Pasquier <spasquie@redhat.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,28 +4,14 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+package version
 
-package util
-
-import "runtime/debug"
-
-func Version() string {
-	version := "unknown"
-	if info, ok := debug.ReadBuildInfo(); ok {
-		for _, setting := range info.Settings {
-			if setting.Key == "vcs.revision" {
-				version = setting.Value
-				break
-			}
-		}
-	}
-
-	return version
-}
+// CommitSHA is the SHA of the current HEAD. Populated at build-time.
+var CommitSHA string
