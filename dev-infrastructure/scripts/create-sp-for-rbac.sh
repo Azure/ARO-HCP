57da09f7d76b5af883a85de59ddb81c07e4cccd2
tschneid <tschneid@redhat.com>
2025-04-30 10:37:50 -0500

    reset credentials for existing apps

--- dev-infrastructure/scripts/create-sp-for-rbac.sh
+++ dev-infrastructure/scripts/create-sp-for-rbac.sh
@@ -1,15 +1,24 @@
 set -euo pipefail
 
-if [[ $(az ad app list --display-name ${APPLICATION_NAME} --query '[*]'.appId -o tsv | wc -l ) == 1 ]];
+APP_ID=$(az ad app list --display-name ${APPLICATION_NAME} --query '[*]'.appId -o tsv)
+if [[ $(echo ${APP_ID} | wc -l ) == 1 ]];
 then
-    echo "Application exists, existing"
+    echo "Application exists, resetting certificate."
+
+    az ad app credential reset \
+        --id "${APPLICATION_NAME}" \
+        --keyvault "${KEY_VAULT_NAME}" \
+        --cert "${CERTIFICATE_NAME}" \
+        --role "${ROLE_DEFINITION_NAME}" \
+        --scopes "/subscriptions/${SUBSCRIPTION_ID}"
+
     exit 0
 fi
 
 az ad sp create-for-rbac \
---years 10 \
---display-name "${APPLICATION_NAME}" \
---keyvault "${KEY_VAULT_NAME}" \
---cert "${CERTIFICATE_NAME}" \
---role "${ROLE_DEFINITION_NAME}" \
---scopes "/subscriptions/${SUBSCRIPTION_ID}"
+    --years 10 \
+    --display-name "${APPLICATION_NAME}" \
+    --keyvault "${KEY_VAULT_NAME}" \
+    --cert "${CERTIFICATE_NAME}" \
+    --role "${ROLE_DEFINITION_NAME}" \
+    --scopes "/subscriptions/${SUBSCRIPTION_ID}"
