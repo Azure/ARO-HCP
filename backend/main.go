56cb642bcafcb48a79db37188f9c72a3fe6304e2
Matthew Barnes <mbarnes@fedorapeople.org>
2025-05-14 10:45:18 -0400

    backend: Pass location to OperationsScanner

--- backend/main.go
+++ backend/main.go
@@ -258,7 +258,7 @@ func Run(cmd *cobra.Command, args []string) error {
 	group.Go(func() error {
 		var (
 			startedLeading    atomic.Bool
-			operationsScanner = NewOperationsScanner(dbClient, ocmConnection)
+			operationsScanner = NewOperationsScanner(argLocation, dbClient, ocmConnection)
 		)
 
 		le, err := leaderelection.NewLeaderElector(leaderelection.LeaderElectionConfig{
