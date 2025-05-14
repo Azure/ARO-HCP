e6d43756f00fb275acda9af59672ee0f15be5456
Patrik Suba <psuba@redhat.com>
2025-05-14 17:05:18 +0200

    Attemp to retype data type of loaded cluster arm data into correct data type used by HcpOpenShiftClustersClient

--- test/e2e/cluster_put_test.go
+++ test/e2e/cluster_put_test.go
@@ -1,43 +1,43 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package e2e
 
 import (
 	"context"
-	"encoding/json"
-	"os"
-	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	. "github.com/Azure/ARO-HCP/internal/api"
+	"github.com/Azure/ARO-HCP/internal/api"
 	"github.com/Azure/ARO-HCP/internal/api/arm"
-	api "github.com/Azure/ARO-HCP/internal/api/v20240610preview/generated"
+	"github.com/Azure/ARO-HCP/internal/api/v20240610preview"
+	apigen "github.com/Azure/ARO-HCP/internal/api/v20240610preview/generated"
+	"github.com/Azure/ARO-HCP/test/util/integration"
 	"github.com/Azure/ARO-HCP/test/util/labels"
-	"github.com/Azure/ARO-HCP/test/util/log"
 )
 
-func getIdentityFromFile() arm.ManagedServiceIdentity {
-	content, err := os.ReadFile(identityFile)
-	if err != nil {
-		panic(err)
-	}
-	decoder := json.NewDecoder(strings.NewReader(string(content)))
-	identity := arm.ManagedServiceIdentity{}
-	decoder.Decode(&identity)
-
-	return identity
-}
-
-func newClusterResource(identity arm.ManagedServiceIdentity) api.HcpOpenShiftCluster {
-	apiIdentity := make(map[string]*api.UserAssignedIdentity)
+func newClusterResource(identity arm.ManagedServiceIdentity) apigen.HcpOpenShiftCluster {
+	apiIdentity := make(map[string]*apigen.UserAssignedIdentity)
 	for key, value := range identity.UserAssignedIdentities {
-		apiIdentity[key] = (*api.UserAssignedIdentity)(value)
+		apiIdentity[key] = (*apigen.UserAssignedIdentity)(value)
 	}
 
-	newCluster := api.HcpOpenShiftCluster{
+	newCluster := apigen.HcpOpenShiftCluster{
 		Location: &location,
-		Identity: &api.ManagedServiceIdentity{
-			Type:                   Ptr(api.ManagedServiceIdentityType(identity.Type)),
+		Identity: &apigen.ManagedServiceIdentity{
+			Type:                   api.Ptr(apigen.ManagedServiceIdentityType(identity.Type)),
 			UserAssignedIdentities: apiIdentity,
 		},
 	}
@@ -46,33 +46,24 @@ func newClusterResource(identity arm.ManagedServiceIdentity) api.HcpOpenShiftClu
 
 var _ = Describe("Cluster put operations", func() {
 	var (
-		clustersClient *api.HcpOpenShiftClustersClient
+		clustersClient *apigen.HcpOpenShiftClustersClient
+		customerEnv    *integration.CustomerEnv
+		cluster        *integration.Cluster
 	)
 
 	BeforeEach(func() {
 		By("Prepare HCP clusters client")
 		clustersClient = clients.NewHcpOpenShiftClustersClient()
+		By("Prepare customer environment")
+		customerEnv = &e2eSetup.CustomerEnv
+		By("Prepare e2esetup cluster resource")
+		cluster = &e2eSetup.Cluster
 	})
 
 	Context("Negative", func() {
 		It("Try to create cluster with managed identities and location", labels.Low, labels.Negative, func(ctx context.Context) {
-			genClusterResource := NewDefaultHCPOpenShiftCluster()
-			By("Set version ID")
-			genClusterResource.Properties.Version.ID = "openshift-v4.18.1"
-			By("Set managed resource group")
-			genClusterResource.Properties.Platform.ManagedResourceGroup = managedResourceGroup
-
-			identity := getIdentityFromFile()
-
-			clusterResource := newClusterResource(identity)
-
-			log.Logger.Infoln(*clusterResource.Identity.Type)
-			for key := range clusterResource.Identity.UserAssignedIdentities {
-				log.Logger.Infoln("Gen Identity")
-				log.Logger.Infoln(key)
-			}
-
-			poller, err := clustersClient.BeginCreateOrUpdate(ctx, customerRGName, clusterName, clusterResource, nil)
+			clusterResource := v20240610preview.NewHCPOpenShiftCluster(cluster.ARMData)
+			poller, err := clustersClient.BeginCreateOrUpdate(ctx, customerEnv.CustomerRGName, cluster.Name, clusterResource.HcpOpenShiftCluster, nil)
 			Expect(err).ToNot(BeNil())
 			Expect(err.Error()).To(ContainSubstring("500 Internal Server Error"))
 			Expect(poller).To(BeNil())
