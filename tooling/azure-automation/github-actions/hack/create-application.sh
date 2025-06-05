44124ee22846c92030875c6db5379455256b66c9
Gerd Oberlechner <goberlec@redhat.com>
2025-06-05 09:00:33 +0200

    crypto officer for gh action identity
    the secret sync running the GH action requires crypto functionality from the KV for proper secret loading
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- tooling/azure-automation/github-actions/hack/create-application.sh
+++ tooling/azure-automation/github-actions/hack/create-application.sh
@@ -23,6 +23,7 @@ az role assignment create --assignee "${OBJ_ID}" --role "Role Based Access Contr
 az role assignment create --assignee "${OBJ_ID}" --role "Grafana Admin" --scope /subscriptions/${SUBSCRIPTION}
 az role assignment create --assignee "${OBJ_ID}" --role "Key Vault Secrets Officer" --scope /subscriptions/${SUBSCRIPTION}
 az role assignment create --assignee "${OBJ_ID}" --role "Key Vault Certificates Officer" --scope /subscriptions/${SUBSCRIPTION}
+az role assignment create --assignee "${OBJ_ID}" --role "Key Vault Crypto Officer" --scope /subscriptions/${SUBSCRIPTION}
 
 az ad app federated-credential create --id "${APP_ID}" --parameters \
 '{
