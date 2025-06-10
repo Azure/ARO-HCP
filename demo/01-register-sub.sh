4f8ea0d7a6c5e04251ad535b58b7a62524e3a908
Matthew Barnes <mbarnes@fedorapeople.org>
2025-06-10 08:03:14 -0400

    demo: Add az_account_is_int function
    Returns 0 when using the ARO-HCP INT subscription by default.
    
    Use it to set the default value for LOCATION and to determine how
    to register a subscription ("curl" or "az provider register").

--- demo/01-register-sub.sh
+++ demo/01-register-sub.sh
@@ -7,4 +7,8 @@ set -o pipefail
 source "$(dirname "$0")"/common.sh
 source env_vars
 
-correlation_headers | curl -sSi -H @- -X PUT "localhost:8443/subscriptions/${SUBSCRIPTION_ID}?api-version=2.0" --json "{\"state\":\"Registered\", \"registrationDate\": \"now\", \"properties\": { \"tenantId\": \"${TENANT_ID}\"}}"
+if az_account_is_int; then
+    az provider register --namespace "Microsoft.RedHatOpenShift"
+else
+    correlation_headers | curl -sSi -H @- -X PUT "localhost:8443/subscriptions/${SUBSCRIPTION_ID}?api-version=2.0" --json "{\"state\":\"Registered\", \"registrationDate\": \"now\", \"properties\": { \"tenantId\": \"${TENANT_ID}\"}}"
+fi
