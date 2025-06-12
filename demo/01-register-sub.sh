0f811881f4e49fe1a85502b749515958ea137ecc
Matthew Barnes <mbarnes@fedorapeople.org>
2025-06-12 09:11:35 -0400

    demo: Rename az_account_is_int to is_int_testing_subscription
    Per peer review feedback.

--- demo/01-register-sub.sh
+++ demo/01-register-sub.sh
@@ -7,7 +7,7 @@ set -o pipefail
 source "$(dirname "$0")"/common.sh
 source env_vars
 
-if az_account_is_int; then
+if is_int_testing_subscription; then
     az provider register --namespace "Microsoft.RedHatOpenShift"
 else
     rp_put_request "${SUBSCRIPTION_RESOURCE_ID}" "{\"state\":\"Registered\", \"registrationDate\": \"now\", \"properties\": { \"tenantId\": \"${TENANT_ID}\"}}" "2.0"
