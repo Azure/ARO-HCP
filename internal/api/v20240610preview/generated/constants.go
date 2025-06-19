603b7a62da7ff36a98be26437b483338622fecf9
Maitiu O Ciarain <m.ociarain@gmail.com>
2025-06-19 13:56:06 +0000

    Fix formatting

--- internal/api/v20240610preview/generated/constants.go
+++ internal/api/v20240610preview/generated/constants.go
@@ -8,7 +8,7 @@
 package generated
 
 const (
-	moduleName = "undefined"
+	moduleName    = "undefined"
 	moduleVersion = "v0.0.1"
 )
 
@@ -21,7 +21,7 @@ const (
 
 // PossibleActionTypeValues returns the possible values for the ActionType const type.
 func PossibleActionTypeValues() []ActionType {
-	return []ActionType{	
+	return []ActionType{
 		ActionTypeInternal,
 	}
 }
@@ -30,15 +30,15 @@ func PossibleActionTypeValues() []ActionType {
 type CreatedByType string
 
 const (
-	CreatedByTypeApplication CreatedByType = "Application"
-	CreatedByTypeKey CreatedByType = "Key"
+	CreatedByTypeApplication     CreatedByType = "Application"
+	CreatedByTypeKey             CreatedByType = "Key"
 	CreatedByTypeManagedIdentity CreatedByType = "ManagedIdentity"
-	CreatedByTypeUser CreatedByType = "User"
+	CreatedByTypeUser            CreatedByType = "User"
 )
 
 // PossibleCreatedByTypeValues returns the possible values for the CreatedByType const type.
 func PossibleCreatedByTypeValues() []CreatedByType {
-	return []CreatedByType{	
+	return []CreatedByType{
 		CreatedByTypeApplication,
 		CreatedByTypeKey,
 		CreatedByTypeManagedIdentity,
@@ -61,7 +61,7 @@ const (
 
 // PossibleDiskStorageAccountTypeValues returns the possible values for the DiskStorageAccountType const type.
 func PossibleDiskStorageAccountTypeValues() []DiskStorageAccountType {
-	return []DiskStorageAccountType{	
+	return []DiskStorageAccountType{
 		DiskStorageAccountTypePremiumLRS,
 		DiskStorageAccountTypeStandardLRS,
 		DiskStorageAccountTypeStandardSSDLRS,
@@ -82,7 +82,7 @@ const (
 
 // PossibleEffectValues returns the possible values for the Effect const type.
 func PossibleEffectValues() []Effect {
-	return []Effect{	
+	return []Effect{
 		EffectNoExecute,
 		EffectNoSchedule,
 		EffectPreferNoSchedule,
@@ -93,15 +93,15 @@ func PossibleEffectValues() []Effect {
 type ManagedServiceIdentityType string
 
 const (
-	ManagedServiceIdentityTypeNone ManagedServiceIdentityType = "None"
-	ManagedServiceIdentityTypeSystemAssigned ManagedServiceIdentityType = "SystemAssigned"
+	ManagedServiceIdentityTypeNone                       ManagedServiceIdentityType = "None"
+	ManagedServiceIdentityTypeSystemAssigned             ManagedServiceIdentityType = "SystemAssigned"
 	ManagedServiceIdentityTypeSystemAssignedUserAssigned ManagedServiceIdentityType = "SystemAssigned,UserAssigned"
-	ManagedServiceIdentityTypeUserAssigned ManagedServiceIdentityType = "UserAssigned"
+	ManagedServiceIdentityTypeUserAssigned               ManagedServiceIdentityType = "UserAssigned"
 )
 
 // PossibleManagedServiceIdentityTypeValues returns the possible values for the ManagedServiceIdentityType const type.
 func PossibleManagedServiceIdentityTypeValues() []ManagedServiceIdentityType {
-	return []ManagedServiceIdentityType{	
+	return []ManagedServiceIdentityType{
 		ManagedServiceIdentityTypeNone,
 		ManagedServiceIdentityTypeSystemAssigned,
 		ManagedServiceIdentityTypeSystemAssignedUserAssigned,
@@ -121,7 +121,7 @@ const (
 
 // PossibleNetworkTypeValues returns the possible values for the NetworkType const type.
 func PossibleNetworkTypeValues() []NetworkType {
-	return []NetworkType{	
+	return []NetworkType{
 		NetworkTypeOVNKubernetes,
 		NetworkTypeOther,
 	}
@@ -137,7 +137,7 @@ const (
 
 // PossibleOptionalClusterCapabilityValues returns the possible values for the OptionalClusterCapability const type.
 func PossibleOptionalClusterCapabilityValues() []OptionalClusterCapability {
-	return []OptionalClusterCapability{	
+	return []OptionalClusterCapability{
 		OptionalClusterCapabilityImageRegistry,
 	}
 }
@@ -147,14 +147,14 @@ func PossibleOptionalClusterCapabilityValues() []OptionalClusterCapability {
 type Origin string
 
 const (
-	OriginSystem Origin = "system"
-	OriginUser Origin = "user"
+	OriginSystem     Origin = "system"
+	OriginUser       Origin = "user"
 	OriginUserSystem Origin = "user,system"
 )
 
 // PossibleOriginValues returns the possible values for the Origin const type.
 func PossibleOriginValues() []Origin {
-	return []Origin{	
+	return []Origin{
 		OriginSystem,
 		OriginUser,
 		OriginUserSystem,
@@ -171,7 +171,7 @@ const (
 
 // PossibleOutboundTypeValues returns the possible values for the OutboundType const type.
 func PossibleOutboundTypeValues() []OutboundType {
-	return []OutboundType{	
+	return []OutboundType{
 		OutboundTypeLoadBalancer,
 	}
 }
@@ -198,7 +198,7 @@ const (
 
 // PossibleProvisioningStateValues returns the possible values for the ProvisioningState const type.
 func PossibleProvisioningStateValues() []ProvisioningState {
-	return []ProvisioningState{	
+	return []ProvisioningState{
 		ProvisioningStateAccepted,
 		ProvisioningStateCanceled,
 		ProvisioningStateDeleting,
@@ -221,9 +221,8 @@ const (
 
 // PossibleVisibilityValues returns the possible values for the Visibility const type.
 func PossibleVisibilityValues() []Visibility {
-	return []Visibility{	
+	return []Visibility{
 		VisibilityPrivate,
 		VisibilityPublic,
 	}
 }
-
