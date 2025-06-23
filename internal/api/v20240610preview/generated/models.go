c57a17cafd8ebc20d61648c05ba7f60b934a6ce5
Matthew Barnes <mbarnes@fedorapeople.org>
2025-06-23 09:35:43 -0400

    api: Add hcpOpenShiftVersions proxy resource type

--- internal/api/v20240610preview/generated/models.go
+++ internal/api/v20240610preview/generated/models.go
@@ -214,6 +214,48 @@ type HcpOpenShiftClusterUpdate struct {
 	Type *string
 }
 
+// HcpOpenShiftVersion represents a location based available HCP OpenShift version
+type HcpOpenShiftVersion struct {
+	// The resource-specific properties for this resource.
+	Properties *HcpOpenShiftVersionProperties
+
+	// READ-ONLY; Fully qualified resource ID for the resource. E.g. "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}"
+	ID *string
+
+	// READ-ONLY; The name of the resource
+	Name *string
+
+	// READ-ONLY; Azure Resource Manager metadata containing createdBy and modifiedBy information.
+	SystemData *SystemData
+
+	// READ-ONLY; The type of the resource. E.g. "Microsoft.Compute/virtualMachines" or "Microsoft.Storage/storageAccounts"
+	Type *string
+}
+
+// HcpOpenShiftVersionListResult - The response of a HcpOpenShiftVersion list operation.
+type HcpOpenShiftVersionListResult struct {
+	// REQUIRED; The HcpOpenShiftVersion items on this page
+	Value []*HcpOpenShiftVersion
+
+	// The link to the next page of items
+	NextLink *string
+}
+
+// HcpOpenShiftVersionProperties contains details of an available HCP Openshift version
+type HcpOpenShiftVersionProperties struct {
+	// REQUIRED; ChannelGroup is the name of the group where this version belongs.
+	ChannelGroup *string
+
+	// REQUIRED; Enabled indicates if this version can be used to create clusters.
+	Enabled *bool
+
+	// REQUIRED; EndOfLifeTimestamp is the date and time when this version will reach End of Life.
+	EndOfLifeTimestamp *time.Time
+
+	// REQUIRED; SupportedArchitectures contains version information for each supported architecture.
+	SupportedArchitectures []*VersionArchitecture
+}
+
 // Label represents the Kubernetes label
 type Label struct {
 	// REQUIRED; The key of the label
@@ -486,6 +528,22 @@ type PlatformProfile struct {
 	IssuerURL *string
 }
 
+// ProxyResource - The resource model definition for a Azure Resource Manager proxy resource. It will not have tags and a
+// location
+type ProxyResource struct {
+	// READ-ONLY; Fully qualified resource ID for the resource. E.g. "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}"
+	ID *string
+
+	// READ-ONLY; The name of the resource
+	Name *string
+
+	// READ-ONLY; Azure Resource Manager metadata containing createdBy and modifiedBy information.
+	SystemData *SystemData
+
+	// READ-ONLY; The type of the resource. E.g. "Microsoft.Compute/virtualMachines" or "Microsoft.Storage/storageAccounts"
+	Type *string
+}
+
 // Resource - Common fields that are returned in the response for all Azure Resource Manager resources
 type Resource struct {
 	// READ-ONLY; Fully qualified resource ID for the resource. E.g. "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/{resourceProviderNamespace}/{resourceType}/{resourceName}"
@@ -579,6 +637,15 @@ type UserAssignedIdentity struct {
 
 	// READ-ONLY; The principal ID of the assigned identity.
 	PrincipalID *string
+}
+
+// VersionArchitecture contains architecture-specific version information.
+type VersionArchitecture struct {
+	// REQUIRED; AvailableUpgrades is the list of available upgrade versions for this architecture.
+	AvailableUpgrades []*string
+
+	// REQUIRED; Name of the architecture.
+	Name *string
 }
 
 // VersionProfile - Versions represents an OpenShift version.
