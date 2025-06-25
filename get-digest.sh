d869819cd465d8bf6d4eba97b9b2248c20145586
Gerd Oberlechner <goberlec@redhat.com>
2025-06-25 09:39:59 +0200

    make RP image digests explicit in dev config
    in dev environments, the rp image digests were undefined and we had a deployment mechanism in place to lookup digests at deploytime. this was supposed to bring some dev-ease-of-life. but it also makes reasoning about the config harder and is problematic for processes that require proper configuration.
    
    this PR proposed the following changes:
    * RH config needs to mention the image digests and the `make deploy` targets as well as the GH action based delivery will rely on that
    * there will be an additional target `make deploy.current` that will provide the current behaviour and can be used in personal dev environments
    
    this implies that 1P needs to bump the digests in config at relevant points in time to make sure the changes that should go to integrated dev are actually promoted.
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- get-digest.sh
+++ get-digest.sh
@@ -1,38 +1,25 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]
+if [ "$#" -ne 3 ]
 then
-    echo "Need ARO_HCP_IMAGE_ACR and REPOSITORY parameters"
+    echo "Usage: $0 <ARO_HCP_IMAGE_ACR> <REPOSITORY> <TAG>"
     exit 1
 fi
 
 aro_hcp_image_acr=${1}
 repository=${2}
+tag=${3}
 
-if [ -n "${IMAGE_DIGEST_OVERRIDE}" ];
+tags=$(az acr repository show-tags --orderby time_desc --n "${aro_hcp_image_acr}" --repository "${repository}" --detail)
+
+# find the digest for the specified tag
+suggested_digest=$(jq -r --arg TAG "${tag}" \
+    'first(.[] | select(.name==$TAG) | .digest)' <<< "${tags}")
+if [ -n "${suggested_digest}" ] && [ "${suggested_digest}" != "null" ];
 then
-    echo ${IMAGE_DIGEST_OVERRIDE}
+    echo "${suggested_digest}"
     exit 0
 fi
 
-if [ -n "${IMAGE_DIGEST}" ];
-then
-    echo ${IMAGE_DIGEST}
-    exit 0
-fi
-
-
-tags=$(mktemp)
-trap "rm ${tags}" EXIT
-
-az acr repository show-tags --orderby time_desc --n ${aro_hcp_image_acr} --repository ${repository} --detail > $tags
-
-suggested_digest=$(jq -r --arg TAG $(git rev-parse --short=7 HEAD) \
-    'first(.[] | select(.name==$TAG) | .digest)' $tags)
-if [ -n "${suggested_digest}" ];
-then
-    echo ${suggested_digest}
-    exit 0
-fi
-
-jq -r 'first(.[] | .digest)' $tags
+# if there is no digest for the specified tag, return the first digest from the list of non-dirty tags
+jq -r 'first(.[] | select(.name | endswith("-dirty") | not) | .digest)' <<< "${tags}"
