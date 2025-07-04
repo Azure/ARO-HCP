08c4e42f454d132d2c4a8b076eae46ff84f61aae
Suraj Patil <patilsuraj767@gmail.com>
2025-07-04 11:13:14 +0530

    Add key vault to support etcd encryption
    Signed-off-by: Suraj Patil <patilsuraj767@gmail.com>

--- demo/02-customer-infra.sh
+++ demo/02-customer-infra.sh
@@ -18,3 +18,29 @@ az network vnet create \
   --subnet-name ${CUSTOMER_VNET_SUBNET1} \
   --subnet-prefixes 10.0.0.0/24 \
   --nsg "${NSG_ID}"  --location ${LOCATION}
+
+az keyvault create \
+  --name "${CUSTOMER_KV_NAME}" \
+  --resource-group "${CUSTOMER_RG_NAME}" \
+  --location "${LOCATION}" \
+  --enable-rbac-authorization true
+
+# Get current user's object ID
+CURRENT_USER_ID=$(az ad signed-in-user show --query id -o tsv)
+
+# Assign Key Vault Crypto Officer role to current user
+az role assignment create \
+  --assignee "${CURRENT_USER_ID}" \
+  --role "Key Vault Crypto Officer" \
+  --scope "/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${CUSTOMER_RG_NAME}/providers/Microsoft.KeyVault/vaults/${CUSTOMER_KV_NAME}"
+
+# Wait for role assignment to propagate
+echo "Waiting for RBAC role assignment to propagate..."
+sleep 30
+
+# Create RSA key in Key Vault
+az keyvault key create \
+  --vault-name "${CUSTOMER_KV_NAME}" \
+  --name "encryption-key" \
+  --kty RSA \
+  --size 2048
