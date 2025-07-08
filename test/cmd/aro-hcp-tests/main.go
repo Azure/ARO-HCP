4fdbeed4592aca5ad59f3dfc0134714070c0f269
David Eads <deads@redhat.com>
2025-07-08 15:18:38 -0400

    Create aro-hcp-tests using openshift-test-extensions
        https://github.com/openshift-eng/openshift-tests-extension is a library
        designed to create suites of ginkgo tests selected by criteria.  Then it
        will run the in parallel in distinct processes to avoid dependencies.
        It will produce junit output compatible with OCP's CI post-processing.

---
+++ test/cmd/aro-hcp-tests/main.go
@@ -0,0 +1,143 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/Azure/ARO-HCP/test/util/labels"
+
+	"github.com/openshift-eng/openshift-tests-extension/pkg/cmd"
+	e "github.com/openshift-eng/openshift-tests-extension/pkg/extension"
+	g "github.com/openshift-eng/openshift-tests-extension/pkg/ginkgo"
+
+	// If using ginkgo, import your tests here
+	_ "github.com/Azure/ARO-HCP/test/e2e"
+)
+
+func main() {
+	// Extension registry
+	registry := e.NewRegistry()
+
+	// You can declare multiple extensions, but most people will probably only need to create one.
+	ext := e.NewExtension("aro-hcp", "payload", "cuj-e2e-tests")
+
+	// The tests that a suite is composed of can be filtered by CEL expressions. By
+	// default, the qualifiers only apply to tests from this extension.
+	ext.AddSuite(e.Suite{
+		Name: "integration/parallel",
+		Qualifiers: []string{
+			// Remember that the label constants are (currently) slices, not items.
+			fmt.Sprintf(`labels.exists(l, l=="%s")`, labels.Integration[0]),
+			fmt.Sprintf(`labels.exists(l, l=="%s")`, labels.Any[0]),
+		},
+		Parallelism: 15,
+	})
+
+	ext.AddSuite(e.Suite{
+		Name: "stage/parallel",
+		Qualifiers: []string{
+			// Remember that the label constants are (currently) slices, not items.
+			fmt.Sprintf(`labels.exists(l, l=="%s")`, labels.Stage[0]),
+			fmt.Sprintf(`labels.exists(l, l=="%s")`, labels.Any[0]),
+		},
+		Parallelism: 15,
+	})
+
+	// If using Ginkgo, build test specs automatically
+	specs, err := g.BuildExtensionTestSpecsFromOpenShiftGinkgoSuite()
+	if err != nil {
+		panic(fmt.Sprintf("couldn't build extension test specs from ginkgo: %+v", err.Error()))
+	}
+
+	// You can add hooks to run before/after tests. There are BeforeEach, BeforeAll, AfterEach,
+	// and AfterAll. "Each" functions must be thread safe.
+	//
+	// specs.AddBeforeAll(func() {
+	// 	initializeTestFramework()
+	// })
+	//
+	// specs.AddBeforeEach(func(spec ExtensionTestSpec) {
+	//	if spec.Name == "my test" {
+	//		// do stuff
+	//	}
+	// })
+	//
+	// specs.AddAfterEach(func(res *ExtensionTestResult) {
+	// 	if res.Result == ResultFailed && apiTimeoutRegexp.Matches(res.Output) {
+	// 		res.AddDetails("api-timeout", collectDiagnosticInfo())
+	// 	}
+	// })
+
+	// You can also manually build a test specs list from other testing tooling
+	// TODO: example
+
+	// Modify specs, such as adding a label to all specs
+	// 	specs = specs.AddLabel("SLOW")
+
+	// Specs can be globally filtered...
+	// specs = specs.MustFilter([]string{`name.contains("filter")`})
+
+	// Or walked...
+	// specs = specs.Walk(func(spec *extensiontests.ExtensionTestSpec) {
+	//	if strings.Contains(e.Name, "scale up") {
+	//		e.Labels.Insert("SLOW")
+	//	}
+	//
+	// Specs can also be selected...
+	// specs = specs.Select(et.NameContains("slow test")).AddLabel("SLOW")
+	//
+	// Or with "any" (or) matching selections
+	// specs = specs.SelectAny(et.NameContains("slow test"), et.HasLabel("SLOW"))
+	//
+	// Or with "all" (and) matching selections
+	// specs = specs.SelectAll(et.NameContains("slow test"), et.HasTagWithValue("speed", "slow"))
+	//
+	// There are also Must* functions for any of the above flavors of selection
+	// which will return an error if nothing is found
+	// specs, err = specs.MustSelect(et.NameContains("slow test")).AddLabel("SLOW")
+	// if err != nil {
+	//    logrus.Warn("no specs found: %w", err)
+	// }
+	// Test renames
+	//	if spec.Name == "[sig-testing] openshift-tests-extension has a test with a typo" {
+	//		spec.OriginalName = `[sig-testing] openshift-tests-extension has a test with a tpyo`
+	//	}
+	//
+	// Filter by environment flags
+	// if spec.Name == "[sig-testing] openshift-tests-extension should support defining the platform for tests" {
+	//		spec.Include(et.PlatformEquals("aws"))
+	//		spec.Exclude(et.And(et.NetworkEquals("ovn"), et.TopologyEquals("ha")))
+	//	}
+	// })
+
+	ext.AddSpecs(specs)
+	registry.Register(ext)
+
+	root := &cobra.Command{
+		Long: "OpenShift Tests Extension Example",
+	}
+
+	root.AddCommand(cmd.DefaultExtensionCommands(registry)...)
+
+	if err := func() error {
+		return root.Execute()
+	}(); err != nil {
+		os.Exit(1)
+	}
+}
--- test/go.mod
+++ test/go.mod
@@ -8,18 +8,31 @@ require (
 	github.com/Azure/ARO-HCP/internal v0.0.0-00010101000000-000000000000
 	github.com/onsi/ginkgo/v2 v2.22.2
 	github.com/onsi/gomega v1.36.2
+	github.com/openshift-eng/openshift-tests-extension v0.0.0-20250702172817-97309544869d
 	github.com/sirupsen/logrus v1.9.3
+	github.com/spf13/cobra v1.9.1
 )
 
 require (
+	cel.dev/expr v0.19.1 // indirect
 	github.com/AzureAD/microsoft-authentication-library-for-go v1.4.2 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/google/cel-go v0.23.2 // indirect
 	github.com/google/pprof v0.0.0-20241210010833-40e02aabc2ad // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476 // indirect
 	golang.org/x/tools v0.34.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250218202821-56aae31c358a // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250219182151-9fdb1cabc7b2 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
 
 require (
@@ -39,3 +52,6 @@ require (
 )
 
 replace github.com/Azure/ARO-HCP/internal => ../internal
+
+// this is the OCP fork of ginkgo that allows listing and inspecting the tests to be compatible with https://github.com/openshift-eng/openshift-tests-extension
+replace github.com/onsi/ginkgo/v2 => github.com/openshift/onsi-ginkgo/v2 v2.6.1-0.20250416174521-4eb003743b54
