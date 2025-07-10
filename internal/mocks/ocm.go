4ef4b04176941ba8b308d92546fc57ad4c365c04
katherinelc321 <klin@redhat.com>
2025-07-10 14:42:11 +0000

    make all-tidy

--- internal/mocks/ocm.go
+++ internal/mocks/ocm.go
@@ -13,10 +13,11 @@ import (
 	context "context"
 	reflect "reflect"
 
-	ocm "github.com/Azure/ARO-HCP/internal/ocm"
 	v1alpha1 "github.com/openshift-online/ocm-sdk-go/arohcp/v1alpha1"
 	v1 "github.com/openshift-online/ocm-sdk-go/clustersmgmt/v1"
 	gomock "go.uber.org/mock/gomock"
+
+	ocm "github.com/Azure/ARO-HCP/internal/ocm"
 )
 
 // MockClusterServiceClientSpec is a mock of ClusterServiceClientSpec interface.
