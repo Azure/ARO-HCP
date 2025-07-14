736b2f8ea64aa7af9534c27e67e3fec5866f0337
Venkatesh <ves@redhat.com>
2025-07-14 23:38:52 +0530

    handle the nil pointer error

--- internal/api/v20240610preview/hcpopenshiftclusters_methods.go
+++ internal/api/v20240610preview/hcpopenshiftclusters_methods.go
@@ -397,7 +397,7 @@ func normalizeCustomerManaged(p *generated.CustomerManagedEncryptionProfile, out
 	if p.EncryptionType != nil {
 		out.EncryptionType = api.CustomerManagedEncryptionType(*p.EncryptionType)
 	}
-	if p.Kms != nil {
+	if p.Kms.ActiveKey != nil {
 		normalizeActiveKey(p.Kms.ActiveKey, &out.Kms.ActiveKey)
 	}
 }
