81a9ee0a2012f9297515d8c0363d501c59c64da7
Gerd Oberlechner <goberlec@redhat.com>
2025-07-15 13:41:08 +0200

    fix oidc read endpoint for CS (#2170)
    we accidentally used the blob endpoint for read instead the web endpoint
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- cluster-service/oidc-base-endpoint.sh
+++ cluster-service/oidc-base-endpoint.sh
@@ -7,7 +7,7 @@ AFD_ENDPOINT="$3"
 
 PUBLIC_ACCESS_ENABLED=$(az storage account show -n "${STORAGE_ACCOUNT}" -g "${RESOURCEGROUP}" --query allowBlobPublicAccess -o tsv)
 if [ "$PUBLIC_ACCESS_ENABLED" == "true" ]; then
-    BLOB_ENDPOINT=$(az storage account show -n "${STORAGE_ACCOUNT}" -g "${RESOURCEGROUP}" --query primaryEndpoints.blob -o tsv)
+    BLOB_ENDPOINT=$(az storage account show -n "${STORAGE_ACCOUNT}" -g "${RESOURCEGROUP}" --query primaryEndpoints.web -o tsv)
     echo "$BLOB_ENDPOINT"
 else
     echo "$AFD_ENDPOINT"
