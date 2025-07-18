c22a5315d6ecd39b7595ea6993c49160729bdc66
Maitiu O Ciarain <m.ociarain@gmail.com>
2025-07-18 13:25:13 +0000

    Tidy up internal tests

---
+++ internal/api/hcpopenshiftclusterexternalauth_test.go
@@ -0,0 +1,210 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/Azure/ARO-HCP/internal/api/arm"
+)
+
+func TestExternalAuthRequiredForPut(t *testing.T) {
+	tests := []struct {
+		name         string
+		resource     *HCPOpenShiftClusterExternalAuth
+		expectErrors []arm.CloudErrorBody
+	}{
+		{
+			name:     "Empty External Auth",
+			resource: &HCPOpenShiftClusterExternalAuth{},
+			expectErrors: []arm.CloudErrorBody{
+				{
+					Message: "Missing required field 'properties'",
+					Target:  "properties",
+				},
+			},
+		},
+		{
+			name:     "Default external auth",
+			resource: NewDefaultHCPOpenShiftClusterExternalAuth(),
+			expectErrors: []arm.CloudErrorBody{
+				{
+					Message: "Missing required field 'properties'",
+					Target:  "properties",
+				},
+			},
+		},
+		{
+			name:     "Minimum valid external auth",
+			resource: MinimumValidExternalAuthTestCase(),
+		},
+	}
+
+	validate := NewTestValidator()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request, err := http.NewRequest(http.MethodPut, "localhost", nil)
+			require.NoError(t, err)
+
+			actualErrors := ValidateRequest(validate, request, tt.resource)
+
+			// from hcpopenshiftcluster_test.go
+			diff := compareErrors(tt.expectErrors, actualErrors)
+			if diff != "" {
+				t.Fatalf("Expected error mismatch:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestExternalAuthValidate(t *testing.T) {
+	TooLongClaim := strings.Repeat("a", 257)
+	ClientId1 := "clientId1"
+	ClientId2 := "clientId2"
+	ClientComponentName := "A"
+	ClientComponentNamespace := "B"
+
+	// Note "required_for_put" validation tests are above.
+	// This function tests all the other validators in use.
+	tests := []struct {
+		name         string
+		tweaks       *HCPOpenShiftClusterExternalAuth
+		expectErrors []arm.CloudErrorBody
+	}{
+		{
+			name:   "Minimum valid node pool",
+			tweaks: &HCPOpenShiftClusterExternalAuth{},
+		},
+		{
+			name: "Max not satisfied for properties.claim.mappings.username.claim",
+			tweaks: &HCPOpenShiftClusterExternalAuth{
+				Properties: HCPOpenShiftClusterExternalAuthProperties{
+					Claim: ExternalAuthClaimProfile{
+						Mappings: TokenClaimMappingsProfile{
+							Username: UsernameClaimProfile{
+								Claim: TooLongClaim,
+							},
+						},
+					},
+				},
+			},
+			expectErrors: []arm.CloudErrorBody{
+				{
+					Code:    "InvalidRequestContent",
+					Message: fmt.Sprintf("Invalid value '%s' for field 'claim' (maximum length is 256)", TooLongClaim),
+					Target:  "properties.claim.mappings.username.claim",
+				},
+			},
+		},
+		{
+			name: "Max not satisfied for properties.claim.mappings.groups.claim",
+			tweaks: &HCPOpenShiftClusterExternalAuth{
+				Properties: HCPOpenShiftClusterExternalAuthProperties{
+					Claim: ExternalAuthClaimProfile{
+						Mappings: TokenClaimMappingsProfile{
+							Groups: GroupClaimProfile{
+								Claim: TooLongClaim,
+							},
+						},
+					},
+				},
+			},
+			expectErrors: []arm.CloudErrorBody{
+				{
+					Code:    "InvalidRequestContent",
+					Message: fmt.Sprintf("Invalid value '%s' for field 'claim' (maximum length is 256)", TooLongClaim),
+					Target:  "properties.claim.mappings.groups.claim",
+				},
+			},
+		},
+		// //--------------------------------
+		// // Complex field validation
+		// //--------------------------------
+		{
+			name: "Bad properties.issuer.ca",
+			tweaks: &HCPOpenShiftClusterExternalAuth{
+				Properties: HCPOpenShiftClusterExternalAuthProperties{
+					Issuer: TokenIssuerProfile{
+						Ca: "NOT A PEM DOC",
+					},
+				},
+			},
+			expectErrors: []arm.CloudErrorBody{
+				{
+					Message: "TokenIssuerProfile Certificate Authority push be PEM encoded",
+					Target:  "properties.issuer.ca",
+				},
+			},
+		},
+		{
+			name: "External Auth with multiple clients that have the same Name/Namespace pair",
+			tweaks: &HCPOpenShiftClusterExternalAuth{
+				Properties: HCPOpenShiftClusterExternalAuthProperties{
+					Clients: []ExternalAuthClientProfile{
+						{
+							ClientId: ClientId1,
+							Component: ExternalAuthClientComponentProfile{
+								Name:      ClientComponentName,
+								Namespace: ClientComponentNamespace,
+							},
+						},
+						{
+							ClientId: ClientId2,
+							Component: ExternalAuthClientComponentProfile{
+								Name:      ClientComponentName,
+								Namespace: ClientComponentNamespace,
+							},
+						},
+					},
+				},
+			},
+			expectErrors: []arm.CloudErrorBody{
+				{
+					Message: fmt.Sprintf(
+						"External Auth Clients must have a unique combination of component.Name & component.Namespace. "+
+							"The following clientIds share the same unique combination '%s%s' and are invalid: \n '[%s %s]' ",
+						ClientComponentName, ClientComponentNamespace, ClientId1, ClientId2),
+					Target: "properties.clients",
+				},
+			},
+		},
+	}
+
+	validate := NewTestValidator()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resource := ExternalAuthTestCase(t, tt.tweaks)
+
+			actualErrors := resource.Validate(validate, nil)
+
+			// from hcpopenshiftcluster_test.go
+			diff := compareErrors(tt.expectErrors, actualErrors)
+			if diff != "" {
+				t.Fatalf("Expected error mismatch:\n%s", diff)
+			}
+
+			for _, e := range actualErrors {
+				AssertJSONPath[HCPOpenShiftClusterExternalAuth](t, e.Target)
+			}
+		})
+	}
+}
