ee4f43c5468c4349e59f435ead1206d51ad1ad59
Maitiu O Ciarain <m.ociarain@gmail.com>
2025-07-22 10:18:47 +0000

    WIP

--- frontend/pkg/frontend/external_auth.go
+++ frontend/pkg/frontend/external_auth.go
@@ -15,10 +15,14 @@
 package frontend
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/Azure/ARO-HCP/internal/api"
 	"github.com/Azure/ARO-HCP/internal/api/arm"
 	"github.com/Azure/ARO-HCP/internal/database"
+	"github.com/Azure/ARO-HCP/internal/ocm"
+	"github.com/Azure/azure-sdk-for-go/sdk/data/azcosmos"
 )
 
 func (f *Frontend) CreateOrUpdateExternalAuth(writer http.ResponseWriter, request *http.Request) {
@@ -46,7 +50,23 @@ func (f *Frontend) CreateOrUpdateExternalAuth(writer http.ResponseWriter, reques
 		return
 	}
 
-	_, resourceDoc, err := f.dbClient.GetResourceDoc(ctx, resourceID)
+	systemData, err := SystemDataFromContext(ctx)
+	if err != nil {
+		logger.Error(err.Error())
+		arm.WriteInternalServerError(writer)
+		return
+	}
+
+	correlationData, err := CorrelationDataFromContext(ctx)
+	if err != nil {
+		logger.Error(err.Error())
+		arm.WriteInternalServerError(writer)
+		return
+	}
+
+	pk := database.NewPartitionKey(resourceID.SubscriptionID)
+
+	resourceItemID, resourceDoc, err := f.dbClient.GetResourceDoc(ctx, resourceID)
 	if err != nil && !database.IsResponseError(err, http.StatusNotFound) {
 		logger.Error(err.Error())
 		arm.WriteInternalServerError(writer)
@@ -54,18 +74,143 @@ func (f *Frontend) CreateOrUpdateExternalAuth(writer http.ResponseWriter, reques
 	}
 
 	var updating = (resourceDoc != nil)
+	var operationRequest database.OperationRequest
+
+	// var versionedCurrentExternalAuth api.HCPOpenShiftClusterExternalAuth
+	var versionedRequestExternalAuth api.VersionedHCPOpenShiftClusterExternalAuth
+	var successStatusCode int
 
 	if updating {
-		f.updateExternalAuth()
+		updateExternalAuth()
 	} else {
-		createExternalAuth()
+		operationRequest = database.OperationRequestCreate
+
+		switch request.Method {
+		case http.MethodPut:
+			// versionedCurrentExternalAuth = versionedInterface.NewHCPOpenShiftClusterExternalAuth(nil)
+			versionedRequestExternalAuth = versionedInterface.NewHCPOpenShiftClusterExternalAuth(nil)
+			successStatusCode = http.StatusCreated
+		case http.MethodPatch:
+			// PATCH requests never create a new resource.
+			logger.Error("Resource not found")
+			arm.WriteResourceNotFoundError(writer, resourceID)
+			return
+		}
+
+		resourceDoc = database.NewResourceDocument(resourceID)
+	}
+
+	// CheckForProvisioningStateConflict does not log conflict errors
+	// but does log unexpected errors like database failures.
+	cloudError := f.CheckForProvisioningStateConflict(ctx, operationRequest, resourceDoc)
+	if cloudError != nil {
+		arm.WriteCloudError(writer, cloudError)
+		return
+	}
+
+	externalAuth := api.NewDefaultHCPOpenShiftClusterExternalAuth()
+	versionedRequestExternalAuth.Normalize(externalAuth)
+
+	externalAuth.Name = request.PathValue(PathSegmentNodePoolName)
+	csNodePool, err := f.BuildCSNodePool(ctx, externalAuth, updating)
+	if err != nil {
+		logger.Error(err.Error())
+		arm.WriteInternalServerError(writer)
+		return
+	}
+
+	if updating {
+		updateExternalAuth()
+	} else {
+		logger.Info(fmt.Sprintf("creating resource %s", resourceID))
+		_, clusterDoc, err := f.dbClient.GetResourceDoc(ctx, resourceID.Parent)
+		if err != nil {
+			logger.Error(err.Error())
+			arm.WriteInternalServerError(writer)
+			return
+		}
+
+		csNodePool, err = f.clusterServiceClient.PostNodePool(ctx, clusterDoc.InternalID, csNodePool)
+		if err != nil {
+			logger.Error(err.Error())
+			arm.WriteCloudError(writer, CSErrorToCloudError(err, resourceID))
+			return
+		}
+
+		resourceDoc.InternalID, err = ocm.NewInternalID(csNodePool.HREF())
+		if err != nil {
+			logger.Error(err.Error())
+			arm.WriteInternalServerError(writer)
+			return
+		}
+	}
+
+	transaction := f.dbClient.NewTransaction(pk)
+
+	operationDoc := database.NewOperationDocument(operationRequest, resourceDoc.ResourceID, resourceDoc.InternalID, correlationData)
+	operationID := transaction.CreateOperationDoc(operationDoc, nil)
+
+	f.ExposeOperation(writer, request, operationID, transaction)
+
+	if !updating {
+		resourceItemID = transaction.CreateResourceDoc(resourceDoc, nil)
+	}
+
+	var patchOperations database.ResourceDocumentPatchOperations
+
+	patchOperations.SetActiveOperationID(&operationID)
+	patchOperations.SetProvisioningState(operationDoc.Status)
+
+	// Record the latest system data values from ARM, if present.
+	if systemData != nil {
+		patchOperations.SetSystemData(systemData)
+	}
+
+	// Here the difference between a nil map and an empty map is significant.
+	// If the Tags map is nil, that means it was omitted from the request body,
+	// so we leave any existing tags alone. If the Tags map is non-nil, even if
+	// empty, that means it was specified in the request body and should fully
+	// replace any existing tags.
+	if externalAuth.Tags != nil {
+		patchOperations.SetTags(externalAuth.Tags)
+	}
+
+	transaction.PatchResourceDoc(resourceItemID, patchOperations, nil)
+
+	transactionResult, err := transaction.Execute(ctx, &azcosmos.TransactionalBatchOptions{
+		EnableContentResponseOnWrite: true,
+	})
+	if err != nil {
+		logger.Error(err.Error())
+		arm.WriteInternalServerError(writer)
+		return
+	}
+
+	// Read back the resource document so the response body is accurate.
+	resourceDoc, err = transactionResult.GetResourceDoc(resourceItemID)
+	if err != nil {
+		logger.Error(err.Error())
+		arm.WriteInternalServerError(writer)
+		return
+	}
+
+	responseBody, err := marshalCSNodePool(csNodePool, resourceDoc, versionedInterface)
+	if err != nil {
+		logger.Error(err.Error())
+		arm.WriteInternalServerError(writer)
+		return
+	}
+
+	_, err = arm.WriteJSONResponse(writer, successStatusCode, responseBody)
+	if err != nil {
+		logger.Error(err.Error())
 	}
 }
 
-func createExternalAuth() {
+func createExternalAuth(versionedInterface api.Version) {
 	// var err error
 }
 
-func (f *Frontend) updateExternalAuth() {
+func updateExternalAuth() {
 	// var err error
 }
