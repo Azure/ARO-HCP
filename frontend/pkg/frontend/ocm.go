f722eb9a00697d9ee1656e255d4e56d69bf2134a
Maitiu O Ciarain <m.ociarain@gmail.com>
2025-07-25 09:39:24 +0000

    Remove old comment

--- frontend/pkg/frontend/ocm.go
+++ frontend/pkg/frontend/ocm.go
@@ -291,9 +291,6 @@ func (f *Frontend) BuildCSCluster(resourceID *azcorearm.ResourceID, requestHeade
 
 	clusterBuilder := arohcpv1alpha1.NewCluster()
 
-	// FIXME HcpOpenShiftCluster attributes not being passed:
-	//       ExternalAuth                        (TODO, complicated)
-
 	// These attributes cannot be updated after cluster creation.
 	if !updating {
 		// Add attributes that cannot be updated after cluster creation.
