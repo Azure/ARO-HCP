c08c2178f9cb9d6e3256b9378469749546bf0142
Rajdeep Singh Chauhan <rajdchau@redhat.com>
2025-07-31 10:36:33 -0400

    ARO-18087 update the mocks

--- internal/mocks/ocm.go
+++ internal/mocks/ocm.go
@@ -584,45 +584,6 @@ func (c *MockClusterServiceClientSpecListClustersCall) DoAndReturn(f func(string
 	return c
 }
 
-// ListManagedIdentitiesRequirements mocks base method.
-func (m *MockClusterServiceClientSpec) ListManagedIdentitiesRequirements(ctx context.Context) (map[string]*v1alpha1.ManagedIdentitiesRequirements, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "ListManagedIdentitiesRequirements", ctx)
-	ret0, _ := ret[0].(map[string]*v1alpha1.ManagedIdentitiesRequirements)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// ListManagedIdentitiesRequirements indicates an expected call of ListManagedIdentitiesRequirements.
-func (mr *MockClusterServiceClientSpecMockRecorder) ListManagedIdentitiesRequirements(ctx any) *MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall {
-	mr.mock.ctrl.T.Helper()
-	call := mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListManagedIdentitiesRequirements", reflect.TypeOf((*MockClusterServiceClientSpec)(nil).ListManagedIdentitiesRequirements), ctx)
-	return &MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall{Call: call}
-}
-
-// MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall wrap *gomock.Call
-type MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall struct {
-	*gomock.Call
-}
-
-// Return rewrite *gomock.Call.Return
-func (c *MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall) Return(arg0 map[string]*v1alpha1.ManagedIdentitiesRequirements, arg1 error) *MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall {
-	c.Call = c.Call.Return(arg0, arg1)
-	return c
-}
-
-// Do rewrite *gomock.Call.Do
-func (c *MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall) Do(f func(context.Context) (map[string]*v1alpha1.ManagedIdentitiesRequirements, error)) *MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall {
-	c.Call = c.Call.Do(f)
-	return c
-}
-
-// DoAndReturn rewrite *gomock.Call.DoAndReturn
-func (c *MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall) DoAndReturn(f func(context.Context) (map[string]*v1alpha1.ManagedIdentitiesRequirements, error)) *MockClusterServiceClientSpecListManagedIdentitiesRequirementsCall {
-	c.Call = c.Call.DoAndReturn(f)
-	return c
-}
-
 // ListNodePools mocks base method.
 func (m *MockClusterServiceClientSpec) ListNodePools(clusterInternalID ocm.InternalID, searchExpression string) ocm.NodePoolListIterator {
 	m.ctrl.T.Helper()
