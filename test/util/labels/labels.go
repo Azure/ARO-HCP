98032782b424514d5d6f95d7b860164cf4269bce
Mark Freer <mfreer@mfreer-m68291500.wtrfrdii.csb>
2025-08-07 13:43:03 +0100

    test: add ExternalAuth E2E tests and supporting artifacts

--- test/util/labels/labels.go
+++ test/util/labels/labels.go
@@ -52,3 +52,9 @@ var (
 	RequireNothing        = ginkgo.Label("PreLaunchSetup:None")
 	RequireHappyPathInfra = ginkgo.Label("PreLaunchSetup:HappyPathInfra")
 )
+
+// Custom labels
+var (
+	Integration  = ginkgo.Label("Integration")
+	ExternalAuth = ginkgo.Label("ExternalAuth")
+)
