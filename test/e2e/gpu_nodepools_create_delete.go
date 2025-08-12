2518c50b655d47e43b16142db4cde69436899e52
Patrik Suba <psuba@redhat.com>
2025-08-12 13:07:37 +0200

    Create Test cases for supported GPU nodepool instance types
    - Update e2e-bicep nodepool file with configurable VMsize
    - Created helper functions to delete and get nodepool with timeout

---
+++ test/e2e/gpu_nodepools_create_delete.go
@@ -0,0 +1,151 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package e2e
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	hcpapi20240610 "github.com/Azure/ARO-HCP/internal/api/v20240610preview/generated"
+	"github.com/Azure/ARO-HCP/test/util/framework"
+	"github.com/Azure/ARO-HCP/test/util/labels"
+)
+
+var _ = Describe("HCP Nodepools GPU instances", func() {
+	// Mapping of human-friendly SKU identifiers to Azure VM size names
+	type gpuSKU struct {
+		display string
+		vmSize  string
+	}
+	gpuSkus := []gpuSKU{
+		{display: "NC4asT4v3", vmSize: "Standard_NC4as_T4_v3"},
+		{display: "NC6sv3", vmSize: "Standard_NC6s_v3"},
+		{display: "NC8asT4v3", vmSize: "Standard_NC8as_T4_v3"},
+		{display: "NC12sv3", vmSize: "Standard_NC12s_v3"},
+		{display: "NC16asT4v3", vmSize: "Standard_NC16as_T4_v3"},
+		{display: "NC24sv3", vmSize: "Standard_NC24s_v3"},
+		{display: "NC24rsv3", vmSize: "Standard_NC24rs_v3"},
+		{display: "NC64asT4v3", vmSize: "Standard_NC64as_T4_v3"},
+		{display: "ND96asrv4", vmSize: "Standard_ND96asr_v4"},
+		{display: "NC24adsA100v4", vmSize: "Standard_NC24ads_A100_v4"},
+		{display: "NC48adsA100v4", vmSize: "Standard_NC48ads_A100_v4"},
+		{display: "NC96adsA100v4", vmSize: "Standard_NC96ads_A100_v4"},
+		{display: "ND96amsrA100v4", vmSize: "Standard_ND96amsr_A100_v4"},
+	}
+
+	for _, sku := range gpuSkus {
+		sku := sku
+		It("creates and deletes vm type "+sku.display+" in a single cluster",
+			labels.RequireNothing,
+			labels.Critical,
+			labels.Positive,
+			func(ctx context.Context) {
+				const (
+					customerClusterName = "gpu-nodepool-cluster"
+					location            = "uksouth"
+				)
+
+				tc := framework.NewTestContext()
+
+				By("creating a resource group")
+				resourceGroup, err := tc.NewResourceGroup(ctx, "gpu-nodepools-"+sku.display, location)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("deploying demo template (single-step infra + identities + cluster)")
+				_, err = framework.CreateBicepTemplateAndWait(ctx,
+					tc.GetARMResourcesClientFactoryOrDie(ctx).NewDeploymentsClient(),
+					*resourceGroup.Name,
+					"aro-hcp-demo",
+					framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/demo.json")),
+					map[string]interface{}{
+						"clusterName": customerClusterName,
+					},
+					45*time.Minute,
+				)
+				Expect(err).NotTo(HaveOccurred())
+
+				By("getting credentials and verifying cluster is viable")
+				adminRESTConfig, err := framework.GetAdminRESTConfigForHCPCluster(
+					ctx,
+					tc.Get20240610ClientFactoryOrDie(ctx).NewHcpOpenShiftClustersClient(),
+					*resourceGroup.Name,
+					customerClusterName,
+					10*time.Minute,
+				)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(framework.VerifyHCPCluster(ctx, adminRESTConfig)).To(Succeed())
+
+				// Use Bicep template to create a nodepool with the specified parameters
+				npName := "np-gpu-" + sku.display
+				By(fmt.Sprintf("creating GPU nodepool %q with VM size %q using Bicep template", npName, sku.vmSize))
+				_, err = framework.CreateBicepTemplateAndWait(ctx,
+					tc.GetARMResourcesClientFactoryOrDie(ctx).NewDeploymentsClient(),
+					*resourceGroup.Name,
+					"aro-hcp-gpu-nodepool-"+sku.display,
+					framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/nodepool.json")),
+					map[string]interface{}{
+						"clusterName":  customerClusterName,
+						"nodePoolName": npName,
+						"replicas":     1,
+						"vmSize":       sku.vmSize,
+					},
+					45*time.Minute,
+				)
+				Expect(err).NotTo(HaveOccurred())
+
+				// Verify provisioning succeeded and VM size matches what we requested
+				created, err := framework.GetNodePool(ctx,
+					tc.Get20240610ClientFactoryOrDie(ctx).NewNodePoolsClient(),
+					*resourceGroup.Name,
+					customerClusterName,
+					npName,
+					5*time.Minute,
+				)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(created.Properties).ToNot(BeNil())
+				Expect(created.Properties.ProvisioningState).ToNot(BeNil())
+				Expect(*created.Properties.ProvisioningState).To(Equal(hcpapi20240610.ProvisioningStateSucceeded))
+				Expect(created.Properties.Platform).ToNot(BeNil())
+				Expect(created.Properties.Platform.VMSize).ToNot(BeNil())
+				Expect(*created.Properties.Platform.VMSize).To(Equal(sku.vmSize))
+
+				// Delete
+				By(fmt.Sprintf("deleting GPU nodepool %qd", npName))
+				Expect(framework.DeleteNodePool(
+					ctx,
+					tc.Get20240610ClientFactoryOrDie(ctx).NewNodePoolsClient(),
+					*resourceGroup.Name,
+					customerClusterName,
+					npName,
+					25*time.Minute,
+				)).To(Succeed())
+
+				// Confirm it's gone
+				_, getErr := framework.GetNodePool(ctx,
+					tc.Get20240610ClientFactoryOrDie(ctx).NewNodePoolsClient(),
+					*resourceGroup.Name,
+					customerClusterName,
+					npName,
+					2*time.Minute,
+				)
+				Expect(getErr).To(HaveOccurred())
+			},
+		)
+	}
+})
