dabf3a2bc974b3c0a9d09f74f1779c772a2ac8ea
Mike Gahagan <mgahagan@redhat.com>
2025-08-15 13:45:56 -0400

    convert test to use its own bicep file.

--- test/e2e/cluster_create_nodepool_osdisk.go
+++ test/e2e/cluster_create_nodepool_osdisk.go
@@ -30,20 +30,16 @@ var _ = Describe("Customer", func() {
 		// do nothing.  per test initialization usually ages better than shared.
 	})
 
-	It("should be able to create an HCP cluster and custom node pool osDisk size using bicep templates",
+	It("should be able to create an HCP cluster and custom node pool osDisk size using bicep template",
 		labels.RequireNothing,
 		labels.Critical,
 		labels.Positive,
 		func(ctx context.Context) {
 			const (
-				customerNetworkSecurityGroupName       = "customer-nsg-name"
-				customerVnetName                       = "customer-vnet-name"
-				customerVnetSubnetName                 = "customer-vnet-subnet1"
-				customerClusterName                    = "basic-hcp-cluster"
-				customerNodePoolName                   = "np-128"
-				openshiftControlPlaneVersionId         = "4.19"
-				openshiftNodeVersionId                 = "4.19.0"
-				openshiftNodeOsDiskSizeGiB       int32 = 128
+				customerClusterName             = "hcp-cluster-np-128"
+				customerNodePoolName            = "nodepool-128GiB"
+				customerNodeOsDiskSizeGiB int32 = 128
+				customerNodeReplicas      int32 = 2
 			)
 			tc := framework.NewTestContext()
 
@@ -51,60 +47,18 @@ var _ = Describe("Customer", func() {
 			resourceGroup, err := tc.NewResourceGroup(ctx, "clusternp128", tc.Location())
 			Expect(err).NotTo(HaveOccurred())
 
-			By("creating a customer-infra")
+			By("creating the infrastructure, cluster and node pool from a single bicep template")
 			_, err = framework.CreateBicepTemplateAndWait(ctx,
 				tc.GetARMResourcesClientFactoryOrDie(ctx).NewDeploymentsClient(),
 				*resourceGroup.Name,
-				"customer-infra",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/customer-infra.json")),
+				"cluster-deployment",
+				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/cluster-nodepool-osdisk-128GB/cluster-nodepool-osdisk-128GB.json")),
 				map[string]interface{}{
-					"persistTagValue":        false,
-					"customerNsgName":        customerNetworkSecurityGroupName,
-					"customerVnetName":       customerVnetName,
-					"customerVnetSubnetName": customerVnetSubnetName,
+					"persistTagValue": false,
+					"clusterName":     customerClusterName,
+					"nodePoolName":    customerNodePoolName,
 				},
-				45*time.Minute,
-			)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("creating managed identities")
-			managedIdentityDeploymentResult, err := framework.CreateBicepTemplateAndWait(ctx,
-				tc.GetARMResourcesClientFactoryOrDie(ctx).NewDeploymentsClient(),
-				*resourceGroup.Name,
-				"managed-identities",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/managed-identities.json")),
-				map[string]interface{}{
-					"clusterName": customerClusterName,
-					"nsgName":     customerNetworkSecurityGroupName,
-					"vnetName":    customerVnetName,
-					"subnetName":  customerVnetSubnetName,
-				},
-				45*time.Minute,
-			)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("creating the cluster")
-			userAssignedIdentities, err := framework.GetOutputValue(managedIdentityDeploymentResult, "userAssignedIdentitiesValue")
-			Expect(err).NotTo(HaveOccurred())
-			identity, err := framework.GetOutputValue(managedIdentityDeploymentResult, "identityValue")
-			Expect(err).NotTo(HaveOccurred())
-			managedResourceGroupName := framework.SuffixName(*resourceGroup.Name, "-managed", 64)
-			_, err = framework.CreateBicepTemplateAndWait(ctx,
-				tc.GetARMResourcesClientFactoryOrDie(ctx).NewDeploymentsClient(),
-				*resourceGroup.Name,
-				"cluster",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/cluster.json")),
-				map[string]interface{}{
-					"openshiftVersionId":          openshiftControlPlaneVersionId,
-					"clusterName":                 customerClusterName,
-					"managedResourceGroupName":    managedResourceGroupName,
-					"nsgName":                     customerNetworkSecurityGroupName,
-					"subnetName":                  customerVnetSubnetName,
-					"vnetName":                    customerVnetName,
-					"userAssignedIdentitiesValue": userAssignedIdentities,
-					"identityValue":               identity,
-				},
-				45*time.Minute,
+				60*time.Minute,
 			)
 			Expect(err).NotTo(HaveOccurred())
 
@@ -122,25 +76,8 @@ var _ = Describe("Customer", func() {
 			err = framework.VerifyHCPCluster(ctx, adminRESTConfig)
 			Expect(err).NotTo(HaveOccurred())
 
-			By("creating the node pool with osDisk size 128GiB")
-			_, err = framework.CreateBicepTemplateAndWait(ctx,
-				tc.GetARMResourcesClientFactoryOrDie(ctx).NewDeploymentsClient(),
-				*resourceGroup.Name,
-				"node-pool",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/nodepool.json")),
-				map[string]interface{}{
-					"openshiftVersionId": openshiftNodeVersionId,
-					"clusterName":        customerClusterName,
-					"nodePoolName":       customerNodePoolName,
-					"replicas":           2,
-					"osDiskSizeGiB":      openshiftNodeOsDiskSizeGiB,
-				},
-				45*time.Minute,
-			)
-			Expect(err).NotTo(HaveOccurred())
-
 			By("verifying nodepool configuration")
-			err = framework.VerifyNodePoolConfiguration(ctx, adminRESTConfig, customerClusterName, customerNodePoolName, 2, openshiftNodeOsDiskSizeGiB)
+			err = framework.VerifyNodePoolConfiguration(ctx, adminRESTConfig, customerClusterName, customerNodePoolName, customerNodeReplicas, customerNodeOsDiskSizeGiB)
 			Expect(err).NotTo(HaveOccurred())
 		})
 })
