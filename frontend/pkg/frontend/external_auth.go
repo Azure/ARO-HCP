141f3b1959010220180770aff68d5894ee40e2b9
Matthew Barnes <mbarnes@fedorapeople.org>
2025-08-25 09:13:20 -0400

    frontend: Remove workaround in marshalCSExternalAuth
    The backend is updating external auth provisioning states in Cosmos
    DB, even if it's only using "Succeeded" at the moment, so the front-
    end can trust the provisioning state in Cosmos DB.

--- frontend/pkg/frontend/external_auth.go
+++ frontend/pkg/frontend/external_auth.go
@@ -267,8 +267,7 @@ func marshalCSExternalAuth(csEternalAuth *arohcpv1alpha1.ExternalAuth, doc *data
 	}
 
 	hcpExternalAuth.SystemData = doc.SystemData
-	// TODO: Use the correct state from the document when CS returns ir.
-	hcpExternalAuth.Properties.ProvisioningState = arm.ProvisioningStateSucceeded
+	hcpExternalAuth.Properties.ProvisioningState = doc.ProvisioningState
 
 	return versionedInterface.MarshalHCPOpenShiftClusterExternalAuth(hcpExternalAuth)
 }
