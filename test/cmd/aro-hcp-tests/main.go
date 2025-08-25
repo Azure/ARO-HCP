5c9f0e710b497bb56a4bdc53de0f20872a4ff213
David Eads <deads@redhat.com>
2025-08-25 09:13:37 -0400

    add prod e2e suite

--- test/cmd/aro-hcp-tests/main.go
+++ test/cmd/aro-hcp-tests/main.go
@@ -59,6 +59,16 @@ func main() {
 		Parallelism: 15,
 	})
 
+	ext.AddSuite(e.Suite{
+		Name: "prod/parallel",
+		Qualifiers: []string{
+			// Remember that the label constants are (currently) slices, not items.
+			// TODO we will need per-env markers eventually, but it's ok to start here
+			fmt.Sprintf(`labels.exists(l, l=="%s") && !labels.exists(l, l=="%s")`, labels.RequireNothing[0], labels.IntegrationOnly[0]),
+		},
+		Parallelism: 15,
+	})
+
 	// If using Ginkgo, build test specs automatically
 	specs, err := g.BuildExtensionTestSpecsFromOpenShiftGinkgoSuite()
 	if err != nil {
