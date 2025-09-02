24bdca04840ccdb72ea83761a927528a70896559
Matthew Barnes <mbarnes@fedorapeople.org>
2025-09-02 15:01:17 -0400

    api: Approximate "omitzero" when creating generated API models
    Generated API models do not use struct tags, so this approximates
    the new "omitzero" option in Go 1.24 which causes empty structs to
    be omitted when marshalling to JSON.

--- internal/api/v20240610preview/external_auth_methods.go
+++ internal/api/v20240610preview/external_auth_methods.go
@@ -197,8 +197,11 @@ type HcpOpenShiftClusterExternalAuth struct {
 	generated.ExternalAuth
 }
 
-func newExternalAuthCondition(from *api.ExternalAuthCondition) *generated.ExternalAuthCondition {
-	return &generated.ExternalAuthCondition{
+func newExternalAuthCondition(from *api.ExternalAuthCondition) generated.ExternalAuthCondition {
+	if from == nil {
+		return generated.ExternalAuthCondition{}
+	}
+	return generated.ExternalAuthCondition{
 		Type:    api.PtrOrNil(generated.ExternalAuthConditionType(from.Type)),
 		Status:  api.PtrOrNil(generated.StatusType(from.Status)),
 		Reason:  api.PtrOrNil(from.Reason),
@@ -206,33 +209,64 @@ func newExternalAuthCondition(from *api.ExternalAuthCondition) *generated.Extern
 	}
 }
 
-func newTokenIssuerProfile(from *api.TokenIssuerProfile) *generated.TokenIssuerProfile {
-	return &generated.TokenIssuerProfile{
+func newTokenIssuerProfile(from *api.TokenIssuerProfile) generated.TokenIssuerProfile {
+	if from == nil {
+		return generated.TokenIssuerProfile{}
+	}
+	return generated.TokenIssuerProfile{
 		URL:       api.PtrOrNil(from.URL),
 		Audiences: api.StringSliceToStringPtrSlice(from.Audiences),
 		CA:        api.PtrOrNil(from.CA),
 	}
 }
 
-func newExternalAuthClaimProfile(from *api.ExternalAuthClaimProfile) *generated.ExternalAuthClaimProfile {
-	var groups *generated.GroupClaimProfile
-
-	if from.Mappings.Groups != nil {
-		groups = &generated.GroupClaimProfile{
-			Claim:  api.PtrOrNil(from.Mappings.Groups.Claim),
-			Prefix: api.PtrOrNil(from.Mappings.Groups.Prefix),
-		}
+func newExternalAuthClientComponent(from *api.ExternalAuthClientComponentProfile) generated.ExternalAuthClientComponentProfile {
+	if from == nil {
+		return generated.ExternalAuthClientComponentProfile{}
 	}
+	return generated.ExternalAuthClientComponentProfile{
+		Name:                api.PtrOrNil(from.Name),
+		AuthClientNamespace: api.PtrOrNil(from.AuthClientNamespace),
+	}
+}
 
-	return &generated.ExternalAuthClaimProfile{
-		Mappings: &generated.TokenClaimMappingsProfile{
-			Username: &generated.UsernameClaimProfile{
-				Claim:        api.PtrOrNil(from.Mappings.Username.Claim),
-				Prefix:       api.PtrOrNil(from.Mappings.Username.Prefix),
-				PrefixPolicy: api.PtrOrNil(generated.UsernameClaimPrefixPolicy(from.Mappings.Username.PrefixPolicy)),
-			},
-			Groups: groups,
-		},
+func newExternalAuthClaimProfile(from *api.ExternalAuthClaimProfile) generated.ExternalAuthClaimProfile {
+	if from == nil {
+		return generated.ExternalAuthClaimProfile{}
+	}
+	return generated.ExternalAuthClaimProfile{
+		Mappings: api.PtrOrNil(newTokenClaimMappingsProfile(&from.Mappings)),
+	}
+}
+
+func newTokenClaimMappingsProfile(from *api.TokenClaimMappingsProfile) generated.TokenClaimMappingsProfile {
+	if from == nil {
+		return generated.TokenClaimMappingsProfile{}
+	}
+	return generated.TokenClaimMappingsProfile{
+		Username: api.PtrOrNil(newUsernameClaimProfile(&from.Username)),
+		Groups:   api.PtrOrNil(newGroupClaimProfile(from.Groups)),
+	}
+}
+
+func newUsernameClaimProfile(from *api.UsernameClaimProfile) generated.UsernameClaimProfile {
+	if from == nil {
+		return generated.UsernameClaimProfile{}
+	}
+	return generated.UsernameClaimProfile{
+		Claim:        api.PtrOrNil(from.Claim),
+		Prefix:       api.PtrOrNil(from.Prefix),
+		PrefixPolicy: api.PtrOrNil(generated.UsernameClaimPrefixPolicy(from.PrefixPolicy)),
+	}
+}
+
+func newGroupClaimProfile(from *api.GroupClaimProfile) generated.GroupClaimProfile {
+	if from == nil {
+		return generated.GroupClaimProfile{}
+	}
+	return generated.GroupClaimProfile{
+		Claim:  api.PtrOrNil(from.Claim),
+		Prefix: api.PtrOrNil(from.Prefix),
 	}
 }
 
@@ -243,35 +277,22 @@ func (v version) NewHCPOpenShiftClusterExternalAuth(from *api.HCPOpenShiftCluste
 
 	out := &ExternalAuth{
 		generated.ExternalAuth{
-			ID:   api.PtrOrNil(from.ID),
-			Name: api.PtrOrNil(from.Name),
-			Type: api.PtrOrNil(from.Type),
+			ID:         api.PtrOrNil(from.ID),
+			Name:       api.PtrOrNil(from.Name),
+			Type:       api.PtrOrNil(from.Type),
+			SystemData: api.PtrOrNil(newSystemData(from.SystemData)),
 			Properties: &generated.ExternalAuthProperties{
 				ProvisioningState: api.PtrOrNil(generated.ExternalAuthProvisioningState(from.Properties.ProvisioningState)),
-				Condition:         newExternalAuthCondition(&from.Properties.Condition),
-				Claim:             newExternalAuthClaimProfile(&from.Properties.Claim),
+				Condition:         api.PtrOrNil(newExternalAuthCondition(&from.Properties.Condition)),
+				Issuer:            api.PtrOrNil(newTokenIssuerProfile(&from.Properties.Issuer)),
+				Claim:             api.PtrOrNil(newExternalAuthClaimProfile(&from.Properties.Claim)),
 			},
 		},
-	}
-	out.Properties.Issuer = newTokenIssuerProfile(&from.Properties.Issuer)
-
-	if from.SystemData != nil {
-		out.SystemData = &generated.SystemData{
-			CreatedBy:          api.PtrOrNil(from.SystemData.CreatedBy),
-			CreatedByType:      api.PtrOrNil(generated.CreatedByType(from.SystemData.CreatedByType)),
-			CreatedAt:          from.SystemData.CreatedAt,
-			LastModifiedBy:     api.PtrOrNil(from.SystemData.LastModifiedBy),
-			LastModifiedByType: api.PtrOrNil(generated.CreatedByType(from.SystemData.LastModifiedByType)),
-			LastModifiedAt:     from.SystemData.LastModifiedAt,
-		}
 	}
 
 	for _, client := range from.Properties.Clients {
 		out.Properties.Clients = append(out.Properties.Clients, &generated.ExternalAuthClientProfile{
-			Component: &generated.ExternalAuthClientComponentProfile{
-				Name:                api.PtrOrNil(client.Component.Name),
-				AuthClientNamespace: api.PtrOrNil(client.Component.AuthClientNamespace),
-			},
+			Component:   api.PtrOrNil(newExternalAuthClientComponent(&client.Component)),
 			ClientID:    api.PtrOrNil(client.ClientID),
 			ExtraScopes: api.StringSliceToStringPtrSlice(client.ExtraScopes),
 			Type:        api.PtrOrNil(generated.ExternalAuthClientType(client.Type)),
