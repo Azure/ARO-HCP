92eecb9b585c8f6b646a53b54b67dfd36a44261b
Gerd Oberlechner <goberlec@redhat.com>
2025-09-08 20:00:41 +0200

    prow job executor for service validation
    utility to kick off service validation PROW jobs. meant to be used within EV2 serviceValidation steps. but can also be used outside of EV2.
    
    the respective PROW token has aquired following the guide on https://docs.ci.openshift.org/docs/how-tos/triggering-prowjobs-via-rest/ (`oc -n aro-hcp-prow-ci extract secret/api-token-secret --to=- --keys=token`)

---
+++ test/execute-prow-job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Stop execution if dry run is enabled
+if [ "${DRY_RUN:-false}" = "true" ]; then
+  echo "DRY_RUN is enabled. Stopping execution before running PROW job."
+  exit 0
+fi
+
+# Start the Prow job
+./prow-job-executor execute --prow-token-keyvault-uri "https://${PROW_TOKEN_KEYVAULT}.${KEY_VAULT_DNSSUFFIX}" --prow-token-keyvault-secret "$PROW_TOKEN_SECRET" --job-name "$PROW_JOB_NAME" --region "$REGION" --ev2-rollout-version "${zz_injected_EV2RolloutVersion:-}"
--- tooling/templatize/go.mod
+++ tooling/templatize/go.mod
@@ -114,7 +114,7 @@ require (
 	github.com/rubenv/sql-migrate v1.8.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/santhosh-tekuri/jsonschema/v6 v6.0.2 // indirect
-	github.com/sergi/go-diff v1.3.1 // indirect
+	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
 	github.com/shopspring/decimal v1.4.0 // indirect
 	github.com/spf13/cast v1.7.0 // indirect
 	github.com/spf13/pflag v1.0.7 // indirect
