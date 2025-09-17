629c7afe4923f5a2ad2d13fbfda414da47136d38
Gerd Oberlechner <goberlec@redhat.com>
2025-09-17 13:15:13 +0200

    don't run the test if no job name is given
    but also don't fail. handy for DEV where we don't have an E2E test yet
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- test/execute-prow-job.sh
+++ test/execute-prow-job.sh
@@ -10,5 +10,11 @@ if [ "${DRY_RUN:-false}" = "true" ]; then
   exit 0
 fi
 
+if [ -z "${PROW_JOB_NAME:-}" ]; then
+  echo "PROW_JOB_NAME is not set. Exiting."
+  exit 0
+fi
+
+
 # Start the Prow job
 ./prow-job-executor execute --prow-token-keyvault-uri "https://${PROW_TOKEN_KEYVAULT}.${KEY_VAULT_DNSSUFFIX}" --prow-token-keyvault-secret "$PROW_TOKEN_SECRET" --job-name "$PROW_JOB_NAME" --region "$REGION" --ev2-rollout-version "${zz_injected_EV2RolloutVersion:-}"
