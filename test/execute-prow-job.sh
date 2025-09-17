f9b72f4c103ea80364f299ba219c2ed2ff79929f
Gerd Oberlechner <goberlec@redhat.com>
2025-09-17 12:11:14 +0200

    enable e2e tests for regional gating
    * enable test in topology
    * don't run test if no job name is given (for DEV)
    * update prow token
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- test/execute-prow-job.sh
+++ test/execute-prow-job.sh
@@ -10,5 +10,23 @@ if [ "${DRY_RUN:-false}" = "true" ]; then
   exit 0
 fi
 
-# Start the Prow job
+if [ -z "${PROW_JOB_NAME:-}" ]; then
+  echo "PROW_JOB_NAME is not set. Exiting."
+  exit 0
+fi
+
+echo "Starting Prow job: ${PROW_JOB_NAME}"
+set +o errexit
 ./prow-job-executor execute --prow-token-keyvault-uri "https://${PROW_TOKEN_KEYVAULT}.${KEY_VAULT_DNSSUFFIX}" --prow-token-keyvault-secret "$PROW_TOKEN_SECRET" --job-name "$PROW_JOB_NAME" --region "$REGION" --ev2-rollout-version "${zz_injected_EV2RolloutVersion:-}"
+JOB_EXIT_CODE=$?
+set -o errexit
+
+# Only fail the script if GATE_PROMOTION is set to true and the job failed
+if [ "${GATE_PROMOTION:-false}" = "true" ] && [ $JOB_EXIT_CODE -ne 0 ]; then
+  echo "GATE_PROMOTION is enabled and Prow job failed with exit code $JOB_EXIT_CODE. Failing the test."
+  exit $JOB_EXIT_CODE
+elif [ $JOB_EXIT_CODE -ne 0 ]; then
+  echo "Prow job failed with exit code $JOB_EXIT_CODE, but GATE_PROMOTION is not enabled. Continuing..."
+else
+  echo "Prow job completed successfully."
+fi
