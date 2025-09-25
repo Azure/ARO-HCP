52b53c50971d983cbd52d1e039d0d3049cc183ab
Roi Vazquez <roivazquez@gmail.com>
2025-09-25 20:56:27 +0200

    feat(test): refactor cleanup with idempotent resource group deletion
    - Restructure: move cleanup logic to test/util/cleanup/ for reusability
    - Add idempotent RG deletion: checks existence before cleanup
    - Maintain (temporary) backwards compatibility with delete-expired-resource-groups
    
    Prevents failures when cleanup tiers run sequentially and RGs are already deleted.

---
+++ test/util/cleanup/options.go
@@ -0,0 +1,35 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cleanup
+
+import "time"
+
+const defaultTimeout = 20 * time.Minute
+const defaultInterruptTimeout = 30 * time.Minute
+const defaultVerbosity = 0
+
+type BaseOptions struct {
+	Verbosity        int
+	Timeout          time.Duration
+	InterruptTimeout time.Duration
+}
+
+func NewBaseOptions() BaseOptions {
+	return BaseOptions{
+		Verbosity:        defaultVerbosity,
+		Timeout:          defaultTimeout,
+		InterruptTimeout: defaultInterruptTimeout,
+	}
+}
