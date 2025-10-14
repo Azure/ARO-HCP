fad0e5169e9c7bbebcbd1730b99872530ae8711d
David Eads <deads@redhat.com>
2025-10-14 18:11:35 -0400

    switch to use new nodepool validation over reflective

--- frontend/pkg/frontend/frontend.go
+++ frontend/pkg/frontend/frontend.go
@@ -1272,7 +1272,10 @@ func (f *Frontend) ArmDeploymentPreflight(writer http.ResponseWriter, request *h
 			}
 
 			// Perform static validation as if for a node pool creation request.
-			cloudError = api.ValidateVersionedHCPOpenShiftClusterNodePool(versionedNodePool, versionedNodePool, nil, false)
+			newInternalNodePool := &api.HCPOpenShiftClusterNodePool{}
+			versionedNodePool.Normalize(newInternalNodePool)
+			validationErrs := validation.ValidateNodePoolCreate(ctx, newInternalNodePool)
+			cloudError = arm.CloudErrorFromFieldErrors(validationErrs)
 
 		case strings.ToLower(api.ExternalAuthResourceType.String()):
 			// This is just "preliminary" validation to ensure all the base resource
