348304413ae056309be496d2b8bf901e8f70299a
Gerd Oberlechner <goberlec@redhat.com>
2025-10-31 13:57:47 +0100

    signal clean git worktree expectation and fail with an informative error
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- generate-tag.sh
+++ generate-tag.sh
@@ -15,6 +15,14 @@ tag=$(git rev-parse --short=7 HEAD)
 
 if [ -n "$(git status --porcelain --untracked-files=no)" ]
 then
+    # Fail if EXPECT_CLEAN_GIT_WORKTREE=1 and worktree is not clean
+    if [[ "${EXPECT_CLEAN_GIT_WORKTREE:-}" == "1" ]]
+    then
+        echo "Error: Git worktree is not clean and EXPECT_CLEAN_GIT_WORKTREE=1 is set" >&2
+        echo "The following modifications are present in the repository:" >&2
+        git status --porcelain >&2
+        exit 1
+    fi
     tag="${tag}-dirty"
 fi
 
