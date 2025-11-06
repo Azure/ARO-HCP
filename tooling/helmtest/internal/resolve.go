bd912791ad2e618be09b6e464be923c831ceeeb0
Jan-Hendrik Boll <jboll@redhat.com>
2025-11-06 10:12:24 +0000

    Recursively search in topology for helmsteps
    Add fixtures generated from helmsteps
    Remove not needed helmtest files and fixtures, replaced by TestFromHelmStep

---
+++ tooling/helmtest/internal/resolve.go
@@ -0,0 +1,91 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+	"strings"
+
+	"github.com/Azure/ARO-Tools/pkg/config"
+	"github.com/Azure/ARO-Tools/pkg/topology"
+	"github.com/Azure/ARO-Tools/pkg/types"
+)
+
+func FindHelmtestFiles() ([]string, error) {
+	allTests := make([]string, 0)
+	err := filepath.WalkDir(RepoRoot, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
+		if strings.HasPrefix(d.Name(), "helmtest_") {
+			allTests = append(allTests, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error walking directory: %v", err)
+	}
+	return allTests, nil
+}
+
+func FindHelmSteps() ([]HelmStepWithPath, error) {
+	cfg, err := loadConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error loading config: %v", err)
+	}
+	topo, err := topology.Load(filepath.Join(RepoRoot, "topology.yaml"))
+	if err != nil {
+		return nil, fmt.Errorf("error loading topology: %v", err)
+	}
+	allSteps := make([]HelmStepWithPath, 0)
+
+	for _, service := range topo.Services {
+		helmStepsWithPath, err := recursiveLoadPipelineReturnHelmSteps(service, cfg)
+		if err != nil {
+			return nil, fmt.Errorf("error loading pipeline: %v", err)
+		}
+		allSteps = append(allSteps, helmStepsWithPath...)
+	}
+
+	return allSteps, nil
+}
+
+func recursiveLoadPipelineReturnHelmSteps(service topology.Service, cfg config.Configuration) ([]HelmStepWithPath, error) {
+	pipeline, err := types.NewPipelineFromFile(filepath.Join(RepoRoot, service.PipelinePath), cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error loading pipeline: %v", err)
+	}
+	allSteps := make([]HelmStepWithPath, 0)
+	for _, resourceGroups := range pipeline.ResourceGroups {
+		for _, step := range resourceGroups.Steps {
+			if helmStep, ok := step.(*types.HelmStep); ok {
+				allSteps = append(allSteps, HelmStepWithPath{
+					HelmStep:     helmStep,
+					PipelinePath: service.PipelinePath,
+				})
+			}
+		}
+	}
+	for _, child := range service.Children {
+		helmStepsWithPath, err := recursiveLoadPipelineReturnHelmSteps(child, cfg)
+		if err != nil {
+			return nil, fmt.Errorf("error loading pipeline: %v", err)
+		}
+		allSteps = append(allSteps, helmStepsWithPath...)
+	}
+	return allSteps, nil
+}
