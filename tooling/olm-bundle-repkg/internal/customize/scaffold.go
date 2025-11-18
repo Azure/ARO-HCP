45055f6ed4fe8df03a7a1aebbb1e628b62645e79
tony-schndr <tony-schndr@users.noreply.github.com>
2025-11-18 13:54:15 -0600

    enhance olm-bundle-rpkg to support oadp-operator
    - added a scaffold directory to bring in additional manifests
    - enhanced the manifest override system to support json path manifest manipulation, array indexing/filtering, selective modification, etcd..

--- tooling/olm-bundle-repkg/internal/customize/scaffold.go
+++ tooling/olm-bundle-repkg/internal/customize/scaffold.go
@@ -18,12 +18,16 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	"sigs.k8s.io/yaml"
 )
 
+// LoadScaffoldTemplates loads manifest templates from the scaffold directory
+// It only loads files that can be parsed as valid YAML (i.e., NOT Helm templates)
+// Files in the "templates/" subdirectory are skipped as they should be raw Helm templates
 func LoadScaffoldTemplates(scaffoldDir string) ([]unstructured.Unstructured, error) {
 	// If no scaffold directory is provided, return empty slice
 	if scaffoldDir == "" {
@@ -36,10 +40,23 @@ func LoadScaffoldTemplates(scaffoldDir string) ([]unstructured.Unstructured, err
 	}
 
 	var manifests []unstructured.Unstructured
+	templatesDir := filepath.Join(scaffoldDir, "templates")
+	valuesFile := filepath.Join(scaffoldDir, "values.yaml")
+
 	err := filepath.Walk(scaffoldDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
+
+		if info.IsDir() && path == templatesDir {
+			return filepath.SkipDir
+		}
+
+		// Skip values.yaml - it's handled separately by LoadScaffoldValues
+		if path == valuesFile {
+			return nil
+		}
+
 		if !info.IsDir() && (filepath.Ext(path) == ".yaml" || filepath.Ext(path) == ".yml") {
 			fileContent, err := os.ReadFile(path)
 			if err != nil {
@@ -59,4 +76,55 @@ func LoadScaffoldTemplates(scaffoldDir string) ([]unstructured.Unstructured, err
 		return nil, err
 	}
 	return manifests, nil
+}
+
+// GetScaffoldTemplateFiles returns a map of scaffold template files to copy verbatim
+// These are files from the scaffold/templates/ directory that should be copied as-is
+func GetScaffoldTemplateFiles(scaffoldDir string) (map[string][]byte, error) {
+	if scaffoldDir == "" {
+		return make(map[string][]byte), nil
+	}
+
+	templatesDir := filepath.Join(scaffoldDir, "templates")
+
+	// Check if templates directory exists
+	if _, err := os.Stat(templatesDir); os.IsNotExist(err) {
+		return make(map[string][]byte), nil // Not an error - no templates directory
+	}
+
+	templates := make(map[string][]byte)
+	err := filepath.Walk(templatesDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		if filepath.Ext(path) != ".yaml" && filepath.Ext(path) != ".yml" {
+			return nil
+		}
+
+		fileContent, err := os.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(templatesDir, path)
+		if err != nil {
+			return err
+		}
+
+		if strings.HasPrefix(relPath, "..") {
+			return fmt.Errorf("invalid template path: %s (resolves to %s)", path, relPath)
+		}
+
+		templates[relPath] = fileContent
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return templates, nil
 }
