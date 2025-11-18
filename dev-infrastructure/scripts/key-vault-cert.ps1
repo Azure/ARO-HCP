712edb0014599e3d47ff95818753acee639f545a
Gerd Oberlechner <goberlec@redhat.com>
2025-11-18 10:29:36 +0100

    update certificate when policy parameters change
    the script now detects changes to certificate policy parameters
    (ValidityInMonths, IssuerName, etc.) and
    automatically regenerates the certificate when any of them differ from the
    existing policy. This ensures certificates are kept in sync with their
    policy definitions
    
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- dev-infrastructure/scripts/key-vault-cert.ps1
+++ dev-infrastructure/scripts/key-vault-cert.ps1
@@ -2,7 +2,7 @@ param (
     [string]$IssuerName,
 
     [string]$VaultName,
-    
+
     [string]$CertName,
 
     [string]$SubjectName,
@@ -23,6 +23,8 @@ param (
 try
 {
     Write-Output "`nUTC is: $(Get-Date)"
+
+    $updateCertificate = $Force
 
     $DNSNamesArray = $DnsNames -split '_'
 
@@ -60,25 +62,33 @@ try
         $Cert = Get-AzKeyVaultCertificate -VaultName $VaultName -Name $CertName
         If ($Cert)
         {
-            $Policy = $Cert | Get-AzKeyVaultCertificatePolicy | Where-Object SubjectName -EQ $SubjectName
-        }
+            $ExistingPolicy = $Cert | Get-AzKeyVaultCertificatePolicy | Where-Object SubjectName -EQ $SubjectName
 
-        if ($Policy)
-        {
-            Write-Warning -Message "Policy exists      [$($policy.SubjectName)]"
-            if ($Force)
+            # Check if policy parameters have changed
+            if ($ExistingPolicy)
             {
-                Write-Warning -Message "Force Policy [$($policy.SubjectName)] settings"
-                $Policy = New-AzKeyVaultCertificatePolicy @PolicyParams
+                Write-Warning -Message "Policy exists      [$($ExistingPolicy.SubjectName)]"
+
+                $policyChanged = (
+                    $ExistingPolicy.RenewAtPercentageLifetime -ne $RenewAtPercentageLifetime -or
+                    $ExistingPolicy.SecretContentType -ne $SecretContentType -or
+                    $ExistingPolicy.ValidityInMonths -ne $ValidityInMonths -or
+                    $ExistingPolicy.IssuerName -ne $IssuerName -or
+                    $ExistingPolicy.Enabled -eq $Disabled -or
+                    (Compare-Object -ReferenceObject $ExistingPolicy.DnsNames -DifferenceObject $DNSNamesArray)
+                )
+
+                if ($policyChanged)
+                {
+                    Write-Warning -Message "Policy parameters changed, certificate will be updated"
+                    $updateCertificate = $true
+                }
             }
         }
-        else
-        {
-            Write-Warning -Message "Creating Policy [$SubjectName]"
-            $Policy = New-AzKeyVaultCertificatePolicy @PolicyParams
-        }
 
-        if ($Cert -and (-not $Force))
+        $Policy = New-AzKeyVaultCertificatePolicy @PolicyParams
+
+        if ($Cert -and (-not $updateCertificate))
         {
             Write-Warning -Message "Certificate exists [$($Cert.Name)]"
         }
