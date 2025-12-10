12c4fa77ca0ab00fb5f12c26ba7a3c1b7a71e2fe
Andrii Klymenko <aklymenk@redhat.com>
2025-12-10 13:24:49 +0100

    Refactor E2E test 'clusters_sharing_resgroup.go' to use direct RP approach

--- test/e2e/clusters_sharing_resgroup.go
+++ test/e2e/clusters_sharing_resgroup.go
@@ -32,6 +32,7 @@ var _ = Describe("Customer", func() {
 
 	It("should be able to create several HCP clusters in their customer resource group, but not in the same managed resource group",
 		labels.RequireNothing,
+		labels.AroRpApiCompatible,
 		labels.Critical,
 		labels.Positive,
 		func(ctx context.Context) {
@@ -40,7 +41,6 @@ var _ = Describe("Customer", func() {
 				customerVnetName                 = "customer-vnet-name"
 				customerVnetSubnetName           = "customer-vnet-subnet1"
 				customerClusterName              = "basic-hcp-cluster"
-				openshiftControlPlaneVersionId   = "4.19"
 
 				customerNetworkSecurityGroupName2 = "customer-nsg2-name"
 				customerVnetName2                 = "customer-vnet2-name"
@@ -54,196 +54,73 @@ var _ = Describe("Customer", func() {
 			)
 			tc := framework.NewTestContext()
 
-			By("creating a managed customer resource group")
+			By("creating a shared customer resource group")
 			customerResourceGroup, err := tc.NewResourceGroup(ctx, "customer-rg", tc.Location())
 			Expect(err).NotTo(HaveOccurred())
 
-			By("creating a customer-infra")
-			customerInfraDeploymentResult, err := tc.CreateBicepTemplateAndWait(ctx,
-				*customerResourceGroup.Name,
-				"customer-infra",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/customer-infra.json")),
+			// First cluster
+			By("creating customer infrastructure and managed identities for first cluster")
+			clusterParams1 := framework.NewDefaultClusterParams()
+			clusterParams1.ClusterName = customerClusterName
+			clusterParams1.ManagedResourceGroupName = framework.SuffixName(*customerResourceGroup.Name, "-managed", 64)
+			clusterParams1, err = tc.CreateClusterCustomerResources(ctx, customerResourceGroup, clusterParams1,
 				map[string]interface{}{
 					"persistTagValue":        false,
 					"customerNsgName":        customerNetworkSecurityGroupName,
 					"customerVnetName":       customerVnetName,
 					"customerVnetSubnetName": customerVnetSubnetName,
 				},
-				45*time.Minute,
+				TestArtifactsFS,
 			)
 			Expect(err).NotTo(HaveOccurred())
 
-			By("creating a managed identities")
-			keyVaultName, err := framework.GetOutputValue(customerInfraDeploymentResult, "keyVaultName")
-			Expect(err).NotTo(HaveOccurred())
-			managedIdentityDeploymentResult, err := tc.CreateBicepTemplateAndWait(ctx,
-				*customerResourceGroup.Name,
-				"managed-identities",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/managed-identities.json")),
-				map[string]interface{}{
-					"clusterName":  customerClusterName,
-					"nsgName":      customerNetworkSecurityGroupName,
-					"vnetName":     customerVnetName,
-					"subnetName":   customerVnetSubnetName,
-					"keyVaultName": keyVaultName,
-				},
-				45*time.Minute,
-			)
+			By("creating the first cluster")
+			err = tc.CreateHCPClusterFromParam(ctx, *customerResourceGroup.Name, clusterParams1, 45*time.Minute)
 			Expect(err).NotTo(HaveOccurred())
 
-			By("creating the cluster")
-			userAssignedIdentities, err := framework.GetOutputValue(managedIdentityDeploymentResult, "userAssignedIdentitiesValue")
-			Expect(err).NotTo(HaveOccurred())
-			identity, err := framework.GetOutputValue(managedIdentityDeploymentResult, "identityValue")
-			Expect(err).NotTo(HaveOccurred())
-			etcdEncryptionKeyName, err := framework.GetOutputValue(customerInfraDeploymentResult, "etcdEncryptionKeyName")
-			Expect(err).NotTo(HaveOccurred())
-			managedResourceGroupName := framework.SuffixName(*customerResourceGroup.Name, "-managed", 64)
-			_, err = tc.CreateBicepTemplateAndWait(ctx,
-				*customerResourceGroup.Name,
-				"cluster",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/cluster.json")),
-				map[string]interface{}{
-					"openshiftVersionId":          openshiftControlPlaneVersionId,
-					"clusterName":                 customerClusterName,
-					"managedResourceGroupName":    managedResourceGroupName,
-					"nsgName":                     customerNetworkSecurityGroupName,
-					"subnetName":                  customerVnetSubnetName,
-					"vnetName":                    customerVnetName,
-					"userAssignedIdentitiesValue": userAssignedIdentities,
-					"identityValue":               identity,
-					"keyVaultName":                keyVaultName,
-					"etcdEncryptionKeyName":       etcdEncryptionKeyName,
-				},
-				45*time.Minute,
-			)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("creating a second customer-infra")
-			customerInfraDeploymentResult, err = tc.CreateBicepTemplateAndWait(ctx,
-				*customerResourceGroup.Name,
-				"customer-infra",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/customer-infra.json")),
+			// Second cluster
+			By("creating customer infrastructure and managed identities for second cluster")
+			clusterParams2 := framework.NewDefaultClusterParams()
+			clusterParams2.ClusterName = customerClusterName2
+			clusterParams2.ManagedResourceGroupName = framework.SuffixName(*customerResourceGroup.Name, "-managed-2", 64)
+			clusterParams2, err = tc.CreateClusterCustomerResources(ctx, customerResourceGroup, clusterParams2,
 				map[string]interface{}{
 					"persistTagValue":        false,
 					"customerNsgName":        customerNetworkSecurityGroupName2,
 					"customerVnetName":       customerVnetName2,
 					"customerVnetSubnetName": customerVnetSubnetName2,
 				},
-				45*time.Minute,
+				TestArtifactsFS,
 			)
 			Expect(err).NotTo(HaveOccurred())
 
-			By("creating a second managed identities")
-			keyVaultName, err = framework.GetOutputValue(customerInfraDeploymentResult, "keyVaultName")
-			Expect(err).NotTo(HaveOccurred())
-			managedIdentityDeploymentResult, err = tc.CreateBicepTemplateAndWait(ctx,
-				*customerResourceGroup.Name,
-				"managed-identities-2",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/managed-identities.json")),
-				map[string]interface{}{
-					"clusterName":  customerClusterName2,
-					"nsgName":      customerNetworkSecurityGroupName2,
-					"vnetName":     customerVnetName2,
-					"subnetName":   customerVnetSubnetName2,
-					"keyVaultName": keyVaultName,
-				},
-				45*time.Minute,
-			)
+			By("creating the second cluster with a different managed resource group")
+			err = tc.CreateHCPClusterFromParam(ctx, *customerResourceGroup.Name, clusterParams2, 45*time.Minute)
 			Expect(err).NotTo(HaveOccurred())
 
-			By("creating the second cluster")
-			userAssignedIdentities, err = framework.GetOutputValue(managedIdentityDeploymentResult, "userAssignedIdentitiesValue")
-			Expect(err).NotTo(HaveOccurred())
-			identity, err = framework.GetOutputValue(managedIdentityDeploymentResult, "identityValue")
-			Expect(err).NotTo(HaveOccurred())
-			etcdEncryptionKeyName, err = framework.GetOutputValue(customerInfraDeploymentResult, "etcdEncryptionKeyName")
-			Expect(err).NotTo(HaveOccurred())
-			managedResourceGroupName = framework.SuffixName(*customerResourceGroup.Name, "-managed-2", 64)
-			_, err = tc.CreateBicepTemplateAndWait(ctx,
-				*customerResourceGroup.Name,
-				"cluster-2",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/cluster.json")),
-				map[string]interface{}{
-					"openshiftVersionId":          openshiftControlPlaneVersionId,
-					"clusterName":                 customerClusterName2,
-					"managedResourceGroupName":    managedResourceGroupName,
-					"nsgName":                     customerNetworkSecurityGroupName2,
-					"subnetName":                  customerVnetSubnetName2,
-					"vnetName":                    customerVnetName2,
-					"userAssignedIdentitiesValue": userAssignedIdentities,
-					"identityValue":               identity,
-					"keyVaultName":                keyVaultName,
-					"etcdEncryptionKeyName":       etcdEncryptionKeyName,
-				},
-				45*time.Minute,
-			)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("creating a third customer-infra")
-			customerInfraDeploymentResult, err = tc.CreateBicepTemplateAndWait(ctx,
-				*customerResourceGroup.Name,
-				"customer-infra",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/customer-infra.json")),
+			// Third cluster (should fail)
+			By("creating customer infrastructure and managed identities for third cluster")
+			clusterParams3 := framework.NewDefaultClusterParams()
+			clusterParams3.ClusterName = customerClusterName3
+			clusterParams3.ManagedResourceGroupName = clusterParams2.ManagedResourceGroupName // Reuse cluster2's managed RG
+			clusterParams3, err = tc.CreateClusterCustomerResources(ctx, customerResourceGroup, clusterParams3,
 				map[string]interface{}{
 					"persistTagValue":        false,
 					"customerNsgName":        customerNetworkSecurityGroupName3,
 					"customerVnetName":       customerVnetName3,
 					"customerVnetSubnetName": customerVnetSubnetName3,
 				},
-				45*time.Minute,
+				TestArtifactsFS,
 			)
 			Expect(err).NotTo(HaveOccurred())
 
-			By("creating a third managed identities")
-			keyVaultName, err = framework.GetOutputValue(customerInfraDeploymentResult, "keyVaultName")
-			Expect(err).NotTo(HaveOccurred())
-			managedIdentityDeploymentResult, err = tc.CreateBicepTemplateAndWait(ctx,
-				*customerResourceGroup.Name,
-				"managed-identities-3",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/managed-identities.json")),
-				map[string]interface{}{
-					"clusterName":  customerClusterName3,
-					"nsgName":      customerNetworkSecurityGroupName3,
-					"vnetName":     customerVnetName3,
-					"subnetName":   customerVnetSubnetName3,
-					"keyVaultName": keyVaultName,
-				},
-				45*time.Minute,
-			)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("creating a third cluster in the same managed resource group as the previous one")
-			userAssignedIdentities, err = framework.GetOutputValue(managedIdentityDeploymentResult, "userAssignedIdentitiesValue")
-			Expect(err).NotTo(HaveOccurred())
-			identity, err = framework.GetOutputValue(managedIdentityDeploymentResult, "identityValue")
-			Expect(err).NotTo(HaveOccurred())
-			etcdEncryptionKeyName, err = framework.GetOutputValue(customerInfraDeploymentResult, "etcdEncryptionKeyName")
-			Expect(err).NotTo(HaveOccurred())
-			_, err = tc.CreateBicepTemplateAndWait(ctx,
-				*customerResourceGroup.Name,
-				"cluster-3",
-				framework.Must(TestArtifactsFS.ReadFile("test-artifacts/generated-test-artifacts/modules/cluster.json")),
-				map[string]interface{}{
-					"openshiftVersionId": openshiftControlPlaneVersionId,
-					"clusterName":        customerClusterName3,
-					// Here we're using the managed resource group from the previous cluster
-					"managedResourceGroupName":    managedResourceGroupName,
-					"nsgName":                     customerNetworkSecurityGroupName3,
-					"subnetName":                  customerVnetSubnetName3,
-					"vnetName":                    customerVnetName3,
-					"userAssignedIdentitiesValue": userAssignedIdentities,
-					"identityValue":               identity,
-					"keyVaultName":                keyVaultName,
-					"etcdEncryptionKeyName":       etcdEncryptionKeyName,
-				},
-				45*time.Minute,
-			)
+			By("attempting to create a third cluster using the same managed resource group as the second cluster")
+			err = tc.CreateHCPClusterFromParam(ctx, *customerResourceGroup.Name, clusterParams3, 45*time.Minute)
 			Expect(err).To(HaveOccurred())
 			Expect(err).To(MatchError(MatchRegexp("please provide a unique managed resource group name")))
 
-			By("Checking that the managed resource group still exists")
-			_, err = tc.GetARMResourcesClientFactoryOrDie(ctx).NewResourceGroupsClient().Get(ctx, managedResourceGroupName, nil)
+			By("verifying that the managed resource group still exists")
+			_, err = tc.GetARMResourcesClientFactoryOrDie(ctx).NewResourceGroupsClient().Get(ctx, clusterParams2.ManagedResourceGroupName, nil)
 			Expect(err).NotTo(HaveOccurred())
 		})
 })
