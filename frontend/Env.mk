77079ac594a916b8ab8c6ada69d68082f646e0ea
Miguel Soriano <msoriano@redhat.com>
2026-01-05 17:35:14 +0100

    fix: ensure needed variables for frontend make run target are set
    The run target of the frontend's Makefile was not working because
    some of the variables that it referenced were not being set correctly.
    
    This commit updates Frontend's Env.mk to ensure that those end up
    being resolved and resolved with the expected contents when it
    is included in the Makefile via the setup-templatize-env.mk in
    the project root.

--- frontend/Env.mk
+++ frontend/Env.mk
@@ -1,3 +1,5 @@
 LOCATION ?= {{ .region }}
 ARO_HCP_IMAGE_ACR ?= {{ .acr.svc.name }}
 FRONTEND_IMAGE_REPOSITORY ?= {{ .frontend.image.repository }}
+DB_NAME ?= {{ .frontend.cosmosDB.name }}
+DB_URL ?= $(shell az cosmosdb show -n {{ .frontend.cosmosDB.name }} -g {{ .regionRG }} --query documentEndpoint -o tsv)
