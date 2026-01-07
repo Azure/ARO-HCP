684e2f3d57e9a110384205ad5049306c8168bc46
Miguel Soriano <msoriano@redhat.com>
2026-01-07 10:11:38 +0100

    feat: add backend controller cluster inflights basic integration test

---
+++ test-integration/backend/controllers/clusterinflights/cluster_inflights_test.go
@@ -0,0 +1,58 @@
+package clusterinflights
+
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import (
+	"context"
+	"embed"
+	"io/fs"
+	"path"
+	"testing"
+
+	"github.com/Azure/ARO-HCP/backend/controllers"
+	"github.com/Azure/ARO-HCP/internal/api"
+	"github.com/Azure/ARO-HCP/internal/database"
+	"github.com/Azure/ARO-HCP/test-integration/utils/controllertesthelpers"
+	"github.com/Azure/ARO-HCP/test-integration/utils/integrationutils"
+)
+
+//go:embed artifacts/*
+var artifacts embed.FS
+
+func TestClusterInflightsController(t *testing.T) {
+	integrationutils.SkipIfNotSimulationTesting(t)
+
+	testCases := []controllertesthelpers.BasicControllerTest{
+		{
+			Name: "sync_cluster",
+			ControllerKey: controllers.HCPClusterKey{
+				SubscriptionID:    "4fa75980-6637-4157-9726-84d878a62e83",
+				ResourceGroupName: "shrillEffectiveness",
+				HCPClusterName:    "lavishUnhappiness",
+			},
+			ArtifactDir: api.Must(fs.Sub(artifacts, path.Join("artifacts"))),
+			ControllerInitializerFn: func(ctx context.Context, t *testing.T, cosmosClient database.DBClient) (controller controllers.Controller, testMemory map[string]any) {
+				return controllers.NewClusterInflightsController(cosmosClient), map[string]any{}
+			},
+			ControllerVerifierFn: func(ctx context.Context, t *testing.T, controller controllers.Controller, testMemory map[string]any) {
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, tc.RunTest)
+	}
+
+}
