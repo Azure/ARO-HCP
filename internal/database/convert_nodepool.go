6cf6014688d7ae3aaae23b058f00d3c75cc41ba4
David Eads <deads@redhat.com>
2026-01-13 19:24:34 -0500

    Add ResourceDocument serialization to non-inline.
    This is part of what is required in order to switch to generic storage
    because we have colliding tags.  We will serialize all items in a
    read/write cycle and then remove the inline serialization and read/write
    again and then finally add the generic serialization.

--- internal/database/convert_nodepool.go
+++ internal/database/convert_nodepool.go
@@ -15,6 +15,7 @@
 package database
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/Azure/ARO-HCP/internal/api"
@@ -36,7 +37,7 @@ func InternalToCosmosNodePool(internalObj *api.HCPOpenShiftClusterNodePool) (*No
 			ResourceType: internalObj.ID.ResourceType.String(),
 		},
 		NodePoolProperties: NodePoolProperties{
-			ResourceDocument: ResourceDocument{
+			ResourceDocument: &ResourceDocument{
 				ResourceID:        internalObj.ID,
 				InternalID:        internalObj.ServiceProviderProperties.ClusterServiceID,
 				ActiveOperationID: internalObj.ServiceProviderProperties.ActiveOperationID,
@@ -50,6 +51,7 @@ func InternalToCosmosNodePool(internalObj *api.HCPOpenShiftClusterNodePool) (*No
 			},
 		},
 	}
+	cosmosObj.IntermediateResourceDoc = cosmosObj.ResourceDocument
 
 	// some pieces of data in the internalNodePool conflict with ResourceDocument fields.  We may evolve over time, but for
 	// now avoid persisting those.
@@ -71,6 +73,13 @@ func CosmosToInternalNodePool(cosmosObj *NodePool) (*api.HCPOpenShiftClusterNode
 	if cosmosObj == nil {
 		return nil, nil
 	}
+	resourceDoc := cosmosObj.ResourceDocument
+	if resourceDoc == nil {
+		resourceDoc = cosmosObj.IntermediateResourceDoc
+	}
+	if resourceDoc == nil {
+		return nil, fmt.Errorf("resource document cannot be nil")
+	}
 
 	tempInternalAPI := cosmosObj.InternalState.InternalAPI
 	internalObj := &tempInternalAPI
@@ -78,21 +87,21 @@ func CosmosToInternalNodePool(cosmosObj *NodePool) (*api.HCPOpenShiftClusterNode
 	// some pieces of data are stored on the ResourceDocument, so we need to restore that data
 	internalObj.TrackedResource = arm.TrackedResource{
 		Resource: arm.Resource{
-			ID:         cosmosObj.ResourceID,
-			Name:       cosmosObj.ResourceID.Name,
-			Type:       cosmosObj.ResourceID.ResourceType.String(),
-			SystemData: cosmosObj.SystemData,
+			ID:         resourceDoc.ResourceID,
+			Name:       resourceDoc.ResourceID.Name,
+			Type:       resourceDoc.ResourceID.ResourceType.String(),
+			SystemData: resourceDoc.SystemData,
 		},
 		Location: cosmosObj.InternalState.InternalAPI.Location,
-		Tags:     cosmosObj.Tags,
+		Tags:     resourceDoc.Tags,
 	}
-	internalObj.Identity = toInternalIdentity(cosmosObj.Identity)
-	internalObj.Properties.ProvisioningState = cosmosObj.ProvisioningState
-	internalObj.SystemData = cosmosObj.SystemData
-	internalObj.Tags = copyTags(cosmosObj.Tags)
+	internalObj.Identity = toInternalIdentity(resourceDoc.Identity)
+	internalObj.Properties.ProvisioningState = resourceDoc.ProvisioningState
+	internalObj.SystemData = resourceDoc.SystemData
+	internalObj.Tags = copyTags(resourceDoc.Tags)
 	internalObj.ServiceProviderProperties.CosmosUID = cosmosObj.ID
-	internalObj.ServiceProviderProperties.ClusterServiceID = cosmosObj.InternalID
-	internalObj.ServiceProviderProperties.ActiveOperationID = cosmosObj.ActiveOperationID
+	internalObj.ServiceProviderProperties.ClusterServiceID = resourceDoc.InternalID
+	internalObj.ServiceProviderProperties.ActiveOperationID = resourceDoc.ActiveOperationID
 
 	return internalObj, nil
 }
