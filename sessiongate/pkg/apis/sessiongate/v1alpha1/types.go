ff4811eb418acd1f4e607163d34b92deae869ebc
Gerd Oberlechner <goberlec@redhat.com>
2026-01-15 09:06:35 -0800

    kubebuilder/codegen annotations on the right level
    Signed-off-by: Gerd Oberlechner <goberlec@redhat.com>

--- sessiongate/pkg/apis/sessiongate/v1alpha1/types.go
+++ sessiongate/pkg/apis/sessiongate/v1alpha1/types.go
@@ -1,18 +1,16 @@
-/*
-Copyright 2017 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package v1alpha1
 
@@ -26,70 +24,84 @@ import (
 type Session struct {
 	metav1.TypeMeta `json:",inline"`
 
-	// metadata is a standard object metadata
 	// +optional
+
+	// metadata is a standard object metadata
 	metav1.ObjectMeta `json:"metadata,omitempty,omitzero"`
 
-	// spec defines the desired state of Session
 	// +required
+
+	// spec defines the desired state of Session
 	Spec SessionSpec `json:"spec"`
 
-	// status defines the observed state of Session
 	// +optional
+
+	// status defines the observed state of Session
 	Status SessionStatus `json:"status,omitempty,omitzero"`
 }
 
 // +kubebuilder:validation:XValidation:rule="self == oldSelf",message="spec is immutable"
+
 type SessionSpec struct {
-	// ttl is the time-to-live duration for the session
 	// +kubebuilder:validation:Required
+
+	// ttl is the time-to-live duration for the session
 	TTL metav1.Duration `json:"ttl"`
 
-	// managementCluster specifies the AKS management cluster
 	// +kubebuilder:validation:Required
+
+	// managementCluster specifies the AKS management cluster
 	ManagementCluster ManagementCluster `json:"managementCluster"`
 
-	// hostedControlPlane specifies the hosted control plane
 	// +kubebuilder:validation:Required
+
+	// hostedControlPlane specifies the hosted control plane
 	HostedControlPlane HostedControlPlane `json:"hostedControlPlane"`
 
-	// accessLevel defines the access permissions for the session
 	// +kubebuilder:validation:Required
+
+	// accessLevel defines the access permissions for the session
 	AccessLevel AccessLevel `json:"accessLevel"`
 
-	// owner identifies the principal (user or service account) that owns this session
 	// +kubebuilder:validation:Required
+
+	// owner identifies the principal (user or service account) that owns this session
 	Owner Principal `json:"owner"`
 }
 
 // ManagementCluster identifies an Azure management cluster
 type ManagementCluster struct {
-	// resourceId is the Azure resource ID of the management cluster
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Pattern=`^/subscriptions/[a-fA-F0-9-]+/resourceGroups/[^/]+/providers/[^/]+/[^/]+/[^/]+$`
+
+	// resourceId is the Azure resource ID of the management cluster
 	ResourceID string `json:"resourceId"`
 }
 
 // HostedCluster identifies an hosted cluster
 type HostedControlPlane struct {
-	// resourceId is the Azure resource ID of the hosted cluster
 	// +kubebuilder:validation:Required
 	// +kubebuilder:validation:Pattern=`^/subscriptions/[a-fA-F0-9-]+/resourceGroups/[^/]+/providers/[^/]+/[^/]+/[^/]+$`
+
+	// resourceId is the Azure resource ID of the hosted cluster
 	ResourceID string `json:"resourceId"`
 
-	// namespace of the HostedControlPlane CR
 	// +kubebuilder:validation:Required
+
+	// namespace of the HostedControlPlane CR
 	Namespace string `json:"namespace,omitempty"`
 }
 
 type AccessLevel struct {
-	// group is the name of the access group
 	// +kubebuilder:validation:Required
+
+	// group is the name of the access group
 	Group string `json:"group"`
 }
 
-// PrincipalType defines the type of principal for authentication
 // +kubebuilder:validation:Enum=User
+
+// PrincipalType defines the type of principal for authentication
 type PrincipalType string
 
 const (
@@ -97,74 +109,71 @@ const (
 	PrincipalTypeUser PrincipalType = "User"
 )
 
-// Principal identifies the authenticated entity that owns this session
 // +kubebuilder:validation:XValidation:rule="self.type == 'User' ? has(self.userPrincipal) : true",message="userPrincipal must be set when type is User"
+
+// Principal identifies the authenticated entity that owns this session
 type Principal struct {
-	// type specifies the authentication method
 	// +kubebuilder:validation:Required
+
+	// type specifies the authentication method
 	Type PrincipalType `json:"type"`
+
+	// +optional
 
 	// userPrincipal identifies the user principal
 	// Required when type is User
-	// +optional
 	UserPrincipal *UserPrincipal `json:"userPrincipal,omitempty"`
 }
 
 // UserPrincipal represents a user identity
 type UserPrincipal struct {
-	// name is the user principal name (e.g., UPN for Azure AD like user@domain.com)
 	// +kubebuilder:validation:Required
+
+	// name is the user principal name (e.g., UPN for Azure AD like user@domain.com)
 	Name string `json:"name"`
 
-	// claim specifies which JWT claim to use for authentication (e.g., "upn", "email", "sub")
 	// +kubebuilder:validation:Required
-	// +kubebuilder:default="upn"
+
+	// claim specifies which JWT claim to use for authentication (e.g., "upn", "email", "sub")
 	Claim string `json:"claim"`
 }
 
 type SessionStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	// conditions represent the current state of the Session resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	// - "Credentials": credentials are being provisioned or ready
-	//
-	// The status of each condition is one of True, False, or Unknown.
+	// +optional
 	// +listType=map
 	// +listMapKey=type
-	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+
+	// The status of each condition is one of True, False, or Unknown.
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
 
-	// expiresAt is the timestamp when the session will expire
 	// +optional
+
+	// expiresAt is the timestamp when the session will expire
 	ExpiresAt *metav1.Time `json:"expiresAt,omitempty"`
 
-	// endpoint is the URL endpoint for accessing the session
 	// +optional
+
+	// endpoint is the URL endpoint for accessing the session
 	Endpoint string `json:"endpoint,omitempty"`
 
-	// authorizationPolicyRef references the AuthorizationPolicy resource
 	// +optional
+
+	// authorizationPolicyRef references the AuthorizationPolicy resource
 	AuthorizationPolicyRef string `json:"authorizationPolicyRef,omitempty"`
 
-	// credentialsSecretRef references the Secret containing the session credentials
 	// +optional
+
+	// credentialsSecretRef references the Secret containing the session credentials
 	CredentialsSecretRef string `json:"credentialsSecretRef,omitempty"`
 
 	// csrRef references the CertificateSigningRequest resource
 	CSRRef string `json:"csrRef,omitempty"`
 
-	// backendKASURL is the Kubernetes API server URL for the backend cluster
 	// +optional
+
+	// backendKASURL is the Kubernetes API server URL for the backend cluster
 	BackendKASURL string `json:"backendKASURL,omitempty"`
 }
 
