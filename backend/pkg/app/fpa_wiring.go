eac960a8c1ed80c6a64e3d61404e6ea34cba3df8
Miguel Soriano <msoriano@redhat.com>
2026-01-16 10:29:44 +0100

    refactor: move backend entrypoint to cmd and pkg/app packages
    The main.go file was too big and in the root directory of the
    backend component. This commit refactors the code organization
    of the entrypoint of the command. Specifically, it leaves
    backend/main.go as a thin wrapper and then it moves the CLI scaffolding
    to backend/cmd directory, and the entrypoint of the backend logic
    in backend/pkg/app/backend.go. It also separates some of the instantiation
    logic of some components that are used when starting backend into
    its own files in backend/pkg/app/.
    
    Flag parsing and validation is performed in backend/cmd, and then
    those are converted to BackendOptions which are pased to instantiate
    a new instance of Backend, which is then run from backend/cmd.
    
    The validation of flags is done at the purely syntactical level. Deeper
    validations are performed in backend/pkg/app itself like for example
    using the flag value to read a file and validate that the file contents
    are the expected ones.

---
+++ backend/pkg/app/fpa_wiring.go
@@ -0,0 +1,53 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	azureconfig "github.com/Azure/ARO-HCP/backend/pkg/azure/config"
+	"github.com/Azure/ARO-HCP/internal/fpa"
+
+	azureclient "github.com/Azure/ARO-HCP/backend/pkg/azure/client"
+)
+
+func getFPAClientBuilder(
+	ctx context.Context, logger *slog.Logger, fpaCertBundlePath string, fpaClientID string,
+	azureConfig azureconfig.AzureConfig,
+) (azureclient.FPAClientBuilder, error) {
+	if len(fpaCertBundlePath) == 0 || len(fpaClientID) == 0 {
+		return nil, nil
+	}
+
+	// Create FPA TokenCredentials with watching
+	certReader, err := fpa.NewWatchingFileCertificateReader(
+		ctx,
+		fpaCertBundlePath,
+		1*time.Minute,
+		logger,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create certificate reader: %w", err)
+	}
+
+	// We create the FPA token credential retriever here. Then we pass it to the cluster inflights controller,
+	// which then is used to instantiate a validation that uses the FPA token credential retriever. And then the
+	// validations uses the retriever to retrieve a token credential based on the information associated to the
+	// cluster(the tenant of the cluster, the subscription id, ...)
+	fpaTokenCredRetriever, err := fpa.NewFirstPartyApplicationTokenCredentialRetriever(
+		logger,
+		fpaClientID,
+		certReader,
+		azureConfig.CloudEnvironment.AZCoreClientOptions(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create FPA token credential retriever: %w", err)
+	}
+
+	fpaClientBuilder := azureclient.NewFPAClientBuilder(
+		fpaTokenCredRetriever, azureConfig.CloudEnvironment.ARMClientOptions(),
+	)
+
+	return fpaClientBuilder, nil
+}
