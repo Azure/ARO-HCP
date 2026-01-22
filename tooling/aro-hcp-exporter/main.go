b413d1174e22bfbe2d966fd6e33282a2f4ca01c8
Jan-Hendrik Boll <jboll@redhat.com>
2026-01-22 05:42:04 +0000

    Add aro-hcp-exporter
    Add an exporter for ARO HCP.
    
    It is supposed to fetch data from graphapi and thus make adding more
    data fairly easy.

---
+++ tooling/aro-hcp-exporter/main.go
@@ -0,0 +1,82 @@
+// Copyright 2025 Microsoft Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/dusted-go/logging/prettylog"
+	"github.com/go-logr/logr"
+	"github.com/spf13/cobra"
+
+	"github.com/Azure/ARO-HCP/tooling/aro-hcp-exporter/internal/cmd"
+)
+
+func main() {
+	logger := createLogger(0)
+
+	// Create a root context with the logger and signal handling
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	var logVerbosity int
+
+	rootCmd := &cobra.Command{
+		Use:   "aro-hcp-exporter",
+		Short: "ARO-HCP Prometheus resource exporter",
+		Long: `aro-hcp-exporter provides Prometheus metrics for ARO-HCP resources.
+
+This tool exposes metrics via HTTP that can be scraped by Prometheus.`,
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			ctx = logr.NewContext(ctx, createLogger(logVerbosity))
+			cmd.SetContext(ctx)
+		},
+		SilenceUsage:     true,
+		SilenceErrors:    true,
+		TraverseChildren: true,
+		CompletionOptions: cobra.CompletionOptions{
+			HiddenDefaultCmd: true,
+		},
+	}
+
+	rootCmd.PersistentFlags().IntVarP(&logVerbosity, "verbosity", "v", 0, "set the verbosity level")
+
+	// Add serve command
+	serveCmd, err := cmd.NewServeCommand()
+	if err != nil {
+		os.Exit(1)
+	}
+	rootCmd.AddCommand(serveCmd)
+
+	rootCmd.SetHelpCommand(&cobra.Command{Hidden: true})
+
+	if err := rootCmd.ExecuteContext(ctx); err != nil {
+		logger.Error(err, "command failed")
+		os.Exit(1)
+	}
+}
+
+func createLogger(verbosity int) logr.Logger {
+	prettyHandler := prettylog.NewHandler(&slog.HandlerOptions{
+		Level:       slog.Level(verbosity * -1),
+		AddSource:   false,
+		ReplaceAttr: nil,
+	})
+	return logr.FromSlogHandler(prettyHandler)
+}
