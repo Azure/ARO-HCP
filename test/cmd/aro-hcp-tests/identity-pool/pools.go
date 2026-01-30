a9c70ac3e1c2dd1e1b23e158a781331048e805f9
Roi Vazquez <roivazquez@gmail.com>
2026-01-30 14:07:25 +0100

    Adjust pools

--- test/cmd/aro-hcp-tests/identity-pool/pools.go
+++ test/cmd/aro-hcp-tests/identity-pool/pools.go
@@ -7,29 +7,40 @@ type identityPool struct {
 	SubscriptionIDHash    string
 }
 
+// Pool size calculations are based on the limit of role assignments per subscription (4000)
+//
+// Rules for pool size calculation:
+// * Each HCP created in resourceGroupScope mode consumes 24 role assignments
+// * Each HCP created in resourceScope mode consumes 41 role assignments
+// * The e2e suite runs all tests using resourceGroupScope except for one test, which uses resourceScope
+// * Leave at least 100 role assignments free for other things
+//
+// max-concurrency = floor(3900 / (((suite-parallelism - 1) * 24) + 41))
+// pool-size = max-concurrency * suite-parallelism
+
 var identityPoolMapping = map[string]identityPool{
 	"dev": {
 		ResourceGroupBaseName: "aro-hcp-test-msi-containers-dev",
 		Location:              "westus3",
 		SubscriptionIDHash:    "f5ead0cb5023266042158b287cc43d43e037bc009fb010d3c6efa596b9e18d47",
-		Size:                  140,
+		Size:                  150,
 	},
 	"int": {
 		ResourceGroupBaseName: "aro-hcp-test-msi-containers-int",
 		Location:              "uksouth",
 		SubscriptionIDHash:    "25aa33440faa44e53b4e36694bf34b27d67104550f3263bec102c52fafe46191",
-		Size:                  140,
+		Size:                  150,
 	},
 	"stg": {
 		ResourceGroupBaseName: "aro-hcp-test-msi-containers-stg",
 		Location:              "uksouth",
 		SubscriptionIDHash:    "1234567890",
-		Size:                  140,
+		Size:                  150,
 	},
 	"prod": {
 		ResourceGroupBaseName: "aro-hcp-test-msi-containers-prod",
 		Location:              "uksouth",
 		SubscriptionIDHash:    "1234567890",
-		Size:                  140,
+		Size:                  150,
 	},
 }
