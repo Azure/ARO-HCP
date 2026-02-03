2cfcc8699c4e34cea067ab42489f5bbe14d1a55d
David Eads <deads@redhat.com>
2026-02-03 17:45:16 -0500

    increase the partition count for cosmos emulator
    Default reference here https://www.google.com/search?client=firefox-b-1-d&q=default+cosmos+emulator+partition+count

--- test-integration/hack/start-cosmos-emulator.sh
+++ test-integration/hack/start-cosmos-emulator.sh
@@ -12,7 +12,9 @@ RESTART_EXISTING_EMULATOR="${RESTART_EXISTING_EMULATOR:-false}"
 
 # Number of partitions to use for the emulator
 # Increase if a lot of tests run in parallel and start failing with 503 errors
-PARTITION_COUNT="${PARTITION_COUNT:-15}"
+# AI claims
+#   The default total partition count for the Azure Cosmos DB emulator is 25.  Increasing
+PARTITION_COUNT="${PARTITION_COUNT:-50}"
 
 RUNNING_CONTAINER=$(get_running_emulator_container_name)
 if [ -n "${RUNNING_CONTAINER}" ]; then
