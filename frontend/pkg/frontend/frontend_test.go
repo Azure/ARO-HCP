befec78dd319e5e036c64fc3e3e3451f9aa1ddd5
Matthew Barnes <mbarnes@fedorapeople.org>
2026-02-16 12:37:38 -0500

    validation: Node pool version ID is now unconditionally required
    Update integration and unit tests to compensate.

--- frontend/pkg/frontend/frontend_test.go
+++ frontend/pkg/frontend/frontend_test.go
@@ -440,6 +440,7 @@ func TestDeploymentPreflight(t *testing.T) {
 				"apiVersion": api.TestAPIVersion,
 				"properties": map[string]any{
 					"version": map[string]any{
+						"id":           "4.20.1",
 						"channelGroup": "stable",
 					},
 					"platform": map[string]any{
@@ -458,6 +459,7 @@ func TestDeploymentPreflight(t *testing.T) {
 				"apiVersion": api.TestAPIVersion,
 				"properties": map[string]any{
 					"version": map[string]any{
+						// 1 missing required field
 						"channelGroup": "stable",
 					},
 					"platform": map[string]any{
@@ -478,6 +480,7 @@ func TestDeploymentPreflight(t *testing.T) {
 			},
 			expectStatus: arm.DeploymentPreflightStatusFailed,
 			expectErrors: []expectedPreflightError{
+				{message: "Required value", target: "properties.version.id"},
 				{message: "Required value", target: "properties.platform.vmSize"},
 				{message: "Invalid value: 1: must be greater than or equal to 3", target: "properties.autoScaling.max"},
 				{message: "Unsupported value: \"NoTouchy\": supported values: \"NoExecute\", \"NoSchedule\", \"PreferNoSchedule\"", target: "properties.taints[0].effect"},
