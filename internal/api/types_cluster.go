d1c767aa9b01b3ef1f183bf58bea593893ba0722
David Eads <deads@redhat.com>
2026-02-27 11:42:00 -0500

    add ManagedIdentitiesDataPlaneIdentityURL for cluster resource

--- internal/api/types_cluster.go
+++ internal/api/types_cluster.go
@@ -66,6 +66,16 @@ type HCPOpenShiftClusterServiceProviderProperties struct {
 	// ExperimentalFeatures captures experimental feature state evaluated from
 	// AFEC and per-resource tags. Stored in Cosmos but NOT exposed via ARM API.
 	ExperimentalFeatures ExperimentalFeatures `json:"experimentalFeatures,omitzero"`
+
+	// ManagedIdentitiesDataPlaneIdentityURL is the Managed Identities Data Plane
+	// Identity URL associated with the cluster. It is the URL that will be used
+	// to communicate with the Managed Identities Resource Provider (MI RP).
+	// When an ARO-HCP Cluster is created, ARM sends a HTTP header X-Ms-Identity-Url
+	// that contains the cluster's identity url. For ARO-HCP environments where
+	// the Managed Identities Dataplane service is not available the http header
+	// is set to a dummy value by our tools/testsuites/developers when
+	// creating ARO-HCP Clusters
+	ManagedIdentitiesDataPlaneIdentityURL string `json:"managedIdentitiesDataPlaneIdentityURL,omitempty"`
 }
 
 // VersionProfile represents the cluster control plane version.
