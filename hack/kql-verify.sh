f0a247fb3cf6023aeea3220ac771f004d1ad5dd0
Jan-Hendrik Boll <jboll@redhat.com>
2026-04-23 06:34:32 +0000

    Fix date

--- hack/kql-verify.sh
+++ hack/kql-verify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2025 Microsoft Corporation
+# Copyright 2026 Microsoft Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
