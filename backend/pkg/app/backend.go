7ade4f0f7a796ad0daf83a2e244978112405e5d9
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2026-05-06 01:12:59 -0400

    feat: add cluster service create controller to backend
    ClusterServiceCreateCluster controller will send a create request to
    Clusters Service if the control plane cluster has not yet been created.

--- backend/pkg/app/backend.go
+++ backend/pkg/app/backend.go
@@ -406,6 +406,11 @@ func (b *Backend) runBackendControllersUnderLeaderElection(ctx context.Context, 
 		b.options.ClustersServiceClient,
 		activeOperationInformer,
 	)
+	dispatchClusterCreateController := operationcontrollers.NewDispatchClusterCreateController(
+		b.options.ResourcesDBClient,
+		b.options.ClustersServiceClient,
+		activeOperationInformer,
+	)
 	operationClusterCreateController := operationcontrollers.NewOperationClusterCreateController(
 		b.options.ResourcesDBClient,
 		b.options.ClustersServiceClient,
@@ -611,6 +616,7 @@ func (b *Backend) runBackendControllersUnderLeaderElection(ctx context.Context, 
 				go doNothingController.Run(ctx, 20)
 				go dispatchRequestCredentialController.Run(ctx, 20)
 				go dispatchRevokeCredentialsController.Run(ctx, 20)
+				go dispatchClusterCreateController.Run(ctx, 20)
 				go operationClusterCreateController.Run(ctx, 20)
 				go operationClusterUpdateController.Run(ctx, 20)
 				go operationClusterDeleteController.Run(ctx, 20)
