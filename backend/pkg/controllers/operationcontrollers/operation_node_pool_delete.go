d5ec851b09ecb8707e367e35f90ab0cbb271029f
Miguel Soriano <msoriano@redhat.com>
2026-05-12 20:52:21 +0200

    add comments on pending steps for nodepool operation controller

--- backend/pkg/controllers/operationcontrollers/operation_node_pool_delete.go
+++ backend/pkg/controllers/operationcontrollers/operation_node_pool_delete.go
@@ -104,6 +104,24 @@ func (c *operationNodePoolDelete) SynchronizeOperation(ctx context.Context, key 
 		return nil // no work to do
 	}
 
+	// TODO we are thinking on leeaving operationNodePoolDelete to just wait until deletion is requested and the Cosmos
+	// NodePool document is deleted by another controller that takes care of that. Then at that point this controller
+	// would set the operation to completed. On the frontend side we would then stop setting CSInternalID on delete.
+	// However, right now this controller does the following:
+	// - It periodically checks the NodePool status from CS side and updates the operation status based on that. This
+	//   includes both terminal and non-terminal states and setting to a terminal state ends the controller handling it
+	// - It performs deletion of direct descendent child cosmos resources (like NodePool-scoped ManagementClusterContents and ServiceProviderNodePool)
+	//   Luckily it seems to list the direct descendent child cosmos resources it doesn't need the parent cosmos resource
+	//   id to exists in cosmos, only the resourceid string is needed, and we can obtain it from operation.ExternalID.
+	//  With the approach mentioned above that behavior changes and it's also not possible anymore to do do the first
+	//  step of checking the NodePool status from CS side and updating the operation status based on that. If we just
+	//  stop doing that part we would go from an operation in Accepted state (what the frontend currently sets when
+	//  it creates a NodePool delete cosmos operation) to a Succeeded state. There wouldn't be changes inbetween, the
+	//  operation wouldn't change between non terminal states and if CS nodepool deletion fails the operation would remain
+	//  in Accepted state. We need to decide what do we want to do about this. Regarding
+	//  the second step of deleting the direct descendent child cosmos resources, we could potentially move it outside
+	//  here but I think we could do it gradually and keep it at first here to reduce the amount of changes needed. Also
+	//  to discuss.
 	nodePoolStatus, err := c.clusterServiceClient.GetNodePoolStatus(ctx, operation.InternalID)
 	var ocmGetNodePoolError *ocmerrors.Error
 	if err != nil && errors.As(err, &ocmGetNodePoolError) && ocmGetNodePoolError.Status() == http.StatusNotFound {
