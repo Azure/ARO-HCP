842fc66a40cae3e273d552955ac9784efa9fd509
Drake Seifert <drakeseifert1@gmail.com>
2026-06-01 23:33:45 -0700

    test(e2e): improve z-stream test cases and observability

--- test/util/framework/constants.go
+++ test/util/framework/constants.go
@@ -33,8 +33,9 @@ const (
 
 // Resource Update timeouts
 const (
-	HCPClusterVersionUpgradeTimeout = 45 * time.Minute
-	NodePoolVersionUpgradeTimeout   = 45 * time.Minute
-	NodePoolScalingTimeout          = 20 * time.Minute
-	UpdateHCPClusterTimeout         = 10 * time.Minute
+	HCPClusterVersionUpgradeTriggeredTimeout = 15 * time.Minute
+	HCPClusterVersionUpgradeTimeout          = 45 * time.Minute
+	NodePoolVersionUpgradeTimeout            = 45 * time.Minute
+	NodePoolScalingTimeout                   = 20 * time.Minute
+	UpdateHCPClusterTimeout                  = 10 * time.Minute
 )
