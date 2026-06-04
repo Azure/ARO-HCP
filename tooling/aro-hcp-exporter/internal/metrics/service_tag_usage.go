8d9c28b5de5ce5df5ebc6e897deb3628485e9cf7
Giulio Frasca <gfrasca@redhat.com>
2026-06-04 01:13:35 -0400

    chore: migrate aro-hcp-exporter and tenant-quota to shared modules
    Replace inline cache and subscription resolver implementations in
    aro-hcp-exporter and tenant-quota with the new metricscache and azutils
    shared modules. Removes ~540 lines of duplicated code.
    
    Signed-off-by: Giulio Frasca <gfrasca@redhat.com>

--- tooling/aro-hcp-exporter/internal/metrics/service_tag_usage.go
+++ tooling/aro-hcp-exporter/internal/metrics/service_tag_usage.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/go-logr/logr"
@@ -26,8 +27,9 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 
-	"github.com/Azure/ARO-HCP/tooling/aro-hcp-exporter/pkg/cache"
 	"github.com/Azure/ARO-HCP/tooling/aro-hcp-exporter/pkg/graphquery"
+	"github.com/Azure/ARO-HCP/tooling/azutils/subscriptions"
+	"github.com/Azure/ARO-HCP/tooling/metricscache"
 )
 
 const (
@@ -47,7 +49,7 @@ var (
 // ServiceTagUsageCollector is a Prometheus collector that gathers public IP metrics from Azure
 type ServiceTagUsageCollector struct {
 	client       *graphquery.ResourceGraphClient
-	cache        *cache.MetricsCache
+	cache        *metricscache.Cache
 	errorCounter prometheus.Counter
 }
 
@@ -58,9 +60,13 @@ func NewServiceTagUsageCollector(ctx context.Context, subscriptionNames []string
 	var resourceGraphClient *graphquery.ResourceGraphClient
 	var err error
 
-	subscriptionIDs, err := getSubscriptionIDs(ctx, credential, subscriptionNames)
+	resolved, err := subscriptions.ResolveByName(ctx, credential, subscriptionNames)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get subscription IDs: %w", err)
+	}
+	subscriptionIDs := make([]string, 0, len(resolved))
+	for _, id := range resolved {
+		subscriptionIDs = append(subscriptionIDs, id)
 	}
 	subscriptionIDsPtrs := make([]*string, len(subscriptionIDs))
 	for i, subscriptionID := range subscriptionIDs {
@@ -73,7 +79,7 @@ func NewServiceTagUsageCollector(ctx context.Context, subscriptionNames []string
 
 	return &ServiceTagUsageCollector{
 		client:       resourceGraphClient,
-		cache:        cache.NewMetricsCache(cacheTTL),
+		cache:        metricscache.NewCache(cacheTTL),
 		errorCounter: errorCounter,
 	}, nil
 }
@@ -87,7 +93,7 @@ func (c *ServiceTagUsageCollector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (c *ServiceTagUsageCollector) Collect(ch chan<- prometheus.Metric) {
-	for _, metric := range c.cache.GetAllMetrics() {
+	for _, metric := range c.cache.GetAll() {
 		ch <- metric
 	}
 }
@@ -158,20 +164,13 @@ func (c *ServiceTagUsageCollector) CollectMetricValues(ctx context.Context) {
 			}
 		}
 		for _, ipTag := range ipTags {
-			err = c.cache.AddMetric(prometheus.MustNewConstMetric(
+			labels := []string{publicIP.SubscriptionId, publicIP.Location, ipTag.ServiceTagType, ipTag.ServiceTagValue}
+			c.cache.Set(strings.Join(labels, "/"), prometheus.MustNewConstMetric(
 				ServiceTagUsageByPublicIpCountDesc,
 				prometheus.GaugeValue,
 				publicIP.Count,
-				publicIP.SubscriptionId,
-				publicIP.Location,
-				ipTag.ServiceTagType,
-				ipTag.ServiceTagValue,
+				labels...,
 			))
-			if err != nil {
-				c.errorCounter.Inc()
-				logger.Error(err, "error adding metric to cache")
-				continue
-			}
 		}
 	}
 }
