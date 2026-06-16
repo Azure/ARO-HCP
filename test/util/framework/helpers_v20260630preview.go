ffb556c7cf0d4ee006f2379872fc200bfe53af5e
cadenmarchese <caden.marchese@gmail.com>
2026-06-16 18:02:25 -0400

    feat: add e2e test for BYOK OS Disks
    use Go SDK rather than bicep templates for des e2e test

--- test/util/framework/helpers_v20260630preview.go
+++ test/util/framework/helpers_v20260630preview.go
@@ -90,6 +90,7 @@ type NodePoolParams20260630 struct {
 	AvailabilityZone string
 	AutoRepair       bool
 	Tags             map[string]*string
+	EncryptionSetID  string
 }
 
 // ---------------------------------------------------------------------------
@@ -683,6 +684,10 @@ func BuildNodePoolFromParams20260630(
 			},
 			AutoRepair: to.Ptr(parameters.AutoRepair),
 		},
+	}
+
+	if parameters.EncryptionSetID != "" {
+		nodePool.Properties.Platform.OSDisk.EncryptionSetID = to.Ptr(parameters.EncryptionSetID)
 	}
 
 	if parameters.AutoScaling != nil {
