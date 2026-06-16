e883d6b7fa0922b86b9b77d1b90fd3e261d46a2b
Steven Clarkson <sclarkso@redhat.com>
2026-06-16 10:48:45 +1000

    fix: update cidr test to use dsv3

--- test/e2e/cluster_authorized_cidrs_connectivity.go
+++ test/e2e/cluster_authorized_cidrs_connectivity.go
@@ -97,11 +97,11 @@ var _ = Describe("Authorized CIDRs", func() {
 				vmName := fmt.Sprintf("%s-test-vm", clusterName)
 				// The test VM is a throwaway kubectl client: it only needs a public IP
 				// (used as the single authorized CIDR) and to make one API call to the
-				// cluster. We use the common general-purpose Standard_D2s_v5 (same 2-vCPU
+				// cluster. We use the common general-purpose Standard_D2s_v3 (same 2-vCPU
 				// size as the old burstable Standard_B2s) because the burstable B-series
 				// is the SKU class most prone to regional SkuNotAvailable capacity
 				// restrictions, which is what flaked this test.
-				const vmSize = "Standard_D2s_v5"
+				const vmSize = "Standard_D2s_v3"
 				var vmDeployment *armresources.DeploymentExtended
 				var deployErr error
 				// Bounded retry to absorb transient ARM errors, but fail fast on
