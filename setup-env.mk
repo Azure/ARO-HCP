2f04b7200b4cd13f4afcd2ecc08ac0e133ace0a1
Tim Steffens <tim.steffens@redhat.com>
2026-06-18 17:24:04 +0200

    fix comment chars

--- setup-env.mk
+++ setup-env.mk
@@ -1,7 +1,7 @@
 CONTAINER_ENGINE ?= $(shell command -v podman 2>/dev/null || echo docker)
 
-// You can specify which docker command az acr login should use by setting DOCKER_COMMAND accordingly.
-// See also https://learn.microsoft.com/en-us/azure/container-registry/container-registry-authentication?tabs=azure-cli#sign-in-by-using-an-alternative-container-tool-instead-of-docker
+# You can specify which docker command az acr login should use by setting DOCKER_COMMAND accordingly.
+# See also https://learn.microsoft.com/en-us/azure/container-registry/container-registry-authentication?tabs=azure-cli#sign-in-by-using-an-alternative-container-tool-instead-of-docker
 define acr-login
 DOCKER_COMMAND="$(CONTAINER_ENGINE)" az acr login --name $(ARO_HCP_IMAGE_ACR)
 endef
