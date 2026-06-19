1356a727f566b3b95b093277edb20298c56d42af
Cliff Schomburg <7424213+cssjr@users.noreply.github.com>
2026-06-19 09:07:10 -0700

    fix: restrict CPO override to 4.22.x only, revert timeout change
    The CPO from main passes --config to cluster-image-registry-operator,
    which is not supported in 4.20/4.21 release payloads. Restrict the
    override to 4.22.x where the flag is supported. Restore 4.19/4.20/4.21
    to their original production overrides. Revert the cluster creation
    timeout back to 20 minutes.

--- test/util/framework/constants.go
+++ test/util/framework/constants.go
@@ -20,7 +20,7 @@ import "time"
 
 // Provisioning timeouts
 const (
-	ClusterCreationTimeout      = 45 * time.Minute
+	ClusterCreationTimeout      = 20 * time.Minute
 	NodePoolCreationTimeout     = 20 * time.Minute
 	ExternalAuthCreationTimeout = 15 * time.Minute
 	GetAdminRESTConfigTimeout   = 10 * time.Minute
