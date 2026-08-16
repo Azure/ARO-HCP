4b2217a28ac9f756c8c99e2b3732fe3a2a613f16
Gerd Oberlechner <goberlec@redhat.com>
2026-08-16 12:35:29 +0200

    refactor: restructure stamp watching controller
    Move generic watching controller logic from backend into
    internal/controllerutils to make it reusable across services.
    StampWatchingController is now a type alias for
    GenericWatchingController[StampKey], eliminating duplicated Run,
    processNext, and QueueForInformers logic.
    
    Stamp-specific tests for enqueue, cooldown, processNext, and shutdown
    behavior were removed because they tested code that now lives in the
    generic watching controller layer, where equivalent coverage exists.

--- fleet/pkg/controllers/base/stamp_watching_controller.go
+++ fleet/pkg/controllers/base/stamp_watching_controller.go
@@ -17,20 +17,14 @@ package base
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/go-logr/logr"
 
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/apimachinery/pkg/util/wait"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/workqueue"
 	utilsclock "k8s.io/utils/clock"
 
 	azcorearm "github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
 
-	"github.com/Azure/ARO-HCP/internal/api/coreapi"
 	"github.com/Azure/ARO-HCP/internal/api/fleetapi"
 	"github.com/Azure/ARO-HCP/internal/api/metadataapi"
 	"github.com/Azure/ARO-HCP/internal/controllerutils"
@@ -42,16 +36,6 @@ const DefaultStampCooldownPeriod = 10 * time.Minute
 const DefaultInformerResyncPeriod = 5 * time.Minute
 
 var ErrStampNotApproved = errors.New("parent stamp is not approved")
-
-type stampScoped interface {
-	GetStampIdentifier() string
-}
-
-// Notifier is the subset of cache.SharedIndexInformer needed to register
-// event handlers. Any informer satisfies this interface.
-type Notifier interface {
-	AddEventHandlerWithOptions(handler cache.ResourceEventHandler, options cache.HandlerOptions) (cache.ResourceEventHandlerRegistration, error)
-}
 
 // StampKey identifies a Stamp in the workqueue.
 type StampKey struct {
@@ -70,20 +54,6 @@ func (k StampKey) AddLoggerValues(logger logr.Logger) logr.Logger {
 	return logger.WithValues(
 		utils.LogValues{}.
 			AddLogValuesForResourceID(k.GetResourceID())...)
-}
-
-// StampKeyFromObject extracts the workqueue key from any fleet
-// object that carries a stamp identifier (*fleetapi.Stamp, *fleetapi.ManagementCluster).
-func StampKeyFromObject(obj any) (StampKey, error) {
-	s, ok := obj.(stampScoped)
-	if !ok {
-		return StampKey{}, fmt.Errorf("object %T does not implement stampScoped", obj)
-	}
-	id := s.GetStampIdentifier()
-	if len(id) == 0 {
-		return StampKey{}, fmt.Errorf("object %T has empty stamp identifier", obj)
-	}
-	return StampKey{StampIdentifier: id}, nil
 }
 
 // StampSyncer is the interface that concrete stamp controllers implement.
@@ -114,12 +84,7 @@ func (c StampWatchingControllerConfig) withDefaults() StampWatchingControllerCon
 // StampWatchingController is a controller base that watches
 // fleet informers, handles etag-based change detection with cooldown gating,
 // and delegates reconciliation to a StampSyncer.
-type StampWatchingController struct {
-	name     string
-	syncer   StampSyncer
-	queue    workqueue.TypedRateLimitingInterface[StampKey]
-	cooldown controllerutils.CooldownChecker
-}
+type StampWatchingController = controllerutils.GenericWatchingController[StampKey]
 
 // NewStampWatchingController creates a controller and delegates
 // reconciliation to the syncer. Call QueueForInformers to register informers.
@@ -139,116 +104,24 @@ func NewStampWatchingController(
 		cooldown = checker
 	}
 
-	return &StampWatchingController{
-		name:   name,
-		syncer: syncer,
-		queue: workqueue.NewTypedRateLimitingQueueWithConfig(
-			workqueue.DefaultTypedControllerRateLimiter[StampKey](),
-			workqueue.TypedRateLimitingQueueConfig[StampKey]{Name: name},
-		),
-		cooldown: cooldown,
-	}
+	adapter := &genericStampSyncer{syncer: syncer, cooldown: cooldown}
+	return controllerutils.NewGenericWatchingController(name, fleetapi.StampResourceType, adapter, ReconcileTotal)
 }
 
-// QueueForInformers registers notifiers whose objects feed into the workqueue.
-// Objects must implement both stampScoped (for key extraction) and
-// coreapi.CosmosPersistable (for etag-based change detection). Add events enqueue
-// immediately. Update events enqueue immediately when the Cosmos etag changed
-// and consult the cooldown gate otherwise.
-func (c *StampWatchingController) QueueForInformers(resyncDuration time.Duration, notifiers ...Notifier) error {
-	errs := []error{}
-	logger := utils.DefaultLogger()
-	logger = logger.WithValues(
-		utils.LogValues{}.AddControllerName(c.name)...,
-	)
-
-	for _, notifier := range notifiers {
-		_, err := notifier.AddEventHandlerWithOptions(
-			cache.ResourceEventHandlerFuncs{
-				AddFunc:    c.handleAdd,
-				UpdateFunc: c.handleUpdate,
-			},
-			cache.HandlerOptions{
-				Logger:       &logger,
-				ResyncPeriod: &resyncDuration,
-			},
-		)
-		errs = append(errs, err)
-	}
-	return errors.Join(errs...)
+// genericStampSyncer adapts a StampSyncer into a controllerutils.GenericSyncer[StampKey].
+type genericStampSyncer struct {
+	syncer   StampSyncer
+	cooldown controllerutils.CooldownChecker
 }
 
-func (c *StampWatchingController) handleAdd(obj any) {
-	c.enqueue(obj, true)
+func (a *genericStampSyncer) SyncOnce(ctx context.Context, key StampKey) error {
+	return a.syncer.SyncOnce(ctx, key)
 }
 
-func (c *StampWatchingController) handleUpdate(oldObj, newObj any) {
-	oldPersistable, oldOK := oldObj.(coreapi.CosmosPersistable)
-	newPersistable, newOK := newObj.(coreapi.CosmosPersistable)
-	if !oldOK || !newOK {
-		utilruntime.HandleError(fmt.Errorf("update handler: expected CosmosPersistable, got old=%T new=%T", oldObj, newObj))
-		return
-	}
-	changed := oldPersistable.GetCosmosData().GetEtag() != newPersistable.GetCosmosData().GetEtag()
-	c.enqueue(newObj, changed)
+func (a *genericStampSyncer) CooldownChecker() controllerutils.CooldownChecker {
+	return a.cooldown
 }
 
-// enqueue extracts the StampKey and adds it to the workqueue.
-// When changed is true, the key is enqueued immediately. Otherwise, the
-// cooldown gate decides.
-func (c *StampWatchingController) enqueue(obj any, changed bool) {
-	key, err := StampKeyFromObject(obj)
-	if err != nil {
-		utilruntime.HandleError(fmt.Errorf("enqueue key extraction: %w", err))
-		return
-	}
-	if changed {
-		c.queue.Add(key)
-		return
-	}
-	if !c.cooldown.CanSync(context.TODO(), key) {
-		return
-	}
-	c.queue.Add(key)
-}
-
-func (c *StampWatchingController) Run(ctx context.Context, threadiness int) {
-	defer utilruntime.HandleCrash()
-	defer c.queue.ShutDown()
-
-	ctx = utils.ContextWithControllerName(ctx, c.name)
-	logger := utils.LoggerFromContext(ctx).WithValues(utils.LogValues{}.AddControllerName(c.name)...)
-	ctx = utils.ContextWithLogger(ctx, logger)
-	logger.Info("starting controller")
-	defer logger.Info("stopped controller")
-
-	for range threadiness {
-		go wait.UntilWithContext(ctx, c.runWorker, time.Second)
-	}
-	<-ctx.Done()
-}
-
-func (c *StampWatchingController) runWorker(ctx context.Context) {
-	for c.processNext(ctx) {
-	}
-}
-
-func (c *StampWatchingController) processNext(ctx context.Context) bool {
-	key, shutdown := c.queue.Get()
-	if shutdown {
-		return false
-	}
-	defer c.queue.Done(key)
-
-	logger := key.AddLoggerValues(utils.LoggerFromContext(ctx))
-	ctx = utils.ContextWithLogger(ctx, logger)
-
-	ReconcileTotal.WithLabelValues(c.name).Inc()
-	if err := c.syncer.SyncOnce(ctx, key); err != nil {
-		utilruntime.HandleErrorWithContext(ctx, err, "sync error; requeuing", "key", key)
-		c.queue.AddRateLimited(key)
-		return true
-	}
-	c.queue.Forget(key)
-	return true
+func (a *genericStampSyncer) MakeKey(resourceID *azcorearm.ResourceID) StampKey {
+	return StampKey{StampIdentifier: resourceID.Name}
 }
