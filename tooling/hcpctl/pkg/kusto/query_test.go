fea039637cde40d70efbfdf8e1ba6c2460b015a4
Tim Steffens <tim.steffens@redhat.com>
2026-09-02 14:29:55 +0200

    feat: collect velero backup server logs with must-gather query to ease investigating backup issues

--- tooling/hcpctl/pkg/kusto/query_test.go
+++ tooling/hcpctl/pkg/kusto/query_test.go
@@ -243,6 +243,35 @@ func TestVeleroHcpLogs_NoTruncation(t *testing.T) {
 	testutil.CompareWithFixture(t, queryToFixture(queries[0]))
 }
 
+func TestVeleroServerLogs(t *testing.T) {
+	f, err := NewQueryFactory()
+	require.NoError(t, err)
+	opts := baseOptions()
+	def, err := f.GetCustomQueryDefinition("veleroServerLogs")
+	require.NoError(t, err)
+	require.NotNil(t, def, "veleroServerLogs custom query not found")
+	queries, err := f.Build(*def, NewTemplateDataFromOptions(opts, WithClusterNames([]string{"mgmt-cluster-1", "mgmt-cluster-2"})))
+	require.NoError(t, err)
+	require.Len(t, queries, 1)
+
+	testutil.CompareWithFixture(t, queryToFixture(queries[0]))
+}
+
+func TestVeleroServerLogs_NoTruncation(t *testing.T) {
+	f, err := NewQueryFactory()
+	require.NoError(t, err)
+	opts := baseOptions()
+	opts.Limit = -1
+	def, err := f.GetCustomQueryDefinition("veleroServerLogs")
+	require.NoError(t, err)
+	require.NotNil(t, def, "veleroServerLogs custom query not found")
+	queries, err := f.Build(*def, NewTemplateDataFromOptions(opts, WithClusterNames([]string{"mgmt-cluster-1", "mgmt-cluster-2"})))
+	require.NoError(t, err)
+	require.Len(t, queries, 1)
+
+	testutil.CompareWithFixture(t, queryToFixture(queries[0]))
+}
+
 func TestClusterIdQuery(t *testing.T) {
 	f, err := NewQueryFactory()
 	require.NoError(t, err)
