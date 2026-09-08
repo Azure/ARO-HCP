e3b08c336fca216fc1d9a0b0d29931b76a0d03f2
cadenmarchese <caden.marchese@gmail.com>
2026-09-08 09:31:34 -0400

    feat(api): remove activeKey in 10-03-preview in favor of keyURL
    This aligns our APIs with Microsoft best practices and unblocks
    future support of other keyvault types. Update round trip tests

--- internal/azureapi/v20261003preview/generated/models.go
+++ internal/azureapi/v20261003preview/generated/models.go
@@ -565,11 +565,8 @@ type IngressProfile struct {
 // to create the cluster must be authorized to access this keyvault, e.g using the AzureCLI: az keyvault
 // set-policy -n $KEYVAULT_NAME --key-permissions decrypt encrypt --spn (YOUR APPLICATION CLIENT ID)
 type KmsEncryptionProfile struct {
-	// REQUIRED; The details of the active key.
-	ActiveKey *KmsKey
-
-	// REQUIRED; vaultName is the name of the keyvault that contains the secret.
-	VaultName *string
+	// REQUIRED; The versioned URL of the key used for ETCD data encryption. Format: https://{vault-name}.{vault-type}.azure.{cloud}/keys/{key-name}/{key-version}
+	KeyEncryptionKeyURL *string
 
 	// REQUIRED; visibility of the keyvault that contains the secret.
 	Visibility *KeyVaultVisibility
@@ -579,23 +576,8 @@ type KmsEncryptionProfile struct {
 // used to create the cluster must be authorized to access this keyvault, e.g using the AzureCLI: az keyvault
 // set-policy -n $KEYVAULT_NAME --key-permissions decrypt encrypt --spn (YOUR APPLICATION CLIENT ID)
 type KmsEncryptionProfileUpdate struct {
-	// The details of the active key.
-	ActiveKey *KmsKeyUpdate
-}
-
-// KmsKey - A representation of a KeyVault Secret.
-type KmsKey struct {
-	// REQUIRED; name is the name of the keyvault key used for encryption/decryption.
-	Name *string
-
-	// REQUIRED; version contains the version of the key to use.
-	Version *string
-}
-
-// KmsKeyUpdate - A representation of a KeyVault Secret.
-type KmsKeyUpdate struct {
-	// version contains the version of the key to use.
-	Version *string
+	// The versioned URL of the key used for ETCD data encryption. Format: https://{vault-name}.{vault-type}.azure.{cloud}/keys/{key-name}/{key-version}
+	KeyEncryptionKeyURL *string
 }
 
 // Label represents the Kubernetes label
