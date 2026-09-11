ac644b5dc8ccd3e7a622f6cc324c6af92f40b3cd
David Eads <deads@redhat.com>
2026-09-11 13:40:45 -0400

    fix(backend): trigger rollout status collection when inputs change

--- backend/pkg/app/backend.go
+++ backend/pkg/app/backend.go
@@ -699,8 +699,7 @@ func (b *Backend) runBackendControllersUnderLeaderElection(ctx context.Context, 
 	controlPlaneVersionStatusController := versionrollout.NewStatusCollectorController(
 		b.options.FleetDBClient,
 		fleetInformers,
-		serviceProviderClusterLister,
-		clusterLister,
+		backendInformers,
 		b.clock,
 		rolloutConfig,
 	)
